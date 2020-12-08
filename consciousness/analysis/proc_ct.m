7a27378e0071455aa613f4463e9ffa740202dfcb
flashsherlock <sherlockholmes221b@163.com>
2020-12-09 00:46:42 +0800

    fuse MRI and CT image

---
+++ consciousness/analysis/proc_ct.m
@@ -0,0 +1,44 @@
+%% load mri image
+subjID = 's02';
+filepath='E:/NanChang/use';
+ct = ft_read_mri([filepath '/' subjID '_CT.nii']);
+%% determine left and right
+ft_determine_coordsys(ct);
+%% Align the anatomical CT to the CTF head surface coordinate system
+cfg           = [];
+cfg.method    = 'interactive';
+cfg.coordsys  = 'ctf';
+ct_ctf = ft_volumerealign(cfg, ct);
+%% Align again
+cfg           = [];
+cfg.method    = 'interactive';
+cfg.coordsys  = 'ctf';
+ct_ctf = ft_volumerealign(cfg, ct_ctf);
+%% convert the CT¡¯s coordinate system into an approximation of the ACPC coordinate system
+ct_acpc = ft_convert_coordsys(ct_ctf, 'acpc');
+%% save acpc aligned image
+cfg           = [];
+cfg.filename  = [filepath '/' subjID '_CT_acpc'];
+cfg.filetype  = 'nifti';
+cfg.parameter = 'anatomy';
+ft_volumewrite(cfg, ct_acpc);
+%% load spm reoriented img
+ct_acpc = ft_read_mri([filepath '/' subjID '_CT_acpc.nii']);
+%% load aligned MRI image
+% fsmri_acpc = ft_read_mri('freesurfer/mri/T1.mgz'); 
+% on Windows, use 'SubjectUCI29_MR_acpc.nii'
+fsmri_acpc = ft_read_mri([filepath '/' subjID '_MRI_acpc.nii']);
+fsmri_acpc.coordsys = 'acpc';
+%% Fusion of the CT with the MRI
+cfg             = [];
+cfg.method      = 'spm';
+cfg.spmversion  = 'spm12';
+cfg.coordsys    = 'acpc';
+cfg.viewresult  = 'yes';
+ct_acpc_f = ft_volumerealign(cfg, ct_acpc, fsmri_acpc);
+%% Write the preprocessed anatomical MRI out to file.
+cfg           = [];
+cfg.filename  = [filepath '/' subjID '_CT_acpc_f'];
+cfg.filetype  = 'nifti';
+cfg.parameter = 'anatomy';
+ft_volumewrite(cfg, ct_acpc_f);
---
+++ consciousness/analysis/proc_mri.m
@@ -0,0 +1,19 @@
+%% load mri image
+subjID = 's02';
+filepath='E:/NanChang/use';
+mri = ft_read_mri([filepath '/' subjID '_MRI.nii']);
+%% determine left and right
+ft_determine_coordsys(mri);
+%% realign to ac-pc system
+cfg           = [];
+cfg.method    = 'interactive';
+cfg.coordsys  = 'acpc';
+% do not put external toolbox in matlab path
+% addpath 'C:\Program Files\MATLAB\R2012b\toolbox\fieldtrip-20201021\compat\matlablt2017b'
+mri_acpc = ft_volumerealign(cfg, mri);
+%% Write the preprocessed anatomical MRI out to file.
+cfg           = [];
+cfg.filename  = [filepath '/' subjID '_MRI_acpc'];
+cfg.filetype  = 'nifti';
+cfg.parameter = 'anatomy';
+ft_volumewrite(cfg, mri_acpc);

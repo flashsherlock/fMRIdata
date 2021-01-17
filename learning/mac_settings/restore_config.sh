c0c54f4fd0f96767f755c889517cc3f65b8f5097
flashsherlock <sherlockholmes221b@163.com>
2021-01-17 16:52:31 +0800

    create restore_config.sh to restore dotfiles

---
+++ learning/mac_settings/restore_config.sh
@@ -0,0 +1,8 @@
+# cd /Users/mac/Documents/GitHub/fMRIdata/learning/mac_settings/
+cp ./.cshrc ~
+cp ./.tcshrc ~
+cp ./.vimrc ~
+cp ./.zshrc ~
+cp ./.vimrc_bk ~
+cp ./.tmux.conf ~
+cp ./startup.m ~/matlab/

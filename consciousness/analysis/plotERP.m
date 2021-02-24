3c17385b6e0ab65d7f850420682e247b266708b9
flashsherlock <sherlockholmes221b@163.com>
2021-02-24 23:41:10 +0800

    plot ERP and TF

--- consciousness/analysis/plotERP.m
+++ consciousness/analysis/plotERP.m
@@ -12,6 +12,11 @@ for i=1:3
     % load and plot if data exist
     if exist(dataset,'file')
         load(dataset);
+        % Baseline-correction options
+        cfg = [];
+        cfg.demean          = 'yes';
+        cfg.baselinewindow  = [-2 0];
+        eeg = ft_preprocessing(cfg, eeg);
         % prepare layout for ploting
         lay = nc_prepare_lay(eeg);
         % plot ERP for each odor
@@ -19,7 +24,9 @@ for i=1:3
         for iodor=[2 4 5]
             % define trials to be ploted
             cfg = [];
-            cfg.trials = find(data_clean.trialinfo==iodor);
+            cfg.trials = find(eeg.trialinfo==iodor);
+            % all of the trials
+            cfg.trials = 1:length(eeg.trialinfo);
             % timelock analysis
             odor = ft_timelockanalysis(cfg, eeg);
             % plot ERP
@@ -28,9 +35,39 @@ for i=1:3
             cfg.interactive = 'yes';
             cfg.showoutline = 'yes';
             ft_multiplotER(cfg, odor);
+            
+            % time-frequency analysis
+            cfg            = [];
+            cfg.method     = 'mtmconvol';
+            cfg.toi        = -1:0.01:5;
+            cfg.foi        = 1:5:96;
+            cfg.t_ftimwin  = ones(length(cfg.foi),1).*0.2;
+            cfg.taper      = 'hanning';
+            cfg.output     = 'pow';
+            cfg.keeptrials = 'no';
+            freq = ft_freqanalysis(cfg, eeg);
+            % baseline correction
+            cfg              = [];
+            cfg.baseline     = [-1 0];
+            cfg.baselinetype = 'relchange';
+            freq_blc = ft_freqbaseline(cfg, freq);
+            % plot
+            cfg             = [];
+            cfg.layout      = lay;
+            cfg.showoutline = 'yes';
+            ft_multiplotTFR(cfg, freq_blc);
         end
     end
 end
+%% plot signal
+cfg = [];
+cfg.channel = 1:5;
+cfg.viewmode = 'vertical';
+cfg.channelclamped = {'DC05'};
+cfg.ylim = 'maxmin';
+cfg.mychan = {'DC05'};
+cfg.mychanscale = 1e-5*2;
+% eegplot = ft_databrowser(cfg,eeg);
 %% old codes for ploting markers
 % load('subject1_2.mat')
 % m=eeg.trial{1,1}(39:42,:);

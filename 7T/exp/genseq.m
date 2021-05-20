d6ea02f2a00dd959f3d4986af728a83242fa910c
flashsherlock <sherlockholmes221b@163.com>
2021-05-20 17:25:02 +0800

    update genseq.m for new odor numbers

--- 7T/exp/genseq.m
+++ 7T/exp/genseq.m
@@ -1,15 +1,20 @@
+% number of odors
+odornum=5;
 % generate sequence for valence and intensity rating
 vi=[];
 for i=1:2
-    temp=perms(1:4);
+    % perms returns all possible sequences
+    temp=perms(1:odornum);
+    % add a rand sequence to the first column
     temp=[randperm(length(temp))' temp];
+    % sortrows and return
     temp=sortrows(temp);
     vi=[vi;temp(:,2:end)];
 end
 % generate sequence for similarity rating
 subjs=48;
 si=cell(subjs,6);
-comb=nchoosek(1:4,2);
+comb=nchoosek(1:odornum,2);
 temp=perms(1:6);
 temp=[randperm(length(temp))' temp];
 temp=sortrows(temp);

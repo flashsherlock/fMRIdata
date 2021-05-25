ab7af81a975a5f4378e32f2d874ec40ca674b5f5
flashsherlock <sherlockholmes221b@163.com>
2021-05-25 21:17:59 +0800

    add picture numbersi

--- learning/picjoint.m
+++ learning/picjoint.m
@@ -13,3 +13,19 @@ imwrite([val number var],[outdir filesep 'number1.bmp']);
 inl=imread([datadir filesep '非常微弱.bmp']);
 inr=imread([datadir filesep '非常强烈.bmp']);
 imwrite([inl number inr],[outdir filesep 'number2.bmp']);
+
+sil=imread([datadir filesep '非常不同.bmp']);
+sir=imread([datadir filesep '非常相似.bmp']);
+imwrite([sil number sir],[outdir filesep 'numbersi.bmp']);
+
+int=zeros(2,5);
+for i=1:5
+% valence
+temp=result(result(:,1)==i&result(:,2)==1,6);
+temp(temp==0)=nan;
+int(1,i)=nanmean(temp);
+% intensity
+temp=result(result(:,1)==i&result(:,2)==2,6);
+temp(temp==0)=nan;
+int(2,i)=nanmean(temp);
+end

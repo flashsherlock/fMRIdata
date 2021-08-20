880f4242fb1fc512cf75fb7bcca6ce751eb4db3d
flashsherlock <sherlockholmes221b@163.com>
2021-08-20 09:50:28 +0800

    try TRs of 3s & 6s

--- 7T/odor/MVPA_cleandata/acc5odor/run_decoding.m
+++ 7T/odor/MVPA_cleandata/acc5odor/run_decoding.m
@@ -1,5 +1,5 @@
 analysis_all={'pabiode'};
-shift=[6 9];
+shift=[3 6];
 % rois={'Amy9_align','corticalAmy_align','CeMeAmy_align','BaLaAmy_align'};
 % for region=[1 3 5 6 7 8 9 10 15]
 %     rois=[rois {['Amy_align' num2str(region) 'seg']}];
@@ -12,7 +12,7 @@ rois={'Amy8_at165','corticalAmy_at165','CeMeAmy_at165','BaLaAmy_at165','Pir_new_
 % rois={'whole_brain'};
 
 % S04-S08
-for i=5:8
+for i=4:8
     sub=sprintf('S%02d',i);
     decoding_roi_5odors_trial(sub,analysis_all,rois,shift);
     decoding_roi_trial(sub,analysis_all,rois,shift);

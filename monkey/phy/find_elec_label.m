dadac8234a08640e15563b63d0b172e19de9617c
flashsherlock <sherlockholmes221b@163.com>
2021-11-12 22:15:29 +0800

    find labels according to levels provided

--- monkey/phy/find_elec_label.m
+++ monkey/phy/find_elec_label.m
@@ -1,4 +1,4 @@
-function position=find_elec_label(subjID,elec)
+function position=find_elec_label(subjID,elec,level)
 % atlas
 atpath = '/Volumes/WD_D/gufei/monkey_data/IMG/';
 % atlas = ft_read_atlas([atpath subjID '_NMT/' 'SARM_in_' subjID '_anat.nii.gz']);
@@ -7,27 +7,31 @@ atpath = '/Volumes/WD_D/gufei/monkey_data/IMG/';
 % save([atpath subjID '_NMT/' 'SARM_in_' subjID '_anat.mat'],'atlas');
 load([atpath subjID '_NMT/' 'SARM_in_' subjID '_anat.mat'])
 
-level=6;
+% transform to atlas space
+x = spm_coreg([atpath subjID '_NMT/' subjID '_anat.nii.gz'], [atpath '/' subjID '_MRI_acpc.nii']);
+elec.chanpos=ft_warp_apply(inv(spm_matrix(x(:)')), elec.chanpos, 'homogenous');
+elec.elecpos=elec.chanpos;
+
+% level=3;
 len=length(elec.label);
-position=cell(len,level-2);
+position=cell(len,length(level));
 
-for i_level=3:level
-i_atlas=atlas;
-i_atlas.dim=i_atlas.dim(1:3);
-i_atlas.parcellation=squeeze(i_atlas.parcellation(:,:,:,i_level));
-cfg            = [];
-% cfg.roi        = elec.chanpos(match_str(elec.label,'LHH1'),:);
-cfg.roi        = elec.chanpos(:,:);
-cfg.atlas      = i_atlas;
-% cfg.inputcoord = 'mni';
-cfg.output     = 'multiple';
-labels = ft_volumelookup(cfg, atlas);
-
-for i=1:len
-    [~, indx] = max(labels(i).count);
-    position{i,i_level}=labels(i).name(indx);
-end
-
+% lookup for each level
+for i_level=1:length(level)
+    % lookup positions
+    i_atlas=atlas;
+    i_atlas.dim=i_atlas.dim(1:3);
+    i_atlas.parcellation=squeeze(i_atlas.parcellation(:,:,:,level(i_level)));
+    cfg            = [];
+    cfg.roi        = elec.chanpos(:,:);
+    cfg.atlas      = i_atlas;
+    cfg.output     = 'multiple';
+    labels = ft_volumelookup(cfg, atlas);
+    % find labels
+    for i=1:len
+        [~, indx] = max(labels(i).count);
+        position{i,i_level}=labels(i).name(indx);
+    end
 end
 
 end
--- monkey/phy/find_elec_position.m
+++ monkey/phy/find_elec_position.m
@@ -1,8 +1,14 @@
 subjID = 'RM035';
 filepath = '/Volumes/WD_D/gufei/monkey_data/IMG/';
 load([filepath '/' subjID '_elec.mat']);
-position = find_elec_label(subjID,elec_acpc_f);
+level=3:4;
+position = find_elec_label(subjID,elec_acpc_f,level);
 %% save position
 % add a new field and save
 elec_acpc_f.position=position;
 save([filepath '/' subjID '_elec_label.mat'], 'elec_acpc_f');
+%% plot electrodes on nii
+
+cmd=['3dcalc -a ' ]
+            -expr 'step(9-(x-20)*(x-20)-(y-30)*(y-30)-(z-70)*(z-70))' \
+            -prefix ball

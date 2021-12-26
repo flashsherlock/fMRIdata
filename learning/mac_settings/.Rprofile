830a9d9d3dbf447382c1d71b14cc67bdb2eb170c
flashsherlock <sherlockholmes221b@163.com>
2021-12-26 17:06:49 +0800

    set vs-code r

---
+++ learning/mac_settings/.Rprofile
@@ -0,0 +1,5 @@
+options(vsc.use_httpgd = TRUE)
+Sys.setenv(TERM_PROGRAM="vscode")
+source(file.path(Sys.getenv(
+    if (.Platform$OS.type == "windows") "USERPROFILE" else "HOME"
+	 ), ".vscode-R", "init.R"))
---
+++ learning/plot.r
@@ -0,0 +1,13 @@
+# install vscode-r extension
+
+# install packages
+# install.packages("languageserver")
+# install.packages("httpgd")
+
+# test plot
+h <- c(1, 2, 3, 4, 5, 6)
+M <- c("A", "B", "C", "D", "E", "F")
+barplot(h,
+    names.arg = M, xlab = "X", ylab = "Y",
+    col = "#00cec9", main = "Chart", border = "#fdcb6e"
+)

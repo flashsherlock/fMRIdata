c279ecdaa0b2a033f21a777bfc92efaec0fd75ac
flashsherlock <sherlockholmes221b@163.com>
2022-01-24 14:42:00 +0800

    can also ajust horizontal position

--- 7T/exp/test_position.m
+++ 7T/exp/test_position.m
@@ -2,14 +2,17 @@ function test_position(offcenter_x, offcenter_y)
 % ROIsLocalizer(offcenter_x, offcenter_y), [LO, FFA, and EBA]
 % Scan = 396s, TR = 3s, 132TR
 if nargin < 2
-    offcenter_x=70; offcenter_y=-165;
+    offcenter_x=0; offcenter_y=-205;
 end
 upmax=-260;
 downmax=50;
+leftmax=-180;
+rightmax=180;
 KbName('UnifyKeyNames');
 escapeKey = KbName('ESCAPE');
 Key1 = KbName('1!');
 Key2 = KbName('2@');
+KeyR = KbName('r');
 
 % rating instruction
 scale=0.8;
@@ -24,8 +27,8 @@ Screen('Preference', 'SkipSyncTests', 1);
 AssertOpenGL;
 whichscreen=max(Screen('Screens'));
 % backup resolution
-oldResolution=Screen('Resolution', whichscreen);
-Screen('Resolution', whichscreen, 800, 600);
+% oldResolution=Screen('Resolution', whichscreen);
+oldResolution=Screen('Resolution', whichscreen, 800, 600);
 
 % colors
 black=BlackIndex(whichscreen);
@@ -54,6 +57,7 @@ Screen('DrawTexture',windowPtr,ins(1),[],StimRect);
 Screen('DrawTexture',windowPtr,number(1),[],StimRect_num);
 Screen('Flip',windowPtr);
 
+% up and down
 while 1
     [touch, ~, keyCode] = KbCheck;
     ifkey=[keyCode(Key1) keyCode(Key2)];
@@ -73,10 +77,36 @@ while 1
         Screen('DrawTexture',windowPtr,ins(1),[],StimRect);
         Screen('DrawTexture',windowPtr,number(1),[],StimRect_num);
         Screen('Flip',windowPtr);
+    elseif touch && keyCode(KeyR) 
+        % left and right
+        while 1
+            [touch, ~, keyCode] = KbCheck;
+            ifkey=[keyCode(Key1) keyCode(Key2)];
+            if touch && ismember(1,ifkey)             
+                switch find(ifkey==1,1,'first')
+                    % left
+                    case 1
+                        offcenter_x=max(offcenter_x-5,leftmax);
+                        WaitSecs(0.1);
+                    % right
+                    case 2
+                        offcenter_x=min(offcenter_x+5,rightmax);
+                        WaitSecs(0.1);
+                end
+                StimRect=OffsetRect(CenterRect(StimSize,rect),offcenter_x,offcenter_y-distance);
+                StimRect_num=OffsetRect(CenterRect(StimSize_num,rect),offcenter_x-1,offcenter_y+distance);
+                Screen('DrawTexture',windowPtr,ins(1),[],StimRect);
+                Screen('DrawTexture',windowPtr,number(1),[],StimRect_num);
+                Screen('Flip',windowPtr);
+            elseif touch && keyCode(escapeKey)        
+                break
+            end
+        end
     elseif touch && keyCode(escapeKey)        
         break
     end
 end
+
 % restore
 Priority(oldPriority);
 ShowCursor;
@@ -84,5 +114,6 @@ ListenChar(0);      % open keyboard
 Screen('CloseAll');
 %restore resolution
 Screen('Resolution', whichscreen, oldResolution.width, oldResolution.height);
-disp(offcenter_y);
+disp('offcenter_x, offcenter_y')
+disp([offcenter_x,offcenter_y]);
 return

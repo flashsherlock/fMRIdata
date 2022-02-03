e7be8cbd703510baffbaf76090024247d006cca8
flashsherlock <sherlockholmes221b@163.com>
2022-02-03 22:14:49 +0800

    update SQL scripts

--- learning/mySQL/learn_invoicing.sql
+++ learning/mySQL/learn_invoicing.sql
@@ -1,3 +1,14 @@
+-- CREATE TABLE
+CREATE TABLE invoices_archived AS
+SELECT *
+FROM invoices
+JOIN clients USING (client_id)
+WHERE payment_date IS NOT NULL;
+
+-- UPDATE table
+UPDATE invoices
+SET payment_total = 10, payment_date = '2019-03-03'
+WHERE client_id = 3;
 -- UNION
 SELECT
 	'first half of 2019' AS date_range,
@@ -115,3 +126,192 @@ FROM (
 WHERE total_sales IS NOT NULL ;
 
 -- mySQL numeric function
+SELECT TRUNCATE(3.1425,2);
+
+-- CREATE VIEW name AS commands (do not save data)
+CREATE VIEW clients_balance AS
+SELECT
+client_id,
+`name`,
+SUM(invoice_total)-	SUM(payment_total) AS balance
+FROM clients
+JOIN invoices USING (client_id)
+GROUP BY client_id,`name`;
+
+-- CREATE OR REPLACE VIEW clients_balance AS
+-- DROP PROCEDURE IF EXISTS clients_balance
+-- WITH CHECK OPTION
+
+-- PROCEDURE FUNCTION
+DELIMITER $$
+CREATE PROCEDURE get_invoices_with_balance()
+BEGIN
+	SELECT
+	*,
+	invoice_total-payment_total AS balance
+	FROM invoices
+	HAVING balance > 0;
+END$$
+DELIMITER ;
+CALL get_invoices_with_balance();
+
+-- PROCEDURE with parameters
+DROP PROCEDURE IF EXISTS get_invoices_by_client;
+DELIMITER $$
+CREATE PROCEDURE get_invoices_by_client(client_id INT)
+BEGIN
+	IF client_id IS NULL THEN
+		SET client_id = 1;
+	END IF;
+	SELECT
+	*
+	FROM invoices i
+	WHERE i.client_id = client_id;
+END$$
+DELIMITER ;
+CALL get_invoices_by_client(1);
+CALL get_invoices_by_client(NULL);
+
+-- IFNULL(state,c.state)
+DROP PROCEDURE IF EXISTS get_invoices_by_staten;
+DELIMITER $$
+CREATE PROCEDURE get_invoices_by_staten(state CHAR(2))
+BEGIN
+	SELECT
+	*
+	FROM clients c
+	WHERE c.state COLLATE utf8mb4_unicode_ci = IFNULL(state,c.state);
+END$$
+DELIMITER ;
+CALL get_invoices_by_staten(NULL);
+
+DELIMITER $$
+DROP PROCEDURE IF EXISTS get_invoices_by_clientname;
+CREATE PROCEDURE get_invoices_by_clientname(client VARCHAR(10))
+BEGIN
+	SELECT
+	*
+	FROM invoices i
+	JOIN clients USING (client_id)
+	WHERE `name` COLLATE utf8mb4_unicode_ci = client;
+END$$
+DELIMITER ;
+CALL get_invoices_by_clientname('Vinte');
+
+-- two parameters (place holder)
+DROP PROCEDURE IF EXISTS get_payments;
+DELIMITER $$
+CREATE PROCEDURE get_payments(
+client_id INT,
+payment_method_id TINYINT)
+BEGIN
+-- 	deal with error
+	IF client_id <=0 THEN
+		SIGNAL SQLSTATE '22003' SET MESSAGE_TEXT = 'Invalid ID';
+	END IF;
+	SELECT * FROM payments p
+	WHERE p.client_id = IFNULL(client_id,p.client_id) 
+	AND p.payment_method = IFNULL(payment_method_id,p.payment_method);
+END$$
+DELIMITER ;
+CALL get_payments(0,1);
+
+-- our parameters
+DROP PROCEDURE IF EXISTS get_unpay;
+DELIMITER $$
+CREATE PROCEDURE get_unpay(
+	client_id INT,
+	OUT invoices_count INT,
+	OUT invoices_total DECIMAL(9,2)
+)
+BEGIN
+	SELECT COUNT(*), SUM(invoice_total)
+	INTO invoices_count,invoices_total
+	FROM invoices i
+	WHERE i.client_id = client_id AND payment_total = 0;	
+END$$
+DELIMITER ;
+
+SET @invoices_count = 0;
+SET @invoices_total = 0;
+CALL get_unpay(1,@invoices_count,@invoices_total);
+SELECT @invoices_count,@invoices_total;
+
+-- create FUNCTION
+DELIMITER $$
+CREATE FUNCTION risk(client_id INT)
+RETURNS INTEGER
+READS SQL DATA
+BEGIN
+	DECLARE risk_factor DECIMAL(9,2) DEFAULT 0;
+	DECLARE invoices_total DECIMAL(9,2);
+	DECLARE invoices_count INT;
+	
+	SELECT COUNT(*),SUM(invoice_total)
+	INTO invoices_count, invoices_total
+	FROM invoices i
+	WHERE i.client_id = client_id;
+	
+	SET risk_factor = invoices_total/invoices_count * 5;
+	RETURN risk_factor;
+END$$
+DELIMITER ;
+
+-- TRIGGER
+DROP TRIGGER IF EXISTS payments_after_insert;
+DELIMITER $$
+CREATE TRIGGER payments_after_insert
+	AFTER INSERT ON payments
+	FOR EACH ROW
+BEGIN
+	UPDATE invoices
+	SET payment_total = payment_total + NEW.amount
+	WHERE invoice_id = NEW.invoice_id;
+			
+	INSERT INTO payments_audit
+	VALUES (NEW.client_id, NEW.date, NEW.amount, 'Insert', NOW());
+END$$
+DELIMITER ;
+
+INSERT INTO payments
+VALUES (DEFAULT, 5,3,'2019-01-01',10,1);
+
+-- trigger after DELETE data
+DROP TRIGGER IF EXISTS payments_after_delete;
+DELIMITER $$
+CREATE TRIGGER payments_after_delete
+	AFTER DELETE ON payments
+	FOR EACH ROW
+BEGIN
+	UPDATE invoices
+	SET payment_total = payment_total - OLD.amount
+	WHERE invoice_id = OLD.invoice_id;
+	
+	INSERT INTO payments_audit
+	VALUES (OLD.client_id, OLD.date, OLD.amount, 'Delete', NOW());
+END$$
+DELIMITER ;
+
+DELETE FROM payments
+WHERE payment_id = 12;
+-- view TRIGGERS
+SHOW TRIGGERS LIKE 'payment%';
+
+-- create EVENT
+SHOW VARIABLES LIKE 'event%';
+SET GLOBAL event_scheduler = OFF;
+
+DELIMITER $$
+CREATE EVENT yearly_delete_audit_rows
+ON SCHEDULE
+-- 	AT '2019-05-01'
+	EVERY 1 YEAR STARTS '2019-05-01' ENDS '2029-05-01'
+DO BEGIN
+	DELETE FROM payments_audit
+	WHERE action_date < NOW() - INTERVAL 1 YEAR;
+-- 	DATE_ADD(NOW(),INTERVAL -1 YEAR)
+END$$
+DELIMITER ;
+-- show events
+SHOW EVENTS;
+ALTER EVENT yearly_delete_audit_rows DISABLE;
--- learning/mySQL/learn_store.sql
+++ learning/mySQL/learn_store.sql
@@ -1,3 +1,55 @@
+-- CROSS JOIN
+SELECT * FROM shippers, products;
+SELECT * FROM shippers CROSS JOIN products ORDER BY shippers.`name`;
+
+-- UNION
+SELECT order_id, order_date, 'Active' AS `status`
+FROM orders
+WHERE order_date >= '2019-01-01'
+UNION
+SELECT order_id, order_date, 'Archived' AS `s`
+FROM orders
+WHERE order_date < '2019-01-01';
+
+SELECT customer_id,first_name,points, 'Bronze' AS type
+FROM customers
+WHERE points < 2000
+UNION
+SELECT customer_id,first_name,points, 'Silver' AS type
+FROM customers
+WHERE points BETWEEN 2000 AND 3000
+UNION
+SELECT customer_id,first_name,points, 'Gold' AS type
+FROM customers
+WHERE points > 3000
+ORDER BY first_name;
+
+-- INSERT INTO insert rows
+INSERT INTO products
+VALUES (DEFAULT,'a',1,1),
+			 (DEFAULT,'b',1,1),
+			 (DEFAULT,'c',1,1);
+
+-- LAST_INSERT_ID() return the ID
+SELECT LAST_INSERT_ID();
+
+CREATE TABLE order_archive AS
+SELECT * FROM orders;
+
+UPDATE customers
+SET points = points + 50
+WHERE birth_date < '1990-01-01';
+
+UPDATE orders
+SET comments = 'Gold Customers'
+WHERE customer_id IN 
+(SELECT customer_id FROM customers WHERE points >=3000);
+
+-- DELETE rows
+DELETE FROM customers
+WHERE customer_id = 1;
+
+-- HAVING clause
 SELECT *,SUM(quantity * unit_price) AS total_pay FROM customers
 JOIN orders USING (customer_id)
 JOIN order_items USING (order_id)
@@ -57,3 +109,48 @@ FROM orders;
 SELECT CONCAT(first_name,' ',last_name) AS customer, IFNULL(phone,'Unknown') AS phone
 FROM customers;
 
+-- IF function
+SELECT
+product_id,
+`name`,
+(SELECT COUNT(*) FROM order_items WHERE product_id=p.product_id) AS orders,
+IF((SELECT orders)=1,'Once','Many times') AS frequency
+FROM products p
+HAVING orders > 0;
+-- another solution
+SELECT
+product_id,
+`name`,
+COUNT(*) AS orders,
+IF(COUNT(*)=1,'Once','Many times') AS frequency
+FROM products p
+JOIN order_items oi USING (product_id)
+GROUP BY product_id,`name`;
+
+-- CASE
+SELECT 
+customer_id,
+CONCAT(first_name,' ',last_name) AS customer,
+points,
+CASE
+	WHEN points < 2000 THEN
+		'Bronze'
+	WHEN points > 3000 THEN
+		'Gold'
+	ELSE
+		'Silver'
+END AS type
+FROM customers
+ORDER BY first_name;
+
+-- TRANSACTION
+START TRANSACTION;
+INSERT INTO orders (customer_id,order_date,`status`)
+VALUES (1,'2019-01-01',1);
+INSERT INTO order_items
+VALUES (LAST_INSERT_ID(),1,1,1);
+COMMIT;
+-- mannually rollback
+ROLLBACK;
+-- insert update delete are commited automatically
+SHOW VARIABLES LIKE 'autocommit';

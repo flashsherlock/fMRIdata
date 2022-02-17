d138e5e63efc90d6dd57d1cdda9b5157f08e8838
flashsherlock <sherlockholmes221b@163.com>
2022-02-17 22:34:52 +0800

    add boxplot for after_va

--- learning/plot.r
+++ learning/plot.r
@@ -68,15 +68,16 @@ vioplot <- function(data,condition, select){
 }
 
 # boxplot
-boxplot <- function(data,condition, select){
+boxplot <- function(data, con, select){
   # select data
   Violin_data <- subset(data,select = c("id","gender",select))
   Violin_data <- reshape2::melt(Violin_data, c("id","gender"),variable.name = "Task", value.name = "Score")
   Violin_data <- mutate(Violin_data,
                         test=ifelse(str_detect(Task,"pre"),"pre_test","post_test"),
-                        condition=ifelse(str_detect(Task,condition[1]),condition[1],condition[2]))
+                        condition=ifelse(str_detect(Task,con[1]),con[1],con[2]))
   
   Violin_data$test <- factor(Violin_data$test, levels = c("pre_test","post_test"),ordered = TRUE)
+  Violin_data$condition <- factor(Violin_data$condition, levels = c("fearful","happy"),ordered = F)
   # violinplot
   ggplot(data=Violin_data, aes(x=condition, y=Score)) + 
     geom_boxplot(aes(color=test),
@@ -88,7 +89,33 @@ boxplot <- function(data,condition, select){
                  jitter.height = 0,
                  dodge.width = 0.6,
                  seed = 1))+
-    # geom_line(aes(group = interaction(id,condition)), position = position_dodge(0.6))+
+    geom_line(aes(group = interaction(id,condition)), position = position_dodge(0.6))+
+    coord_cartesian(ylim = c(0,100))+
+    scale_fill_manual(values = c("#233b42","#65adc2")) + 
+    scale_y_continuous(breaks = c(1,seq(from=10, to=100, by=10)))
+}
+
+# boxplot with line
+boxplot_line <- function(data, con, select){
+  # select data
+  Violin_data <- subset(data,select = c("id","gender",select))
+  Violin_data <- reshape2::melt(Violin_data, c("id","gender"),variable.name = "Task", value.name = "Score")
+  Violin_data <- mutate(Violin_data,
+                        test=ifelse(str_detect(Task,"pre"),"pre_test","post_test"),
+                        condition=ifelse(str_detect(Task,con[1]),con[1],con[2]))
+  
+  Violin_data$test <- factor(Violin_data$test, levels = c("pre_test","post_test"),ordered = TRUE)
+  Violin_data$condition <- factor(Violin_data$condition, levels = c("fearful","happy"),ordered = F)
+  # violinplot
+  pd <- position_dodge(0.1)
+  ggplot(data=Violin_data, aes(x=test, y=Score)) + 
+    geom_boxplot(aes(color=test),
+                 outlier.shape = NA, fill=NA, width=0.5, position = position_dodge(0.6)) +
+    scale_color_manual(values=c("grey50","black"))+
+    geom_point(aes(group =id, fill=test), size = 0.5, shape=21, color = "gray",show.legend = F,
+               position = pd)+
+    geom_line(aes(group = id), color = "#e8e8e8", position = pd)+
+    facet_grid(~condition) +
     coord_cartesian(ylim = c(0,100))+
     scale_fill_manual(values = c("#233b42","#65adc2")) + 
     scale_y_continuous(breaks = c(1,seq(from=10, to=100, by=10)))
@@ -157,6 +184,7 @@ ggsave(paste0(data_dir,"violin_in_pm.eps"), width = 4, height = 3)
 ggsave(paste0(data_dir,"violin_in_pm.pdf"), width = 4, height = 3)
 
 boxplot(data_exp1,c("happy","fearful"),c("prehappy.va","prefear.va","afterhappy.va","afterfear.va"))
+boxplot_line(data_exp1,c("happy","fearful"),c("prehappy.va","prefear.va","afterhappy.va","afterfear.va"))
 ggsave(paste0(data_dir,"box_va_hf.pdf"), width = 4, height = 3)
 
 boxplot(data_exp1,c("happy","fearful"),c("prehappy.in","prefear.in","afterhappy.in","afterfear.in"))
@@ -167,3 +195,45 @@ ggsave(paste0(data_dir,"box_va_pm.pdf"), width = 4, height = 3)
 
 boxplot(data_exp1,c("plus","minus"),c("preplus.in","preminus.in","afterplus.in","afterminus.in"))
 ggsave(paste0(data_dir,"box_in_pm.pdf"), width = 4, height = 3)
+
+# select valence in post-test
+after_box_data <- subset(data_exp1,select = c("id","gender","pair","afterplus.va","afterminus.va"))
+after_box_data <- reshape2::melt(after_box_data, c("id","gender","pair"),variable.name = "Odor", value.name = "Score")
+after_box_data <- mutate(after_box_data, Odor=ifelse(str_detect(Odor,"plus"),"(+)-pinene","(-)-pinene"))
+after_box_data <- mutate(after_box_data, Condition=ifelse((Odor=="(+)-pinene" & pair=="+")|(Odor=="(-)-pinene" & pair=="-"),"happy","fearful"))
+
+after_box_data$Odor <- factor(after_box_data$Odor, levels = c("(+)-pinene","(-)-pinene"),ordered = F)
+after_box_data$pair <- factor(after_box_data$pair, levels = c("+","-"),labels = c("(+)-happy","(-)-happy"),ordered = F)
+after_box_data$Condtion <- factor(after_box_data$Condtion, levels = c("happy","fearful"),ordered = F)
+
+ggplot(data=after_box_data, aes(x=pair, y=Score)) + 
+  geom_boxplot(aes(color=Odor),
+               outlier.shape = NA, fill=NA, width=0.5, position = position_dodge(0.6)) +
+  scale_color_manual(values=c("grey50","black"))+
+  geom_point(aes(group = Odor, fill=Odor), size = 0.5, color = "gray",show.legend = F,
+             position = position_jitterdodge(
+               jitter.width = 0.2,
+               jitter.height = 0,
+               dodge.width = 0.6,
+               seed = 1))+
+  coord_cartesian(ylim = c(0,100))+
+  scale_fill_manual(values = c("#233b42","#65adc2")) + 
+  scale_y_continuous(breaks = c(1,seq(from=10, to=100, by=10)))
+ggsave(paste0(data_dir,"box_va_after.pdf"), width = 4, height = 3)
+
+ggplot(data=after_box_data, aes(x=pair, y=Score,fill=Condition)) + 
+  geom_boxplot(aes(color=Odor),
+               outlier.shape = NA, width=0.5, position = position_dodge(0.6)) +
+  scale_color_manual(values=c("grey50","black"))+
+  geom_point(aes(group = Odor), size = 0.5, color = "gray",show.legend = F,
+             position = position_jitterdodge(
+               jitter.width = 0.2,
+               jitter.height = 0,
+               dodge.width = 0.6,
+               seed = 1))+
+  coord_cartesian(ylim = c(0,100))+
+  guides(color = guide_legend(
+    order = 1,override.aes = list(fill = NA)))+
+  scale_fill_manual(values = c("red","blue")) + 
+  scale_y_continuous(breaks = c(1,seq(from=10, to=100, by=10)))
+ggsave(paste0(data_dir,"boxcolor_va_after.pdf"), width = 4, height = 3)

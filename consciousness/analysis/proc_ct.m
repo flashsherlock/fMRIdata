3bbb70ff95fad6f995883dcda0d0ee670f7c766c
flashsherlock <sherlockholmes221b@163.com>
2022-02-22 15:16:26 +0800

    generate and recon images for s08-s13

--- consciousness/analysis/proc_ct.m
+++ consciousness/analysis/proc_ct.m
@@ -1,7 +1,7 @@
 %% use spm to reorient CT image
 % spm will clear workspace if quit
 % workdir = pwd;
-subjID = 's07';
+subjID = 's13';
 filepath=['/Volumes/WD_D/gufei/consciousness/electrode/use/' subjID];
 cd(filepath);
 copy = ['cp ' subjID '_CT.nii ' subjID '_CT_acpc.nii'];
@@ -10,7 +10,7 @@ spm fmri
 % mfilename('fullpath')
 % cd(workdir);
 %% set path
-subjID = 's06';
+subjID = 's13';
 filepath=['/Volumes/WD_D/gufei/consciousness/electrode/use/' subjID];
 %% load CT image
 ct = ft_read_mri([filepath '/' subjID '_CT.nii']);
--- consciousness/analysis/recon_all.m
+++ consciousness/analysis/recon_all.m
@@ -1,5 +1,6 @@
-% the filename con't contain '-',so change to '_'
-sub		   = 's07';
+% the filename should not contain '-',so change to '_'
+for subi = 11:13
+sub		   = sprintf('s%02d',subi);
 fshome     = '/Applications/freesurfer/7.1.1/';
 subdir     = ['/Volumes/WD_D/gufei/consciousness/electrode/use/' sub];
 mrfile     = [subdir '/' sub '_MRI_acpc.nii'];
@@ -8,3 +9,4 @@ system(['export FREESURFER_HOME=' fshome '; ' ...
 'source $FREESURFER_HOME/SetUpFreeSurfer.sh; ' ...
 'mri_convert -c -oc 0 0 0 ' mrfile ' ' [subdir '/tmp.nii'] '; ' ...
 'recon-all -i ' [subdir '/tmp.nii'] ' -s ' sub ' -sd ' subdir ' -all -parallel -threads 26'])
+end

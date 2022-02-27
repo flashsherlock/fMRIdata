3bdc23212d330c092e44dfd15eb7ef36828f9593
flashsherlock <sherlockholmes221b@163.com>
2022-02-27 23:13:56 +0800

    compute zscore for powspec

--- monkey/phy/roi_powspec.m
+++ monkey/phy/roi_powspec.m
@@ -1,6 +1,6 @@
 %% load and reorganize data
-data_dir='/Volumes/WD_D/gufei/monkey_data/yuanliu/';
-pic_dir=[data_dir 'pic/odorresp_roi/'];
+data_dir='/Volumes/WD_D/gufei/monkey_data/yuanliu/merge2monkey/';
+pic_dir=[data_dir 'pic/powerspec/'];
 if ~exist(pic_dir,'dir')
     mkdir(pic_dir);
 end
@@ -18,11 +18,9 @@ trl_type = 'odor';
 roi_num=size(cur_level_roi,1);
 odor_num=7;
 %% power spectrum analysis
-spectr_resp=cell(roi_num,odor_num);
-spectr_lfp=spectr_resp;
+spectr_resp_all=cell(roi_num,1);
+spectr_lfp_all=spectr_resp_all;
 for roi_i=1:roi_num
-    for odor_i=1:odor_num
-    cur_roi=cur_level_roi{roi_i,1};
     lfp=roi_lfp{roi_i};
     resp=roi_resp{roi_i};
     % select time
@@ -35,15 +33,49 @@ for roi_i=1:roi_num
     cfg.output  = 'pow';
     cfg.method  = 'mtmfft';
     cfg.taper   = 'hanning';
-    if odor_i==7
-        cfg.trials  = find(lfp.trialinfo~=6);
-    else
-        cfg.trials  = find(lfp.trialinfo==odor_i);
-    end
-    % cfgtf.foi = logspace(log10(1),log10(200),51);
+    cfg.keeptrials = 'yes';
     cfg.foilim = [0.1 80];
-    spectr_resp{roi_i,odor_i}  = ft_freqanalysis(cfg, resp);
-    spectr_lfp{roi_i,odor_i}  = ft_freqanalysis(cfg, lfp);
+    spectr_resp_all{roi_i}  = ft_freqanalysis(cfg, resp);
+    spectr_lfp_all{roi_i}  = ft_freqanalysis(cfg, lfp);
+end
+%% average each condition
+spectr_resp=cell(roi_num,odor_num);
+spectr_lfp=spectr_resp;
+for roi_i=1:roi_num
+    for odor_i=1:odor_num
+        lfp = spectr_lfp_all{roi_i};
+        resp = spectr_resp_all{roi_i};
+        % frequency spectrum
+        cfg         = [];
+        cfg.avgoverrpt =  'yes';
+        if odor_i==7
+            cfg.trials  = find(lfp.trialinfo~=6);
+        else
+            cfg.trials  = find(lfp.trialinfo==odor_i);
+        end
+        spectr_resp{roi_i,odor_i} = ft_selectdata(cfg, resp);
+        spectr_lfp{roi_i,odor_i} = ft_selectdata(cfg, lfp);
+    end
+end
+%% zscore
+spectr_respz=cell(roi_num,odor_num);
+spectr_lfpz=spectr_respz;
+for roi_i=1:roi_num
+    for odor_i=1:odor_num
+        lfp = spectr_lfp_all{roi_i};
+        lfp.powspctrm = zscore(lfp.powspctrm,0,1);
+        resp = spectr_resp_all{roi_i};
+        resp.powspctrm = zscore(resp.powspctrm,0,1);
+        % frequency spectrum
+        cfg         = [];
+        cfg.avgoverrpt =  'yes';
+        if odor_i==7
+            cfg.trials  = find(lfp.trialinfo~=6);
+        else
+            cfg.trials  = find(lfp.trialinfo==odor_i);
+        end
+        spectr_respz{roi_i,odor_i} = ft_selectdata(cfg, resp);
+        spectr_lfpz{roi_i,odor_i} = ft_selectdata(cfg, lfp);
     end
 end
 %% plot
@@ -64,5 +96,21 @@ legend('Ind','Iso_l','Iso_h','Peach','Banana','Air','Odor')
 xlabel('Frequency (Hz)')
 ylabel('Power (\mu V^2)')
 end
-save([data_dir 'powspec_odor_8s_0.1_80hz.mat'],'spectr_lfp','spectr_resp')
+%% plot zscore
+for roi_i=1:roi_num
+figure;
+hold on;
+for odor_i=1:odor_num
+    plot(spectr_lfpz{roi_i,odor_i}.freq, smooth(mean(spectr_lfpz{roi_i,odor_i}.powspctrm,1),32),'Color',hex2rgb(colors{odor_i}),'linewidth', 2)
+end
+set(gca,'xlim',[1 75]);
+title(cur_level_roi{roi_i,1})
+legend('Ind','Iso_l','Iso_h','Peach','Banana','Air','Odor')
+xlabel('Frequency (Hz)')
+ylabel('ZPower')
+saveas(gcf, [pic_dir cur_level_roi{roi_i,1} '-zpower', '.fig'], 'fig')
+saveas(gcf, [pic_dir cur_level_roi{roi_i,1} '-zpower', '.png'], 'png')
+close all
+end
+% save([data_dir 'powspec_odor_8s_0.1_80hz.mat'],'spectr_lfp','spectr_resp')
 % save([data_dir 'level3_position_2monkey.mat'],'cur_level_roi');

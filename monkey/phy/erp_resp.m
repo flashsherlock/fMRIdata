dfb9d3d7f979a76fef7f877f47b28e69f549792d
flashsherlock <sherlockholmes221b@163.com>
2022-03-01 20:43:26 +0800

    add permutation test for correlation

--- monkey/phy/erp_resp.m
+++ monkey/phy/erp_resp.m
@@ -24,9 +24,9 @@ cfg.trials = find(resp.trialinfo==condition);
 resavg=ft_timelockanalysis(cfg, resp);
 %% show low frequency signal
 cfg=[];
-cfg.bpfilter = 'yes';
-cfg.bpfilttype = 'fir';
-cfg.bpfreq = [0.1 0.6];
+cfg.lpfilter = 'yes';
+cfg.lpfilttype = 'fir';
+cfg.lpfreq = 0.6;
 cfg.trials = find(lfp.trialinfo==condition);
 lfp_l = ft_preprocessing(cfg,lfp);
 % cfg          = [];
@@ -52,22 +52,56 @@ cfg.trials = find(erp_blc.trialinfo==condition);
 cfg.avgoverrpt =  'yes';
 erp_blc=ft_selectdata(cfg,erp_blc);
 % compute correlation
-time_range = [-1 4];
+time_range = [-1 3];
 select=resavg.time>=0&resavg.time<=time_range(2);
-r4=corr(resavg.avg(select)',squeeze(mean(erp_blc.trial(:,select),1))');
-select=resavg.time>=time_range(1)&resavg.time<=time_range(2);
-r7=corr(resavg.avg',squeeze(mean(erp_blc.trial,1))');
+r3=corr(resavg.avg(select)',squeeze(mean(erp_blc.trial(:,select),1))');
+% select=resavg.time>=time_range(1)&resavg.time<=time_range(2);
+% r7=corr(resavg.avg',squeeze(mean(erp_blc.trial,1))');
 
-figure
+%% permutation test
+nper=1000;
+r3_per=zeros(1,nper);
+erp_per=zeros(length(erp.trialinfo),3001);
+for per_i=1:nper
+    r=randi([1 length(erp.time)-3000],length(erp.trialinfo),1);
+%     r=100*(ceil(r/100)-1)+1;
+    for trial_i=1:length(erp.trialinfo)
+        erp_per(trial_i,:)=erp.trial(trial_i,1,r(trial_i):r(trial_i)+3000);
+    end
+    r3_per(per_i)=corr(resavg.avg(select)',mean(erp_per,1)');
+end
+r3_per=atanh(r3_per);
+cut1=prctile(r3_per,97.5);
+cut2=prctile(r3_per,2.5);
+true=atanh(r3);
+sig='';
+if true>cut1 || true<cut2
+    sig='*';
+end
+%% plot
+figure('position',[20 20 1000 450])
+subplot(1,2,1)
 hold on
 plot(erp_blc.time,1000*erp_blc.trial,'b','LineWidth',1.5)
 xlabel('Time (s)')
-ylabel('Voltage (μV)')
+ylabel('Voltage')
 yyaxis right
 plot(resavg.time,resavg.avg,'k','LineWidth',1.5)
 set(gca,'xlim',time_range,'ytick',[]);
-title([cur_roi ' 0-4s:' num2str(r4) ' -1-4s:' num2str(r7)])
-hold off
+set(gca,'FontSize',16);
+% permutation test
+subplot(1,2,2)
+hold on
+hist(r3_per,50)
+set(gca,'xlim',[-3 3],'ylim',[0 60]);
+plot([cut1 cut1],get(gca,'Ylim'),'b','linewidth',1.5)
+plot([cut2 cut2],get(gca,'Ylim'),'b','linewidth',1.5)
+plot(true,1,'r.','MarkerSize',30)
+text(-2.8,50,[sprintf('R: %0.2f',round(r3,2)) sig],'Fontsize',16)
+xlabel('Z')
+ylabel('Iterations')
+set(gca,'FontSize',16);
+suptitle([cur_roi ' 0-3s:' num2str(r3)])
 saveas(gcf, [pic_dir cur_roi],'png')
 close all
 % figure

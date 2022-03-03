1aa91447e8556964437dcf1d48a09635abcc39d3
flashsherlock <sherlockholmes221b@163.com>
2022-03-03 13:39:10 +0800

    replicate lfp_odorresp on merge2monkey data

--- monkey/phy/lfp_odorresp.m
+++ monkey/phy/lfp_odorresp.m
@@ -1,38 +1,22 @@
 %% load data
-data_dir='/Volumes/WD_D/gufei/monkey_data/yuanliu/rm035_ane/mat/';
-pic_dir=[data_dir 'pic/odorresp/'];
+data_dir='/Volumes/WD_D/gufei/monkey_data/yuanliu/merge2monkey/';
+pic_dir=[data_dir 'pic/lfp_odorresp/'];
 if ~exist(pic_dir,'dir')
     mkdir(pic_dir);
 end
-dates={'200731','200807','200814','200820','200828'};
-for i_date=1:length(dates)
-cur_date=dates{i_date};
-data=load([data_dir cur_date '_rm035_ane.mat']);
-%% cut to trials
-lfp=cell(1,length(data.lfp));
-resp=lfp;
-for i=1:length(data.lfp)
-cfg=[];
-cfg.trl=data.trl(i).odorresp;
-lfp{i} = ft_redefinetrial(cfg, data.lfp{i});
-resp{i} = ft_redefinetrial(cfg, data.bioresp{i});
-end
-%% append data
-cfg=[];
-cfg.keepsampleinfo='no';
-lfp = ft_appenddata(cfg,lfp{:});
-resp = ft_appenddata(cfg,resp{:});
-% remove trials containing nan values
-cfg=[];
-cfg.trials=~(cellfun(@(x) any(any(isnan(x),2)),lfp.trial)...
-    |cellfun(@(x) any(any(isnan(x),2)),resp.trial));
-resp=ft_selectdata(cfg,resp);
-% select channel and trial
-channel=0;
-if channel~=0
-cfg.channel=strcat('WB',num2str(channel));
-end
-lfp=ft_selectdata(cfg,lfp);
+%% generate data
+level = 3;
+trl_type = 'odorresp';
+% combine 2 monkeys
+[roi_lfp,roi_resp,cur_level_roi] = save_merge_2monkey(level,trl_type);
+% get number of roi
+roi_num=size(cur_level_roi,1);
+odor_num=7;
+%% analyze
+for roi_i=2:roi_num
+cur_roi=cur_level_roi{roi_i,1};
+lfp=roi_lfp{roi_i};
+resp=roi_resp{roi_i};
 %% time frequency analysis
 freq_range = [1.5 200];
 time_range = [-1 7.5];
@@ -49,12 +33,6 @@ cfgtf.taper      = 'hanning';
 cfgtf.output     = 'pow';
 cfgtf.keeptrials = 'yes';
 freq_sep = ft_freqanalysis(cfgtf, lfp);
-% average across channels
-cfg=[];
-cfg.avgoverchan =  'yes';
-cfg.frequency=freq_range;
-cfg.latency=time_range;
-freq_sep = ft_selectdata(cfg,freq_sep);
 %% odor and air 2 cycles
 freq=cell(1,7);
 freq_blc=freq;
@@ -109,30 +87,30 @@ threshmean = realmean;
 threshmean(abs(zmap)<=norminv(1-voxel_pval/2))=0;
 un_zmapthresh=abs(sign(threshmean));
 
-%cluster correction
-for permi = 1:n_permutes
-    % for cluster correction, apply uncorrected threshold and get maximum cluster sizes
-    fakecorrsz = squeeze((permuted_vals(permi,:,:)-mean(permuted_vals)) ./ std(permuted_vals) );
-    fakecorrsz(abs(fakecorrsz)<norminv(1-voxel_pval/2))=0;
-    % get number of elements in largest supra-threshold cluster
-    clustinfo = bwconncomp(fakecorrsz);
-    max_clust_info(permi) = max([ 0 cellfun(@numel,clustinfo.PixelIdxList) ]); % the zero accounts for empty maps
-    % using cellfun here eliminates the need for a slower loop over cells
-end
-% apply cluster-level corrected threshold
-zmapthresh = zmap;
-% uncorrected pixel-level threshold
-zmapthresh(abs(zmapthresh)<norminv(1-voxel_pval/2))=0;
-% find islands and remove those smaller than cluster size threshold
-clustinfo = bwconncomp(zmapthresh);
-clust_info = cellfun(@numel,clustinfo.PixelIdxList);
-clust_threshold = prctile(max_clust_info,100-cluster_pval*100);
-% identify clusters to remove
-whichclusters2remove = find(clust_info<clust_threshold);
-% remove clusters
-for i_r=1:length(whichclusters2remove)
-    zmapthresh(clustinfo.PixelIdxList{whichclusters2remove(i_r)})=0;
-end
+% %cluster correction
+% for permi = 1:n_permutes
+%     % for cluster correction, apply uncorrected threshold and get maximum cluster sizes
+%     fakecorrsz = squeeze((permuted_vals(permi,:,:)-mean(permuted_vals)) ./ std(permuted_vals) );
+%     fakecorrsz(abs(fakecorrsz)<norminv(1-voxel_pval/2))=0;
+%     % get number of elements in largest supra-threshold cluster
+%     clustinfo = bwconncomp(fakecorrsz);
+%     max_clust_info(permi) = max([ 0 cellfun(@numel,clustinfo.PixelIdxList) ]); % the zero accounts for empty maps
+%     % using cellfun here eliminates the need for a slower loop over cells
+% end
+% % apply cluster-level corrected threshold
+% zmapthresh = zmap;
+% % uncorrected pixel-level threshold
+% zmapthresh(abs(zmapthresh)<norminv(1-voxel_pval/2))=0;
+% % find islands and remove those smaller than cluster size threshold
+% clustinfo = bwconncomp(zmapthresh);
+% clust_info = cellfun(@numel,clustinfo.PixelIdxList);
+% clust_threshold = prctile(max_clust_info,100-cluster_pval*100);
+% % identify clusters to remove
+% whichclusters2remove = find(clust_info<clust_threshold);
+% % remove clusters
+% for i_r=1:length(whichclusters2remove)
+%     zmapthresh(clustinfo.PixelIdxList{whichclusters2remove(i_r)})=0;
+% end
 
 % plot by contourf
 figure;
@@ -154,18 +132,18 @@ end
 resavg=ft_timelockanalysis(cfg, resp);
 hold on
 % uncorrected
-% contour(freq_blc{i}.time,freq_blc{i}.freq,un_zmapthresh,1,'linecolor','k','LineWidth',1)
+contour(freq_blc{i}.time,freq_blc{i}.freq,un_zmapthresh,1,'linecolor','k','LineWidth',1)
 % cluster based correction
-contour(freq_blc{i}.time,freq_blc{i}.freq,abs(sign(zmapthresh)),1,'linecolor','k','LineWidth',1)
+% contour(freq_blc{i}.time,freq_blc{i}.freq,abs(sign(zmapthresh)),1,'linecolor','k','LineWidth',1)
 set(gca, 'yminortick', 'off');
 plot(bs,1.5*ones(1,100),'k','LineWidth',5)
 yyaxis right
 plot(resavg.time,resavg.avg,'k','LineWidth',1.5)
 set(gca,'xlim',time_range,'ytick',[]);
-title([cur_date '-' num2str(channel) '-odorresp' num2str(i) '-oddtrial'])
+title([cur_roi '-odorresp' num2str(i) '-oddtrial'])
 hold off
-saveas(gcf, [pic_dir cur_date '-' num2str(channel) '-odorresp' num2str(i) '-oddtrial'], 'fig')
-saveas(gcf, [pic_dir cur_date '-' num2str(channel) '-odorresp' num2str(i) '-oddtrial'], 'png')
+saveas(gcf, [pic_dir cur_roi '-odorresp' num2str(i) '-oddtrial', '.fig'], 'fig')
+saveas(gcf, [pic_dir cur_roi '-odorresp' num2str(i) '-oddtrial', '.png'], 'png')
 close all
 end
 %% select -1-4s
@@ -227,30 +205,30 @@ threshmean = realmean;
 threshmean(abs(zmap)<=norminv(1-voxel_pval/2))=0;
 un_zmapthresh=abs(sign(threshmean));
 
-%cluster correction
-for permi = 1:n_permutes
-    % for cluster correction, apply uncorrected threshold and get maximum cluster sizes
-    fakecorrsz = squeeze((permuted_vals(permi,:,:)-mean(permuted_vals)) ./ std(permuted_vals) );
-    fakecorrsz(abs(fakecorrsz)<norminv(1-voxel_pval/2))=0;
-    % get number of elements in largest supra-threshold cluster
-    clustinfo = bwconncomp(fakecorrsz);
-    max_clust_info(permi) = max([ 0 cellfun(@numel,clustinfo.PixelIdxList) ]); % the zero accounts for empty maps
-    % using cellfun here eliminates the need for a slower loop over cells
-end
-% apply cluster-level corrected threshold
-zmapthresh = zmap;
-% uncorrected pixel-level threshold
-zmapthresh(abs(zmapthresh)<norminv(1-voxel_pval/2))=0;
-% find islands and remove those smaller than cluster size threshold
-clustinfo = bwconncomp(zmapthresh);
-clust_info = cellfun(@numel,clustinfo.PixelIdxList);
-clust_threshold = prctile(max_clust_info,100-cluster_pval*100);
-% identify clusters to remove
-whichclusters2remove = find(clust_info<clust_threshold);
-% remove clusters
-for i_r=1:length(whichclusters2remove)
-    zmapthresh(clustinfo.PixelIdxList{whichclusters2remove(i_r)})=0;
-end
+% %cluster correction
+% for permi = 1:n_permutes
+%     % for cluster correction, apply uncorrected threshold and get maximum cluster sizes
+%     fakecorrsz = squeeze((permuted_vals(permi,:,:)-mean(permuted_vals)) ./ std(permuted_vals) );
+%     fakecorrsz(abs(fakecorrsz)<norminv(1-voxel_pval/2))=0;
+%     % get number of elements in largest supra-threshold cluster
+%     clustinfo = bwconncomp(fakecorrsz);
+%     max_clust_info(permi) = max([ 0 cellfun(@numel,clustinfo.PixelIdxList) ]); % the zero accounts for empty maps
+%     % using cellfun here eliminates the need for a slower loop over cells
+% end
+% % apply cluster-level corrected threshold
+% zmapthresh = zmap;
+% % uncorrected pixel-level threshold
+% zmapthresh(abs(zmapthresh)<norminv(1-voxel_pval/2))=0;
+% % find islands and remove those smaller than cluster size threshold
+% clustinfo = bwconncomp(zmapthresh);
+% clust_info = cellfun(@numel,clustinfo.PixelIdxList);
+% clust_threshold = prctile(max_clust_info,100-cluster_pval*100);
+% % identify clusters to remove
+% whichclusters2remove = find(clust_info<clust_threshold);
+% % remove clusters
+% for i_r=1:length(whichclusters2remove)
+%     zmapthresh(clustinfo.PixelIdxList{whichclusters2remove(i_r)})=0;
+% end
 
 % plot by contourf
 figure;
@@ -272,18 +250,18 @@ end
 resavg=ft_timelockanalysis(cfg, resp);
 hold on
 % uncorrected
-% contour(freq_blc{i}.time,freq_blc{i}.freq,un_zmapthresh,1,'linecolor','k','LineWidth',1)
+contour(freq_blc{i}.time,freq_blc{i}.freq,un_zmapthresh,1,'linecolor','k','LineWidth',1)
 % cluster based correction
-contour(freq_blc{i}.time,freq_blc{i}.freq,abs(sign(zmapthresh)),1,'linecolor','k','LineWidth',1)
+% contour(freq_blc{i}.time,freq_blc{i}.freq,abs(sign(zmapthresh)),1,'linecolor','k','LineWidth',1)
 set(gca, 'yminortick', 'off');
 plot(bs,1.5*ones(1,100),'k','LineWidth',5)
 yyaxis right
 plot(resavg.time,resavg.avg,'k','LineWidth',1.5)
 set(gca,'xlim',time_range,'ytick',[]);
-title([cur_date '-' num2str(channel) '-odorresp' num2str(i)])
+title([cur_roi '-odorresp' num2str(i)])
 hold off
-saveas(gcf, [pic_dir cur_date '-' num2str(channel) '-odorresp' num2str(i)], 'fig')
-saveas(gcf, [pic_dir cur_date '-' num2str(channel) '-odorresp' num2str(i)], 'png')
+saveas(gcf, [pic_dir cur_roi '-odorresp' num2str(i), '.fig'], 'fig')
+saveas(gcf, [pic_dir cur_roi '-odorresp' num2str(i), '.png'], 'png')
 close all
 end
 
@@ -389,18 +367,18 @@ end
 resavg=ft_timelockanalysis(cfg, resp);
 hold on
 % uncorrected
-% contour(freq_blc{i}.time,freq_blc{i}.freq,un_zmapthresh,1,'linecolor','k','LineWidth',1)
+contour(freq_blc{i}.time,freq_blc{i}.freq,un_zmapthresh,1,'linecolor','k','LineWidth',1)
 % cluster based correction
-contour(freq_blc{i}.time,freq_blc{i}.freq,abs(sign(zmapthresh)),1,'linecolor','k','LineWidth',1)
+% contour(freq_blc{i}.time,freq_blc{i}.freq,abs(sign(zmapthresh)),1,'linecolor','k','LineWidth',1)
 set(gca, 'yminortick', 'off');
 plot(bs,1.5*ones(1,100),'k','LineWidth',5)
 yyaxis right
 plot(resavg.time,resavg.avg,'k','LineWidth',1.5)
 set(gca,'xlim',time_range,'ytick',[]);
-title([cur_date '-' num2str(channel) '-odorresp' num2str(i) '-vs-air'])
+title([cur_roi '-odorresp' num2str(i) '-vs-air'])
 hold off
-saveas(gcf, [pic_dir cur_date '-' num2str(channel) '-odorresp' num2str(i) '_vs_air'], 'fig')
-saveas(gcf, [pic_dir cur_date '-' num2str(channel) '-odorresp' num2str(i) '_vs_air'], 'png')
+saveas(gcf, [pic_dir cur_roi '-odorresp' num2str(i) '_vs_air', '.fig'], 'fig')
+saveas(gcf, [pic_dir cur_roi '-odorresp' num2str(i) '_vs_air', '.png'], 'png')
 close all
 
 figure;
@@ -420,10 +398,10 @@ plot(bs,1.5*ones(1,100),'k','LineWidth',5)
 yyaxis right
 plot(resavg.time,resavg.avg,'k','LineWidth',1.5)
 set(gca,'xlim',time_range,'ytick',[]);
-title([cur_date '-' num2str(channel) '-odorresp' num2str(i) '-vs-air-t'])
+title([cur_roi '-odorresp' num2str(i) '-vs-air-t'])
 hold off
-saveas(gcf, [pic_dir cur_date '-' num2str(channel) '-odorresp' num2str(i) '_vs_air_t'], 'fig')
-saveas(gcf, [pic_dir cur_date '-' num2str(channel) '-odorresp' num2str(i) '_vs_air_t'], 'png')
+saveas(gcf, [pic_dir cur_roi '-odorresp' num2str(i) '_vs_air_t', '.fig'], 'fig')
+saveas(gcf, [pic_dir cur_roi '-odorresp' num2str(i) '_vs_air_t', '.png'], 'png')
 close all
 
 % apply uncorrected threshold
@@ -447,10 +425,10 @@ plot(bs,1.5*ones(1,100),'k','LineWidth',5)
 yyaxis right
 plot(resavg.time,resavg.avg,'k','LineWidth',1.5)
 set(gca,'xlim',time_range,'ytick',[]);
-title([cur_date '-' num2str(channel) '-odorresp' num2str(i) '-vs-air-tun'])
+title([cur_roi '-odorresp' num2str(i) '-vs-air-tun'])
 hold off
-saveas(gcf, [pic_dir cur_date '-' num2str(channel) '-odorresp' num2str(i) '_vs_air_tun'], 'fig')
-saveas(gcf, [pic_dir cur_date '-' num2str(channel) '-odorresp' num2str(i) '_vs_air_tun'], 'png')
+saveas(gcf, [pic_dir cur_roi '-odorresp' num2str(i) '_vs_air_tun', '.fig'], 'fig')
+saveas(gcf, [pic_dir cur_roi '-odorresp' num2str(i) '_vs_air_tun', '.png'], 'png')
 close all
 
 % 
@@ -553,10 +531,10 @@ plot(bs,1.5*ones(1,100),'k','LineWidth',5)
 yyaxis right
 plot(resavg.time,resavg.avg,'k','LineWidth',1.5)
 set(gca,'xlim',time_range,'ytick',[]);
-title([cur_date '-' num2str(channel) '-odorresp-valence-t'])
+title([cur_roi '-odorresp-valence-t'])
 hold off
-saveas(gcf, [pic_dir cur_date '-' num2str(channel) '-odorresp-valence-t'], 'fig')
-saveas(gcf, [pic_dir cur_date '-' num2str(channel) '-odorresp-valence-t'], 'png')
+saveas(gcf, [pic_dir cur_roi '-odorresp-valence-t', '.fig'], 'fig')
+saveas(gcf, [pic_dir cur_roi '-odorresp-valence-t', '.png'], 'png')
 close all
 
 % apply uncorrected threshold
@@ -580,10 +558,10 @@ plot(bs,1.5*ones(1,100),'k','LineWidth',5)
 yyaxis right
 plot(resavg.time,resavg.avg,'k','LineWidth',1.5)
 set(gca,'xlim',time_range,'ytick',[]);
-title([cur_date '-' num2str(channel) '-odorresp-valence-tun'])
+title([cur_roi '-odorresp-valence-tun'])
 hold off
-saveas(gcf, [pic_dir cur_date '-' num2str(channel) '-odorresp-valence-tun'], 'fig')
-saveas(gcf, [pic_dir cur_date '-' num2str(channel) '-odorresp-valence-tun'], 'png')
+saveas(gcf, [pic_dir cur_roi '-odorresp-valence-tun', '.fig'], 'fig')
+saveas(gcf, [pic_dir cur_roi '-odorresp-valence-tun', '.png'], 'png')
 close all
 
 % % 

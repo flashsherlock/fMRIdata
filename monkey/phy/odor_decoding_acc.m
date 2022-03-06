a5cbee4eebd06143d8305c829838c64d579541d7
flashsherlock <sherlockholmes221b@163.com>
2022-03-06 22:42:30 +0800

    add a function to analysis decoding results

--- monkey/phy/odor_decoding_acc.m
+++ monkey/phy/odor_decoding_acc.m
@@ -1,4 +1,4 @@
-function [acc, rois]=odor_decoding_acc(results_odor)
+function [acc, rois, chance]=odor_decoding_acc(results_odor)
 %% calculate acc
 % row-roi column-repeat
 repeat_num = size(results_odor,2);
@@ -6,10 +6,12 @@ roi_num = size(results_odor,1);
 % return acc and rois
 acc=zeros(roi_num,repeat_num);
 rois=cell(roi_num,1);
+% return chance level
+chance=results_odor{1,1}.accuracy_minus_chance.chancelevel;
 for roi_i=1:size(results_odor,1)
     % roi label only in the first column
     roi=strsplit(results_odor{roi_i,1}.analysis,'_');
-    rois{roi_i}=roi{1};
+    rois{roi_i}=roi{1};    
     % get acc fron results
     for repeat_i=1:size(results_odor,2)
         % acc minus chance
---
+++ monkey/phy/odor_decoding_results.m
@@ -0,0 +1,50 @@
+function odor_decoding_results(result_file)
+    %% load and reorganize data
+    data_dir='/Volumes/WD_D/gufei/monkey_data/yuanliu/merge2monkey/';
+    pic_dir=[data_dir 'pic/decoding/'];
+    % result_file='decoding_results_odor.mat';
+    load([pic_dir result_file]);
+    %% analyze results
+    for condition_i = 1:size(results,1)
+        condition = results{condition_i,1};    
+        % save results for this condition
+        results_odor = results{condition_i,2};
+        repeat_num = size(results_odor,2);
+        roi_num = size(results_odor,1);
+        % plot acc
+        [acc, rois, chance]=odor_decoding_acc(results_odor);
+        % ttest
+        [h,p,ci,stats]=ttest(acc',chance);    
+        % bar plot
+        figure('position',[20 20 800 500])
+        hold on
+        bar(mean(acc,2))
+        xnum = get(gca,'Xlim');
+        plot(xnum,[chance chance],'r','linestyle','--','LineWidth',2)
+        % add text
+        for roi_i=1:roi_num
+            % significance
+            sig='';
+            if p(roi_i)<0.001
+                sig='***';
+            elseif p(roi_i)<0.01
+                sig='**';
+            elseif p(roi_i)<0.05
+                sig='*';
+            end
+            % accuracy star p-value
+            text(roi_i,80,sprintf('%0.2f\n%s\n%0.3f',round(mean(acc(roi_i,:)),2),sig,round(p(roi_i),3)),...
+                'Fontsize',12, 'VerticalAlignment','bottom', 'HorizontalAlignment','center')
+        end
+        set(gca,'ylim',[0 100])
+        ylabel('ACC')
+        xlabel('ROI')
+        % set x label
+        set(gca,'XTick',1:xnum(2)-1)
+        set(gca,'XTickLabel',rois)
+        % title avoid latex
+        title([condition '   ' num2str(repeat_num) 'repeats   ' result_file],'Interpreter','none')
+        % save figure
+        saveas(gcf, [pic_dir condition '_' num2str(repeat_num) '.png'],'png')
+    end
+end

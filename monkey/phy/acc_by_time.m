393b71e3f854d23b36321beb58f03434c49206ac
flashsherlock <sherlockholmes221b@163.com>
2022-03-10 11:01:20 +0800

    decoding by time window

--- monkey/phy/acc_by_time.m
+++ monkey/phy/acc_by_time.m
@@ -4,16 +4,21 @@ pic_dir=[data_dir 'pic/decoding/'];
 if ~exist(pic_dir,'dir')
     mkdir(pic_dir);
 end
-time_bin={'0.2-0.8s','0.8-1.4s','1.4-2s'};
+times=[0:0.2:1.4];
+time_win=0.6;
+tnum=50;
+time_bin=cell(1,length(times));
+% time_bin={'0.2-0.8s','0.8-1.4s','1.4-2s'};
 conditions={'5odor','vaodor','airodor'};
-% condition acc_matrix roi-repeats-time_bin
-results_bytime=cell(length(conditions),3);
+% condition  chance acc_matrix roi-repeats-time_bin
+results_bytime=cell(length(conditions),4);
 results_bytime(:,1)=conditions;
 
 % find data for each timebin
-for time_i=1:length(time_bin)
-    time=time_bin{time_i};
-    load([pic_dir 'linear_' time '/decoding_results_linear_' time '.mat']);
+for time_i=1:length(times)
+    time=[num2str(times(time_i)) '-' num2str(times(time_i)+time_win) 's'];
+    time_bin{time_i}=time;
+    load([pic_dir 'decoding_results' num2str(tnum) '_linear_' time '.mat']);
     % find each condition
     for condition_i = 1:length(conditions)
         condition = conditions{condition_i};
@@ -23,7 +28,8 @@ for time_i=1:length(time_bin)
         roi_num = size(results_odor,1);
         % plot acc
         [acc, rois, chance]=odor_decoding_acc(results_odor);
-        results_bytime{condition_i,2}=cat(3,results_bytime{condition_i,2},acc);
+        results_bytime{condition_i,3}=cat(3,results_bytime{condition_i,3},acc);
+        results_bytime{condition_i,2}=chance;
         % ttest
         % [h,p,ci,stats]=ttest(acc',chance);
     end
@@ -33,13 +39,35 @@ end
 % calculate mean acc and plot
 for condition_i = 1:length(conditions)
     condition = results_bytime{condition_i,1};
-    results_bytime{condition_i,3} = squeeze(mean(results_bytime{condition_i,2},2));
+    % roi-timebin
+    results_bytime{condition_i,4} = squeeze(mean(results_bytime{condition_i,3},2));
     % plot
     figure
-    bar(results_bytime{condition_i,3})
+    plot(results_bytime{condition_i,4})
     title(condition)
     legend(time_bin)
     ylabel('ACC')
     xlabel('ROI')
     set(gca,'XTickLabel',rois)
+end
+
+line_wid=1.5;
+roi_select=[3:5 7:11 13];
+roi_select=[4 7 9 11];
+roi_select=[4 5 7:9 11];
+for condition_i = 1:length(conditions)
+    condition = results_bytime{condition_i,1};
+    % line plot
+    figure
+    data_select=results_bytime{condition_i,4};
+    data_select=data_select(roi_select,:);
+    plot(data_select','linewidth', line_wid)
+    % plot chance
+    chance=results_bytime{condition_i,2};
+    title(condition)
+    legend(rois(roi_select))
+    ylabel('ACC')
+    xlabel('ROI')
+    set(gca,'XTickLabel',time_bin)
+    set(gca,'ylim',[chance chance+0.1*chance]);
 end
--- monkey/phy/roi_decoding.m
+++ monkey/phy/roi_decoding.m
@@ -21,36 +21,45 @@ trl_type = 'odorresp';
 roi_num=size(cur_level_roi,1);
 % number of repeats
 repeat_num=100;
-time_range=[0 2];
-% conditions = {'6odor','banana','5odor','airodor','vaodor'};
-conditions = {'intensity','fakeva'};
-trial_num = [100 100 100 100 100];
+% time_range=[1.4 2];
+% time=[0:0.2:1.4];
+time=[0 0.4 0.6 1 1.2];
+time_win=0.6;
+
+conditions = {'5odor','airodor','vaodor'};
+% conditions = [conditions {'6odor','banana','intensity','fakeva'}];
+% trial_num = [100 100 100 100 100 100 100];
+trial_num = 100*ones(1,length(conditions));
 results = cell(length(conditions),2);
 results(:,1) = conditions';
-
-% run decoding for each condition
-for condition_i = 1:length(conditions)
-    condition = conditions{condition_i};
-    results_odor = cell(roi_num,repeat_num);
-    tnum = trial_num(condition_i);
-    % each roi
-    for roi_i=1:roi_num
-        % change data format and select odor condition
-        cfg         = [];
-        cfg.latency = time_range;
-        cfg.keeptrials = 'yes';
-        lfp_odor    = ft_timelockanalysis(cfg, roi_lfp{roi_i});
-        % run decoding
-        parfor repeat_i=1:repeat_num
-            results_odor{roi_i,repeat_i}=sample_lfp_decoding(lfp_odor, condition, tnum);
+for time_i = 1:length(time)
+    time_range=[time(time_i) time(time_i)+time_win];
+    % run decoding for each condition
+    for condition_i = 1:length(conditions)
+        condition = conditions{condition_i};
+        results_odor = cell(roi_num,repeat_num);
+        tnum = trial_num(condition_i);
+        % each roi
+        for roi_i=1:roi_num
+            % change data format and select odor condition
+            cfg         = [];
+            cfg.latency = time_range;
+            cfg.keeptrials = 'yes';
+            lfp_odor    = ft_timelockanalysis(cfg, roi_lfp{roi_i});
+            % run decoding
+            parfor repeat_i=1:repeat_num
+                results_odor{roi_i,repeat_i}=sample_lfp_decoding(lfp_odor, condition, tnum);
+            end
+            results_odor{roi_i,1}.analysis = [cur_level_roi{roi_i,1}, '_' results_odor{roi_i,1}.analysis];        
         end
-        results_odor{roi_i,1}.analysis = [cur_level_roi{roi_i,1}, '_' results_odor{roi_i,1}.analysis];        
+        % save results for this condition
+        results{condition_i,2}=results_odor;    
     end
-    % save results for this condition
-    results{condition_i,2}=results_odor;    
+    % save results
+    time_bin=[num2str(time(time_i)) '-' num2str(time(time_i)+time_win) 's'];
+    matname = ['decoding_results' num2str(tnum) '_linear_' time_bin '.mat'];
+    save([pic_dir matname],'results')
+    % plot acc
+%     odor_decoding_results(matname);
+%     close all
 end
-% save results
-matname = 'decoding_results_linear_fakeva-int.mat';
-save([pic_dir matname],'results')
-% plot acc
-odor_decoding_results(matname)

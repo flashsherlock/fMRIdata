3009be2ba4ad4de219ae86ffd4f0c829d1fddb4a
flashsherlock <sherlockholmes221b@163.com>
2022-03-15 11:03:03 +0800

    save correlation results to mat file

--- monkey/phy/erp_resp.m
+++ monkey/phy/erp_resp.m
@@ -1,6 +1,6 @@
 %% set path
 data_dir='/Volumes/WD_D/gufei/monkey_data/yuanliu/merge2monkey/';
-pic_dir=[data_dir 'pic/erp_resp/'];
+pic_dir=[data_dir 'pic/erp_resp/2monkey/'];
 if ~exist(pic_dir,'dir')
     mkdir(pic_dir);
 end
@@ -12,115 +12,127 @@ trl_type = 'odorresp';
 % get number of roi
 roi_num=size(cur_level_roi,1);
 odor_num=7;
-r3s=zeros(roi_num,1);
+% save to results
+results=cell(roi_num,5,2);
+results(:,1,1)=cur_level_roi(:,1);
+results(:,1,2)=cur_level_roi(:,1);
 %% analyze
 for roi_i=1:roi_num
-cur_roi=cur_level_roi{roi_i,1};
-lfp=roi_lfp{roi_i};
-resp=roi_resp{roi_i};
-% select air condition
-condition=7;
-cfg=[];
-if condition<=6
-% air
-cfg.trials = find(resp.trialinfo==condition);
-else
-% odor
-cfg.trials = find(resp.trialinfo~=6);
-end
-resavg=ft_timelockanalysis(cfg, resp);
-%% show low frequency signal
-cfg=[];
-cfg.lpfilter = 'yes';
-cfg.lpfilttype = 'fir';
-cfg.lpfreq = 0.6;
-if condition<=6
-% air
-cfg.trials = find(lfp.trialinfo==condition);
-else
-% odor
-cfg.trials = find(lfp.trialinfo~=6);
-end
-lfp_l = ft_preprocessing(cfg,lfp);
-% cfg          = [];
-% cfg.method   = 'trial';
-% dummy        = ft_rejectvisual(cfg,lfp_l);
-% cfg = [];
-% cfg.viewmode = 'vertical';
-% cfg.ylim = 'maxmin';
-% lfp_l = rmfield(lfp_l,'sampleinfo');
-% eegplot = ft_databrowser(cfg,lfp_l);
-%% ERP
-% baseline correction
-cfg = [];
-cfg.keeptrials = 'yes';
-erp = ft_timelockanalysis(cfg, lfp_l);
-cfg              = [];
-cfg.baseline     = [-1 -0.5];
-bs=linspace(cfg.baseline(1),cfg.baseline(2),100);
-erp_blc = ft_timelockbaseline(cfg, erp);
-% average trials
-cfg = [];
-cfg.avgoverrpt =  'yes';
-erp_blc=ft_selectdata(cfg,erp_blc);
-% compute correlation
-time_range = [-1 3];
-select=resavg.time>=0&resavg.time<=time_range(2);
-r3=corr(resavg.avg(select)',squeeze(mean(erp_blc.trial(:,select),1))');
-% select=resavg.time>=time_range(1)&resavg.time<=time_range(2);
-% r7=corr(resavg.avg',squeeze(mean(erp_blc.trial,1))');
-true=atanh(r3);
-r3s(roi_i)=r3;
-%% permutation test
-for step=1%[1 10 100 200 500]
-nper=1000;
-r3_per=zeros(1,nper);
-erp_per=zeros(length(erp.trialinfo),3001);
-for per_i=1:nper
-    r=randi([1 length(erp.time)-3000],length(erp.trialinfo),1);
-    r=step*(ceil(r/step)-1)+1;
-    for trial_i=1:length(erp.trialinfo)
-        erp_per(trial_i,:)=erp.trial(trial_i,1,r(trial_i):r(trial_i)+3000);
+    cur_roi=cur_level_roi{roi_i,1};
+    lfp=roi_lfp{roi_i};
+    resp=roi_resp{roi_i};
+    % select air/odor condition
+    for condition = 6:7
+        % condition=7;
+        cfg=[];
+        if condition<=6
+            % air
+            cfg.trials = find(resp.trialinfo==condition);
+        else
+            % odor
+            cfg.trials = find(resp.trialinfo~=6);
+        end
+        resavg=ft_timelockanalysis(cfg, resp);
+        %% show low frequency signal
+        cfg=[];
+        cfg.lpfilter = 'yes';
+        cfg.lpfilttype = 'fir';
+        cfg.lpfreq = 0.6;
+        if condition<=6
+            % air
+            cfg.trials = find(lfp.trialinfo==condition);
+        else
+            % odor
+            cfg.trials = find(lfp.trialinfo~=6);
+        end
+        lfp_l = ft_preprocessing(cfg,lfp);
+        % cfg          = [];
+        % cfg.method   = 'trial';
+        % dummy        = ft_rejectvisual(cfg,lfp_l);
+        % cfg = [];
+        % cfg.viewmode = 'vertical';
+        % cfg.ylim = 'maxmin';
+        % lfp_l = rmfield(lfp_l,'sampleinfo');
+        % eegplot = ft_databrowser(cfg,lfp_l);
+        %% ERP
+        % baseline correction
+        cfg = [];
+        cfg.keeptrials = 'yes';
+        erp = ft_timelockanalysis(cfg, lfp_l);
+        cfg              = [];
+        cfg.baseline     = [-1 -0.5];
+        bs=linspace(cfg.baseline(1),cfg.baseline(2),100);
+        erp_blc = ft_timelockbaseline(cfg, erp);
+        % average trials
+        cfg = [];
+        cfg.avgoverrpt =  'yes';
+        erp_blc=ft_selectdata(cfg,erp_blc);
+        % compute correlation
+        time_range = [-1 3];
+        select=resavg.time>=0&resavg.time<=time_range(2);
+        r3=corr(resavg.avg(select)',squeeze(mean(erp_blc.trial(:,select),1))');
+        % select=resavg.time>=time_range(1)&resavg.time<=time_range(2);
+        % r7=corr(resavg.avg',squeeze(mean(erp_blc.trial,1))');
+        true=atanh(r3);
+        % save results
+        results{roi_i,2,condition-5}=resavg;
+        results{roi_i,3,condition-5}=erp_blc;
+        results{roi_i,4,condition-5}=r3;
+        %% permutation test
+        for step=1%[1 10 100 200 500]
+        nper=1000;
+        r3_per=zeros(1,nper);
+        erp_per=zeros(length(erp.trialinfo),3001);
+        for per_i=1:nper
+            r=randi([1 length(erp.time)-3000],length(erp.trialinfo),1);
+            r=step*(ceil(r/step)-1)+1;
+            for trial_i=1:length(erp.trialinfo)
+                erp_per(trial_i,:)=erp.trial(trial_i,1,r(trial_i):r(trial_i)+3000);
+            end
+            r3_per(per_i)=corr(resavg.avg(select)',mean(erp_per,1)');
+        end
+        % save raw permutated r, combine steps
+        results{roi_i,5,condition-5}=[results{roi_i,5,condition-5};r3_per];
+        % fisher-z
+        r3_per=atanh(r3_per);
+        cut1=prctile(r3_per,97.5);
+        cut2=prctile(r3_per,2.5);
+        sig='';
+        if true>cut1 || true<cut2
+            sig='*';
+        end
+        %% plot
+        figure('position',[20 20 1000 450])
+        subplot(1,2,1)
+        hold on
+        plot(erp_blc.time,1000*erp_blc.trial,'b','LineWidth',1.5)
+        xlabel('Time (s)')
+        ylabel('Voltage')
+        yyaxis right
+        plot(resavg.time,resavg.avg,'k','LineWidth',1.5)
+        set(gca,'xlim',time_range,'ytick',[]);
+        set(gca,'FontSize',16);
+        % permutation test
+        subplot(1,2,2)
+        hold on
+        hist(r3_per,50)
+        set(gca,'xlim',[-3 3],'ylim',[0 60]);
+        plot([cut1 cut1],get(gca,'Ylim'),'b','linewidth',1.5)
+        plot([cut2 cut2],get(gca,'Ylim'),'b','linewidth',1.5)
+        plot(true,1,'r.','MarkerSize',30)
+        text(-2.8,50,[sprintf('R: %0.2f',round(r3,2)) sig],'Fontsize',16)
+        xlabel('Z')
+        ylabel('Iterations')
+        set(gca,'FontSize',16);
+        suptitle([cur_roi ' 0-3s:' num2str(r3)])
+        saveas(gcf, [pic_dir cur_roi num2str(step) '_' num2str(condition) '.png'],'png')
+        % saveas(gcf, [pic_dir cur_roi num2str(step) '_' num2str(condition) '.fig'],'fig')
+        close all
+        end
     end
-    r3_per(per_i)=corr(resavg.avg(select)',mean(erp_per,1)');
-end
-r3_per=atanh(r3_per);
-cut1=prctile(r3_per,97.5);
-cut2=prctile(r3_per,2.5);
-sig='';
-if true>cut1 || true<cut2
-    sig='*';
-end
-%% plot
-figure('position',[20 20 1000 450])
-subplot(1,2,1)
-hold on
-plot(erp_blc.time,1000*erp_blc.trial,'b','LineWidth',1.5)
-xlabel('Time (s)')
-ylabel('Voltage')
-yyaxis right
-plot(resavg.time,resavg.avg,'k','LineWidth',1.5)
-set(gca,'xlim',time_range,'ytick',[]);
-set(gca,'FontSize',16);
-% permutation test
-subplot(1,2,2)
-hold on
-hist(r3_per,50)
-set(gca,'xlim',[-3 3],'ylim',[0 60]);
-plot([cut1 cut1],get(gca,'Ylim'),'b','linewidth',1.5)
-plot([cut2 cut2],get(gca,'Ylim'),'b','linewidth',1.5)
-plot(true,1,'r.','MarkerSize',30)
-text(-2.8,50,[sprintf('R: %0.2f',round(r3,2)) sig],'Fontsize',16)
-xlabel('Z')
-ylabel('Iterations')
-set(gca,'FontSize',16);
-suptitle([cur_roi ' 0-3s:' num2str(r3)])
-saveas(gcf, [pic_dir cur_roi num2str(step) '_' num2str(condition) '.png'],'png')
-saveas(gcf, [pic_dir cur_roi num2str(step) '_' num2str(condition) '.fig'],'fig')
-close all
-end
 % figure
 % scatter(resavg.avg(select)',squeeze(mean(erp_blc.trial(:,select),1)'))
 % figure
 % scatter(resavg.avg',squeeze(mean(erp_blc.trial,1))')
 end
+save([pic_dir 'correlation.mat'],'results');

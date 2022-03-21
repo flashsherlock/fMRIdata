8c10ea5f34e3288d4632591bc8763b3ecb2c15a5
flashsherlock <sherlockholmes221b@163.com>
2022-03-21 23:00:22 +0800

    calculate mi using ft_crossfrequencyanalysis

--- monkey/phy/roi_coherence.m
+++ monkey/phy/roi_coherence.m
@@ -1,6 +1,13 @@
 %% load and reorganize data
+% monkeys = {'RM035','RM033'};
+monkeys = {'RM035'};
+if length(monkeys) > 1
+    m = '2monkey';
+else
+    m = monkeys{1};
+end
 data_dir='/Volumes/WD_D/gufei/monkey_data/yuanliu/merge2monkey/';
-pic_dir=[data_dir 'pic/coherence/'];
+pic_dir=[data_dir 'pic/coherence/' m '/'];
 if ~exist(pic_dir,'dir')
     mkdir(pic_dir);
 end
@@ -20,49 +27,90 @@ trl_type = 'odor';
 % number of roi
 roi_num=size(cur_level_roi,1);
 odor_num=7;
-cross_freq_result=cell(roi_num,odor_num);
-% filter
-latency = [0 8];
-freqs=13:3:85;
-bandwidth=2;
-% modulation index
-nbins=20;
-nsurrogates=1000;
-randtype=2; %timesplice
+% 1-respiration 2-theta
+low={'-respiration','-theta'};
+cross_freq_result=cell(roi_num,length(low));
+% time and method
+latency = [0 7];
+method = 'mi';
+nper=1000;
 % plot 
 colors = {'#777DDD', '#69b4d9', '#149ade', '#41AB5D', '#ECB556', '#000000', '#E12A3C', '#777DDD', '#41AB5D'};
 line_wid = 1.5;
 %% cross frequency analysis
 for roi_i=1:roi_num
-    % select trials
-    for odor_i=1:odor_num
-        cfg         = [];
-        if odor_i==7
-            cfg.trials  = find(roi_lfp{roi_i}.trialinfo~=6);
-        else
-            cfg.trials  = find(roi_lfp{roi_i}.trialinfo==odor_i);
+    % must have the same label
+    roi_resp{roi_i}.label = roi_lfp{roi_i}.label;
+    % frequency analysis
+    cfg=[];
+    cfg.output='fourier';
+    cfg.method='mtmconvol';
+    cfg.taper='hanning';
+    cfg.toi = -3.5:0.05:9.5; 
+    cfg.keeptrials='yes';
+    % resp
+    cfg.foi=0.1:10;
+    cfg.t_ftimwin = ones(length(cfg.foi),1).*0.5;
+    resp_freq=ft_freqanalysis(cfg,roi_resp{roi_i});
+    % lfp
+    cfg.foi=1:200;
+    cfg.t_ftimwin = ones(length(cfg.foi),1).*0.5;
+    lfp_freq=ft_freqanalysis(cfg,roi_lfp{roi_i});
+
+    % select 7s
+    cfg = [];
+    cfg.latency= latency;
+    resp_freq = ft_selectdata(cfg,resp_freq);
+    lfp_freq = ft_selectdata(cfg,lfp_freq);
+    % modulation index
+    cfg=[];
+    cfg.freqlow = [0 10];
+    cfg.freqhigh = [1 200];
+    cfg.method = method;
+    cfg.keeptrials = 'yes';
+    cross_freq_result{roi_i,1} = ft_crossfrequencyanalysis(cfg, resp_freq, lfp_freq);
+    cfg.freqlow = [4 8];
+    cfg.freqhigh = [13 200];
+    cross_freq_result{roi_i,2} = ft_crossfrequencyanalysis(cfg, lfp_freq);
+    
+    % TODO: permutation test
+    
+    % add trialinfo and remove cfg
+    for low_i=1:length(low)
+        cross_freq_result{roi_i,low_i}.trialinfo = roi_lfp{roi_i}.trialinfo;
+        cross_freq_result{roi_i,low_i} = rmfield(cross_freq_result{roi_i,low_i}, 'cfg');
         end
-        lfp = ft_selectdata(cfg, roi_lfp{roi_i});
-        resp = ft_selectdata(cfg, roi_resp{roi_i});    
-        % get low frequency phase and high frequency amplitude
-        [xphase, xamp]=get_signal_pa(resp, lfp, latency, freqs, bandwidth);
-        % compute modulation index
-        cross_freq_result{roi_i,odor_i} = get_mi(xphase,xamp,nbins,nsurrogates,randtype);
+    
     end
-    % plot
-    % 1-respiration 2-theta
-    low={'-respiration','-theta'};
+
+%% plot
+for roi_i=1:roi_num
+    % respiration and theta
     for low_i=1:length(low)        
+        
         figure('position',[20,20,600,600]);
         % modulation index
         subplot(2,1,1)
         hold on;
         title([cur_level_roi{roi_i,1} low{low_i}])
+        freqs=cross_freq_result{roi_i,low_i}.freqhigh;
+        
+        % calculate mean modulation index and plot
         for odor_i=1:odor_num
-            plot(freqs,cross_freq_result{roi_i,odor_i}.MI(low_i,2:end),...
+            % slect trials
+            if odor_i==7
+                trials  = find(cross_freq_result{roi_i,low_i}.trialinfo~=6);
+            else
+                trials  = find(cross_freq_result{roi_i,low_i}.trialinfo==odor_i);
+            end       
+            % rpt_chan_freqlow_freqhigh
+            plv=cross_freq_result{roi_i,low_i}.crsspctrm(trials,:,:,:);
+            plot(freqs,squeeze(mean(mean(plv,1),3)),...
                 'Color',hex2rgb(colors{odor_i}),'linewidth', line_wid)
         end
-        set(gca,'xlim',[freqs(1) freqs(end)]);
+        set(gca,'xlim',[13 80]);
+        set(gca,'XTick',[13 20:10:80]);
+        set(gca,'XTickLabel',[13 20:10:80]);
         legend('Ind','Iso_l','Iso_h','Peach','Banana','Air','Odor')
         ylabel('Modulation Index')
         

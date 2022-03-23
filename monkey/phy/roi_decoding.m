62c92ccf3029477b03f9e7dde6de5ec59b44c9f1
flashsherlock <sherlockholmes221b@163.com>
2022-03-23 14:58:23 +0800

    control distribution of trials for decoding

--- monkey/phy/roi_decoding.m
+++ monkey/phy/roi_decoding.m
@@ -1,6 +1,13 @@
 %% load and reorganize data
+monkeys = {'RM035','RM033'};
+% monkeys = {'RM033'};
+if length(monkeys) > 1
+    m = '2monkey';
+else
+    m = monkeys{1};
+end
 data_dir='/Volumes/WD_D/gufei/monkey_data/yuanliu/merge2monkey/';
-pic_dir=[data_dir 'pic/decoding/'];
+pic_dir=[data_dir 'pic/decoding/' m '/'];
 if ~exist(pic_dir,'dir')
     mkdir(pic_dir);
 end
@@ -22,14 +29,14 @@ roi_num=size(cur_level_roi,1);
 % number of repeats
 repeat_num=100;
 % time_range=[1.4 2];
-time=[0:0.1:1.6];
+time=[0:0.2:1.4];
 % time=[0 0.4 0.6 1 1.2];
-time_win=0.4;
+time_win=0.6;
 
 conditions = {'5odor','vaodor'};
 % conditions = [conditions {'6odor','banana','intensity','fakeva'}];
 % trial_num = [100 100 100 100 100 100 100];
-trial_num = 50*ones(1,length(conditions));
+trial_num = 60*ones(1,length(conditions));
 results = cell(length(conditions),2);
 results(:,1) = conditions';
 for time_i = 1:length(time)
--- monkey/phy/sample_lfp_decoding.m
+++ monkey/phy/sample_lfp_decoding.m
@@ -1,5 +1,6 @@
 function results = sample_lfp_decoding(data,condition,trial_num)
-    cfg = [];    
+    cfg = [];
+    balence = 0;
     switch condition
         case '5odor'
             cfg.trials  = find(data.trialinfo<=5);
@@ -7,25 +8,38 @@ function results = sample_lfp_decoding(data,condition,trial_num)
         case '6odor'
             % do nothing
         case 'airodor'
+            balence = 1;
+            weight = [ones(1,5)/5 1];
             data.trialinfo(data.trialinfo<=5) = 1;%odor
             data.trialinfo(data.trialinfo==6) = 2;%air
         case 'vaodor'
+            balence = 1;
+            weight = [0.5 0.25 0.25 0.5 0.5];
             cfg.trials  = find(data.trialinfo<=5);
             data = ft_selectdata(cfg, data);
+            % original trialinfo
+            orig_info = data.trialinfo;
             % unpleasant
             data.trialinfo(data.trialinfo<=3) = 1;
             % pleasant
             data.trialinfo(data.trialinfo==4 | data.trialinfo==5) = 2;        
         case 'banana'
+            balence = [ones(1,4)/4 1];
             cfg.trials  = find(data.trialinfo<=5);
             data = ft_selectdata(cfg, data);
+            % original trialinfo
+            orig_info = data.trialinfo;
             % unpleasant
             data.trialinfo(data.trialinfo<=4) = 1;
             % pleasant
             data.trialinfo(data.trialinfo==5) = 2;
         case 'fakeva'
+            balence = 1;
+            weight = [0.25 0.5 0.5 0.25 0.5];
             cfg.trials  = find(data.trialinfo<=5);
             data = ft_selectdata(cfg, data);
+            % original trialinfo
+            orig_info = data.trialinfo;
             % ind iso_l pea
             data.trialinfo(data.trialinfo<=2 | data.trialinfo==4) = 1;
             % iso_h ban
@@ -36,14 +50,33 @@ function results = sample_lfp_decoding(data,condition,trial_num)
             % change to 1 and 2 to avoid 0 count of 1 in tabulate
             data.trialinfo = data.trialinfo-1;
     end
-    
     % calculate trial numbers
-    t=tabulate(data.trialinfo);
+    if balence == 0    
+        info=data.trialinfo;
+        t=tabulate(data.trialinfo);
+        % find select number
+        min_trial_num=min(t(:,2));
+        select_num = min(trial_num,min_trial_num)*ones(size(t,1),1);
+    else
+        info=orig_info;
+        t=tabulate(orig_info);
+        % find select number
+        actual = t(:,2)./weight';        
+        if any(actual < trial_num)
+            select_num = min(actual)*weight';
+        else
+            select_num = trial_num*weight';
+            % check whether select_num is an integer array
+            if any(mod(select_num,1))~=0
+                error('Inappropriate trial number!');
+            end
+        end
+    end
     % find min number
-    min_trial_num=min(t(:,2));
+    % min_trial_num=min(t(:,2));
     % t(:,2)=t(:,2)-min_trial_num;
     % number of trials
-    select_num = min(trial_num,min_trial_num);    
+    % select_num = min(trial_num,min_trial_num);    
     
     for t_i=1:size(t,1)
         % remove first several trials to balance conditions
@@ -51,8 +84,8 @@ function results = sample_lfp_decoding(data,condition,trial_num)
 %           data.trialinfo(find(data.trialinfo==t(t_i,1),t(t_i,2)))=0;          
 %        end
         % randomly select trials
-       trials=find(data.trialinfo==t(t_i,1));
-       trials=trials(datasample(1:t(t_i,2),t(t_i,2)-select_num,'Replace',false));
+       trials=find(info==t(t_i,1));
+       trials=trials(datasample(1:t(t_i,2),t(t_i,2)-select_num(t_i),'Replace',false));
        data.trialinfo(trials)=0;
     end
     % select trial
@@ -70,5 +103,5 @@ function results = sample_lfp_decoding(data,condition,trial_num)
     clear data
     [results,~]=odor_decoding_function(passed_data,length(unique(label)));
     % return condtion and number of selection
-    results.analysis=[condition '_' num2str(select_num)];
+    results.analysis=[condition '_' num2str(select_num')];
 end

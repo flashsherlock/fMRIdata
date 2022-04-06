a4712e9e649e31adebab2163772fd0dbf35a4c85
flashsherlock <sherlockholmes221b@163.com>
2022-04-06 20:19:36 +0800

    tf-analysis for each position

---
+++ monkey/phy/lfp_odorresp_sep.m
@@ -0,0 +1,49 @@
+%% load and reorganize data
+monkeys = {'RM035','RM033'};
+if length(monkeys) > 1
+    m = '2monkey';
+else
+    m = monkeys{1};
+end
+data_dir='/Volumes/WD_D/gufei/monkey_data/yuanliu/merge2monkey/';
+pic_dir=[data_dir 'pic/lfp_odorresp/' m '/'];
+if ~exist(pic_dir,'dir')
+    mkdir(pic_dir);
+end
+%% generate data
+level = 3;
+trl_type = 'odorresp';
+% combine 2 monkeys
+[roi_lfp,roi_resp,cur_level_roi] = save_sep_2monkey(level,trl_type,monkeys);
+% get number of positions
+roi_num=size(cur_level_roi,1);
+odor_num=7;
+%% TF analysis
+if exist([data_dir 'tf_sep_' m '.mat'],'file')
+    load([data_dir 'tf_sep_' m '.mat'])
+else
+    freq_sep_all=cell(roi_num,1);
+    for roi_i=1:roi_num
+        cur_roi=cur_level_roi{roi_i,1};
+        % filt data
+        cfg=[];
+        cfg.bsfilter    = 'yes';
+        cfg.bsfilttype = 'fir';
+        cfg.bsfreq      = [99 101;149 151;199 201];
+        lfp = ft_preprocessing(cfg,roi_lfp{roi_i});
+        % time frequency analysis
+        cfgtf=[];
+        cfgtf.method     = 'mtmconvol';
+        cfgtf.toi        = -3.5:0.05:9.5;
+        % cfgtf.foi        = 1:1:100;
+        % other wavelet parameters
+        cfgtf.foi = logspace(log10(1),log10(200),51);
+        % cfgtf.t_ftimwin  = ones(length(cfgtf.foi),1).*0.5;
+        cfgtf.t_ftimwin  = 5./cfgtf.foi;
+        cfgtf.taper      = 'hanning';
+        cfgtf.output     = 'pow';
+        cfgtf.keeptrials = 'yes';
+        freq_sep_all{roi_i} = ft_freqanalysis(cfgtf, lfp);
+    end
+    save([data_dir 'tf_sep_' m '.mat'],'freq_sep_all','cur_level_roi','-v7.3')
+end

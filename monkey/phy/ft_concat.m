64b206d05076d82fb46fcb80ee0a8662a971e537
flashsherlock <sherlockholmes221b@163.com>
2022-07-05 20:56:18 +0800

    add resample to ft_concat

--- monkey/phy/ft_concat.m
+++ monkey/phy/ft_concat.m
@@ -1,16 +1,26 @@
-function eeg = ft_concat(data,trial)
+function eeg = ft_concat(data,condition)
     % remove fields
     rmf = setdiff(fieldnames(data),{'label','trialinfo','trial','time','fsample'});
     data = rmfield(data,rmf);
     % select trials
     cfg = [];
-    cfg.trials = find(data.trialinfo == trial);
+    if condition<=6
+        % air
+        cfg.trials = find(data.trialinfo==condition);
+    else
+        % odor
+        cfg.trials = find(data.trialinfo~=6);
+    end
     eeg = ft_selectdata(cfg, data);    
     % set trialinfo
-    eeg.trialinfo = trial;
+    eeg.trialinfo = condition;
     % concatenate trials
     trials = eeg.trial;
-    eeg.trial = cell2mat(trials);
+    eeg.trial = cell2mat(trials);          
+    % resample
+    fsnew = 200;
+    eeg.trial = resample(eeg.trial,fsnew,eeg.fsample);
+    eeg.fsample = fsnew;
     % set time
     eeg.time = ((1:length(eeg.trial))-1)/eeg.fsample;
 end

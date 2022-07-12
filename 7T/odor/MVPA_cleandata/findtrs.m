bf2eb0f8b830e019919f6aceb26d311a22b63cc1
flashsherlock <sherlockholmes221b@163.com>
2022-07-12 21:34:52 +0800

    add columns for run and repeat number

--- 7T/odor/MVPA_cleandata/findtrs.m
+++ 7T/odor/MVPA_cleandata/findtrs.m
@@ -35,10 +35,12 @@ if times==8
 elseif times==6
     trs=392;
 end
-timing=zeros(2,odor_num*times,length(run));
+timing=zeros(3,odor_num*times,length(run));
 
 timing(1,:,i)=odor;
 timing(2,:,i)=time+(i-1)*trs;
+% run number
+timing(3,:,i)=i;
 
 % start from the 2nd run
 for i=2:length(run)
@@ -59,11 +61,11 @@ for i=2:length(run)
     time=result(:,5);
     timing(1,:,i)=odor;
     timing(2,:,i)=time+(i-1)*trs;
-    
+    timing(3,:,i)=i;
 end
 
 % reshape to only 2 rows
-timing=reshape(timing,[2 length(run)*odor_num*times]);
+timing=reshape(timing,[3 length(run)*odor_num*times]);
 % code odors to 1:4 if above 6
 if timing(1,1)>=7
     timing(1,:)=timing(1,:)-6;
@@ -72,5 +74,7 @@ end
 timing(2,:)=ceil((timing(2,:)+shift)/3);
 % sort by odors
 timing=sortrows(timing');
+% add a column for repeats
+timing=[timing  kron(ones(size(timing,1)/times,1),(1:times)')];
 end
 

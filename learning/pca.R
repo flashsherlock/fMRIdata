74a96668be7c843cbb6236be3640ed89cf69ca05
flashsherlock <sherlockholmes221b@163.com>
2022-07-13 14:42:02 +0800

    understand pca using covar as input

--- learning/pca.R
+++ learning/pca.R
@@ -9,10 +9,21 @@ r[1,1] <- 16
 pcu <- principal(r,nfactors = 5,rotate = "none")
 pc <- principal(r,nfactors = 5,rotate = "varimax")
 pco <- principal(r,nfactors = 5,rotate = "oblimin")
-
+corpcu <- principal(cov(r),nfactors = 5,rotate = "none",covar = F)
+copcu <- principal(cov(r),nfactors = 5,rotate = "none",covar = T)
+#  prcomp function
+prcopc <- prcomp(r, center = T,scale. = F)
+prcopcs <- prcomp(r, center = T,scale. = T)
 # weights and loadings has the same direction afte pca
 pcu$weights[,5]/norm(pcu$weights[,5],type = "2")
 pcu$loadings[,5]/norm(pcu$loadings[,5],type = "2")
+corpcu$weights[,5]/norm(corpcu$weights[,5],type = "2")
+corpcu$loadings[,5]/norm(corpcu$loadings[,5],type = "2")
+prcopc
+# unscaled pcs
+prcopcs
+copcu$weights[,5]/norm(copcu$weights[,5],type = "2")
+copcu$loadings[,5]/norm(copcu$loadings[,5],type = "2")
 # not for rotated weights (because unrotated W is the )
 pc$loadings[,5]/norm(pc$loadings[,5],type = "2")
 pc$weights[,5]/norm(pc$weights[,5],type = "2")
@@ -22,6 +33,27 @@ zr%*%pcu$weights
 pcu$scores
 zr%*%pc$weights
 pc$scores
+# scores are normalized
+describe(pcu$scores)
+describe(pc$scores)
+# no scores if covar = F
+describe(r)
+# scale columns weights and loadings are in the same direction
+scale(copcu$weights, center=FALSE, scale=apply(copcu$weights,2,norm,type = "2"))
+scale(copcu$loadings, center=FALSE, scale=apply(copcu$loadings,2,norm,type = "2"))
+# loadings were scaled by the root of eigen values
+sqrt(copcu$values)
+apply(copcu$loadings,2,norm,type = "2")
+# sd of the r*uniformed_eigen is the root of eigen values
+describe(r%*%scale(copcu$loadings, center=FALSE,
+                   scale=apply(copcu$loadings,2,norm,type = "2")))
+# zr * weights sd!=1
+zr%*%copcu$weights
+describe(zr%*%copcu$weights)
+# input * weights sd=1
+r%*%copcu$weights
+describe(r%*%copcu$weights)
+zr%*%corpcu$weights
 # weights = inv(R)*structure(loadings)
 solve(cor(r))%*%pcu$loadings
 solve(cor(r))%*%pcu$Structure
@@ -29,6 +61,9 @@ pcu$weights
 solve(cor(r))%*%pc$loadings
 solve(cor(r))%*%pc$Structure
 pc$weights
+# inv(COV) if use covar
+solve(cov(r))%*%copcu$loadings
+copcu$weights
 # pc weights is the rotated pcu weights
 pcu$weights%*%pc$rot.mat
 # pc loadings is the rotated pcu loadings
@@ -53,3 +88,14 @@ pco$weights
 cor(zr)
 pc$loadings%*%t(pc$loadings)
 pcu$loadings%*%t(pcu$loadings)
+# sum of square of loadings
+sum(pcu$loadings[1,]^2)
+sum(pcu$loadings[,1]^2)
+# rotated
+sum(pc$loadings[1,]^2)
+sum(pc$loadings[,1]^2)
+sum(pco$loadings[1,]^2)
+sum(pco$loadings[,1]^2)
+# Structure is the correlation between zr and pco$scores
+pco$Structure
+cor(zr,pco$scores)

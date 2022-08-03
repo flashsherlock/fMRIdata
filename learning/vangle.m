d4b6c222670414ce622de2650bab18a93cdd6627
flashsherlock <sherlockholmes221b@163.com>
2022-08-03 22:57:09 +0800

    update comments and change encoding to utf8

--- learning/vangle.m
+++ learning/vangle.m
@@ -1,11 +1,16 @@
 function degree =vangle( distance,size,resolution,pixel )
-%compute the visual angle
-%input the distance to the screen(meter),the diagonal size of the screen
-%(meter),the resolution of the screen([width height]),pixel size of the 
-%stimuli,then you can get the visual angel of the stimuli.
-%Ã¿¸öÏñËØµÄ´óĞ¡
+% compute the visual angle
+% input the distance to the screen(meter),the diagonal size of the screen
+% (meter),the resolution of the screen([width height]),pixel size of the 
+% stimuli,then you can get the visual angel of the stimuli.
+% e.g. vangle(20,20.32,[1920 1080],42) -> 1.1098
+% GOOVIS G2 : 80-inch(20.32 meters) screen from 20 meters
+
+% æ¯ä¸ªåƒç´ çš„å¤§å°
+% size for each pixel
 d=sin(atan(resolution(1)/resolution(2)))*size/resolution(1);
-%¼ÆËãÊÓ½Ç
+% calculate angle
+% è®¡ç®—è§†è§’
 degree=2*atand(pixel*d/2/distance);
 end
 

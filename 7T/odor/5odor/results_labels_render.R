ace33fd91375240fad166b729aae823741102a63
flashsherlock <sherlockholmes221b@163.com>
2022-08-08 19:49:50 +0800

    decode using head motion from odorVIva

--- 7T/odor/5odor/results_labels_render.R
+++ 7T/odor/5odor/results_labels_render.R
@@ -1,6 +1,6 @@
 # default values
 path <- "/Volumes/WD_E/gufei/7T_odor/"
-mp<- c("roi_ARodor_l1_labelhead_6")
+mp<- c("roi_ARodor_l1_labelheadva_6")
 analysis <- c("pabiode")
 # subs 
 subs <- c(sprintf('S%02d',c(4:11,13,14,16:18)))
@@ -19,10 +19,10 @@ rmarkdown::render("/Users/mac/Documents/GitHub/fMRIdata/7T/odor/5odor/ratings.Rm
 
 # odor_va results
 # render mean mvpa results
-title <- "Mean6head_ARodor_4-18"
+title <- "Mean6headva_ARodor_4-18"
 rmarkdown::render("/Users/mac/Documents/GitHub/fMRIdata/7T/odor/5odor/results_labels_mean.Rmd",
                   output_dir = paste0(path,"results_labels_r"),
-                  output_file = paste0("Mean_6ARhead_418"),
+                  output_file = paste0("Mean_6ARheadva_418"),
                   params = list(sub = subs, set_title = title, analysis = analysis, 
                                 roi = roi, roiname = roiname, mvpa_pattern = mp))
 
--- 7T/odor/MVPA_cleandata/acc5odor/decoding_roi_5odors_trial.m
+++ 7T/odor/MVPA_cleandata/acc5odor/decoding_roi_5odors_trial.m
@@ -54,7 +54,7 @@ for i=1:length(rois)
     cfg.searchlight.radius = 3; % use searchlight of radius 3 (by default in voxels), see more details below
 
     % Set the output directory where data will be saved, e.g. '/misc/data/mystudy'
-    cfg.results.dir = [datafolder sub '/' sub '.' analysis '.results/mvpa/' cfg.analysis '_ARodor_l1_labelhead_' strrep(num2str(shift), ' ', '') '/' test];
+    cfg.results.dir = [datafolder sub '/' sub '.' analysis '.results/mvpa/' cfg.analysis '_ARodor_l1_labelheadva_' strrep(num2str(shift), ' ', '') '/' test];
     if ~exist(cfg.results.dir,'dir')
         mkdir(cfg.results.dir)
     end
@@ -77,7 +77,7 @@ for i=1:length(rois)
         for subi = 1:numtr
             t=timing(subi,2);
             % F{subi} = [datafolder sub '/' sub '.' analysis '.results/'  'NIerrts.' sub '.' analysis '.odorVI_noblur+orig.BRIK,' num2str(t)];
-            F{subi} = [datafolder sub '/' sub '.' analysis '.results/'  'NIfittshead.' sub '.' analysis '.odorVI_noblur+orig.BRIK,' num2str(t)];
+            F{subi} = [datafolder sub '/' sub '.' analysis '.results/'  'NIfittshead.' sub '.' analysis '.odorVIva_noblur+orig.BRIK,' num2str(t)];
             % F{subi} = [datafolder sub '/' sub '.' analysis '.results/'  'allrun.volreg.' sub '.' analysis '+orig.BRIK,' num2str(t)];
         end
         cfg.files.name = [cfg.files.name F];

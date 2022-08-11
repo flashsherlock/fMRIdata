9389c3ed7574cf7aa14bd4fe69b1ed5b949d474d
flashsherlock <sherlockholmes221b@163.com>
2022-08-11 22:09:17 +0800

    decode using polort (only) removed from odorVIva

--- 7T/odor/MVPA_cleandata/acc5odor/decoding_roi_trial.m
+++ 7T/odor/MVPA_cleandata/acc5odor/decoding_roi_trial.m
@@ -58,7 +58,7 @@ parfor i=1:length(comb)
     cfg.searchlight.radius = 3; % use searchlight of radius 3 (by default in voxels), see more details below
 
     % Set the output directory where data will be saved, e.g. '/misc/data/mystudy'
-    cfg.results.dir = [datafolder sub '/' sub '.' analysis '.results/mvpa/' cfg.analysis '_ARodor_l1_labelheadva_' strrep(num2str(shift), ' ', '') '/' test];
+    cfg.results.dir = [datafolder sub '/' sub '.' analysis '.results/mvpa/' cfg.analysis '_ARodor_l1_labelpolva_' strrep(num2str(shift), ' ', '') '/' test];
     if ~exist(cfg.results.dir,'dir')
         mkdir(cfg.results.dir)
     end
@@ -79,8 +79,8 @@ parfor i=1:length(comb)
         F=cell(1,numtr);
         for subi = 1:numtr
             t=timing(subi,2);
-            % F{subi} = [datafolder sub '/' sub '.' analysis '.results/'  'NIerrts.' sub '.' analysis '.odorVI_noblur+orig.BRIK,' num2str(t)];
-            F{subi} = [datafolder sub '/' sub '.' analysis '.results/'  'NIfittshead.' sub '.' analysis '.odorVIva_noblur+orig.BRIK,' num2str(t)];
+            F{subi} = [datafolder sub '/' sub '.' analysis '.results/'  'NIerrts.' sub '.' analysis '.onlypolva+orig.BRIK,' num2str(t)];
+%             F{subi} = [datafolder sub '/' sub '.' analysis '.results/'  'NIfittshead.' sub '.' analysis '.odorVIva_noblur+orig.BRIK,' num2str(t)];
             % F{subi} = [datafolder sub '/' sub '.' analysis '.results/'  'allrun.volreg.' sub '.' analysis '+orig.BRIK,' num2str(t)];
         end
         cfg.files.name = [cfg.files.name F];

e4ada0c35f8a187a661695ae59f16d58e7dbc3e5
flashsherlock <sherlockholmes221b@163.com>
2022-08-29 22:05:27 +0800

    heatmap for zpower relative to air

--- monkey/phy/powspec_by_roi.m
+++ monkey/phy/powspec_by_roi.m
@@ -1,7 +1,8 @@
 %% load and reorganize data
 m = 'RM033';
+% m = '2monkey';
 data_dir='/Volumes/WD_D/gufei/monkey_data/yuanliu/merge2monkey/pic/';
-pic_dir=[data_dir 'powerspec/' m '/'];
+pic_dir=[data_dir 'powerspec/' m '_air/'];
 load([pic_dir 'powspec_odor_7s_1_80hz.mat']);
 % load([data_dir 'trial_count/odor_level3_trial_count.mat']);
 % get number of roi
@@ -49,33 +50,33 @@ end
 rois=cur_level_roi(:,1);
 save([pic_dir 'zpower_7s_1_80hz.mat'],'zpower','rois');
 %% reorganize data
-switch m
-case '2monkey'
-    roi_select=[8 4 5 10 13 3 9 2 1 12 6 7 11];
-case 'RM033'
-    roi_select=[8 4 5 10 12 3 9 2 1 6 7 11];    
-case 'RM035'
-    roi_select=[5 2 3 10 7 6 1 9 4 8];
-end
-rois=rois(roi_select);
-zpower=zpower(roi_select,:,:);
-colors = {'#777DDD', '#69b4d9', '#149ade', '#41AB5D', '#ECB556', '#000000', '#E12A3C', '#777DDD', '#41AB5D'};
-colors_cp = colors([1:5 7]);
-%% plot mean zpower
-for band_i=1:length(bands)
-    figure('position',[20,20,900,450])
-    b=bar(zpower(:,[1:6],band_i));
-    for i=1:6
-        b(i).FaceColor = hex2rgb(colors{i});
-    end
-    band=bands{band_i};
-    title(['frequency: ' num2str(band) ' Hz'])
-    xlabel('ROI')
-    ylabel('Zpower')
-    legend('Ind','Iso_l','Iso_h','Peach','Banana','Air','Location','eastoutside')
-    set(gca,'XTickLabel',rois)
-    set(gca,'FontSize',18);
-    % save plot
-    saveas(gcf, [pic_dir  'band_' num2str(band(1)) '-' num2str(band(2)) 'Hz', '.png'], 'png')
-    close all
-end
+% switch m
+% case '2monkey'
+%     roi_select=[8 4 5 10 13 3 9 2 1 12 6 7 11];
+% case 'RM033'
+%     roi_select=[8 4 5 10 12 3 9 2 1 6 7 11];    
+% case 'RM035'
+%     roi_select=[5 2 3 10 7 6 1 9 4 8];
+% end
+% rois=rois(roi_select);
+% zpower=zpower(roi_select,:,:);
+% colors = {'#777DDD', '#69b4d9', '#149ade', '#41AB5D', '#ECB556', '#000000', '#E12A3C', '#777DDD', '#41AB5D'};
+% colors_cp = colors([1:5 7]);
+% %% plot mean zpower
+% for band_i=1:length(bands)
+%     figure('position',[20,20,900,450])
+%     b=bar(zpower(:,[1:6],band_i));
+%     for i=1:6
+%         b(i).FaceColor = hex2rgb(colors{i});
+%     end
+%     band=bands{band_i};
+%     title(['frequency: ' num2str(band) ' Hz'])
+%     xlabel('ROI')
+%     ylabel('Zpower')
+%     legend('Ind','Iso_l','Iso_h','Peach','Banana','Air','Location','eastoutside')
+%     set(gca,'XTickLabel',rois)
+%     set(gca,'FontSize',18);
+%     % save plot
+%     saveas(gcf, [pic_dir  'band_' num2str(band(1)) '-' num2str(band(2)) 'Hz', '.png'], 'png')
+%     close all
+% end

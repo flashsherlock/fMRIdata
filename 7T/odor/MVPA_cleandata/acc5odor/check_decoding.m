bdb337ef4ce6e61d55c8992f8e643e660b14b0cf
flashsherlock <sherlockholmes221b@163.com>
2022-11-02 16:36:26 +0800

    normalize mvpa results

--- 7T/odor/MVPA_cleandata/acc5odor/check_decoding.m
+++ 7T/odor/MVPA_cleandata/acc5odor/check_decoding.m
@@ -1,11 +1,12 @@
 % check decoding results
 datafolder='/Volumes/WD_F/gufei/7T_odor/';
-decode=[reshape(repmat([4:11,13,14,16:34],10,1),[],1) repmat([1:10]',29,1)];
+decode=[reshape(repmat([4:11,13,14,16:29 31:34],10,1),[],1) repmat([1:10]',28,1)];
 odors={'lim','tra','car','cit','ind'};
 comb=nchoosek(1:length(odors), 2);
 rois={'BoxROI'};
 shift=[6];
-check = '_VIvaodor_l1_label_';
+% check = '_VIvaodor_l1_label_';
+check = '_ARodor_l1_labelpolandva_';
 for i=1:size(decode,1)
     sub=sprintf('S%02d',decode(i,1));
     odornumber=comb(decode(i,2),:);
@@ -15,5 +16,11 @@ for i=1:size(decode,1)
     result = [datafolder sub '/' sub '.pabiode.results/mvpa/searchlight' check num2str(shift) '/' test];
     if ~exist([result '/res_accuracy_minus_chance+orig.BRIK'],'file')
         disp([num2str(i) ' ' sub ' ' test]);
+    else
+        % align results to standard space
+        cd(result);
+        template = ['../../../../anat_final.' sub '.pabiode+tlrc'];
+        cmd = ['@auto_tlrc -apar ' template ' -input res_accuracy_minus_chance+orig'];
+        unix(cmd);
     end
 end

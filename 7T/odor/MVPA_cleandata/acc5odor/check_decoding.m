d8c8cddeb3bcf2528c1fd0471cb696e752b21374
flashsherlock <sherlockholmes221b@163.com>
2022-11-03 23:56:55 +0800

    generate 3dttest commands

--- 7T/odor/MVPA_cleandata/acc5odor/check_decoding.m
+++ 7T/odor/MVPA_cleandata/acc5odor/check_decoding.m
@@ -1,8 +1,12 @@
 % check decoding results
 datafolder='/Volumes/WD_F/gufei/7T_odor/';
-decode=[reshape(repmat([4:11,13,14,16:29 31:34],10,1),[],1) repmat([1:10]',28,1)];
+subs=[4:11,13,14,16:29 31:34];
+subnum=length(subs);
+
 odors={'lim','tra','car','cit','ind'};
 comb=nchoosek(1:length(odors), 2);
+combn=size(comb,1);
+decode=[reshape(repmat(subs,combn,1),[],1) repmat([1:combn]',subnum,1)];
 rois={'BoxROI'};
 shift=[6];
 % check = '_VIvaodor_l1_label_';
@@ -18,9 +22,33 @@ for i=1:size(decode,1)
         disp([num2str(i) ' ' sub ' ' test]);
     else
         % align results to standard space
-        cd(result);
-        template = ['../../../../anat_final.' sub '.pabiode+tlrc'];
-        cmd = ['@auto_tlrc -apar ' template ' -input res_accuracy_minus_chance+orig'];
-        unix(cmd);
+%         cd(result);
+%         template = ['../../../../anat_final.' sub '.pabiode+tlrc'];
+%         cmd = ['@auto_tlrc -apar ' template ' -input res_accuracy_minus_chance+orig'];
+%         unix(cmd);
     end
 end
+
+% generate commands
+CWPath = fileparts(mfilename('fullpath'));
+cd(CWPath);
+f=fopen('result_avg.bash','w');
+fprintf(f,'#! /bin/bash\n\n');
+fprintf(f,'# datafolder=/Volumes/WD_E/gufei/7T_odor\n');
+fprintf(f,'datafolder=/Volumes/WD_F/gufei/7T_odor\n');
+fprintf(f,'cd "${datafolder}" || exit\n\n');
+fprintf(f, 'mask=group/mask/allROI+tlrc\n');
+fprintf(f, ['stats=' check(2:end-1) '\n']);
+% 3dtest++
+for con_i=1:combn
+    con = [odors{comb(con_i,1)} '-' odors{comb(con_i,2)}];
+    fprintf(f, ['3dttest++ -prefix group/mvpa/${stats}_' con ' -mask ${mask} -setA ' con '\\\n']);
+    for sub_i=1:subnum
+        sub=sprintf('S%02d',subs(sub_i));
+        test=[rois{1} '/' '2odors_' odors{comb(con_i,1)} '_' odors{comb(con_i,2)}];
+        result = ['./' sub '/' sub '.pabiode.results/mvpa/searchlight' check num2str(shift) '/' test];
+        result_tlrc = [result '/res_accuracy_minus_chance+tlrc'];
+        fprintf(f, [sprintf('%02d "%s"',sub_i,result_tlrc) ' \\\n']);
+    end
+end    
+fclose(f);

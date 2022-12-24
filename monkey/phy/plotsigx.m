f4af1bc8eb9556d5a3445dc7bdbe7a6afe285476
flashsherlock <sherlockholmes221b@163.com>
2022-12-24 16:22:22 +0800

    remove sigx from legend

--- monkey/phy/plotsigx.m
+++ monkey/phy/plotsigx.m
@@ -8,7 +8,9 @@ hold on
 for i = 1:size(p,1)
     x(p(i,:) >= cri) = nan;
     y = ystart(i)*ones(size(x));
-    plot(x,y,'Color',colors(i,:),'linewidth', line_wid)
+    h = plot(x,y,'Color',colors(i,:),'linewidth', line_wid);
+    % the following line skip the name of the previous plot from the legend
+    h.Annotation.LegendInformation.IconDisplayStyle = 'off';
 end
 
 end

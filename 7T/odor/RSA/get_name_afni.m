808058963cf1f53b91b5cae9203bba66aff9cfab
flashsherlock <sherlockholmes221b@163.com>
2023-01-13 15:20:37 +0800

    use prefix and suffix for filename

--- 7T/odor/RSA/get_name_afni.m
+++ 7T/odor/RSA/get_name_afni.m
@@ -9,8 +9,11 @@ timing=findtrs(userOptions.shift,subn);
 
 tr=timing(:,2);
 % generate image name
-% only remove pol
-image_name=[folder filesep 'NIerrts.' subn '.' userOptions.analysis '.onlypolva+orig.BRIK,'];
-% image_name=[folder filesep 'allrun.volreg.' subn '.' userOptions.analysis '+orig.BRIK,'];
+% only remove pol (default)
+if isfield(userOptions,'filename')
+    image_name=[folder filesep userOptions.filename{1} subn '.' userOptions.analysis userOptions.filename{2}];
+else
+    image_name=[folder filesep 'NIerrts.' subn '.' userOptions.analysis '.onlypolva+orig.BRIK,'];
+end
 image_name=strcat(image_name,num2str(tr));
 end

f0e62fa8edd7d8b9eeec661a812ae03e619a0eee
flashsherlock <sherlockholmes221b@163.com>
2023-01-19 17:10:34 +0800

    try decoding using data_pca

--- monkey/phy/roi_decoding_tf.m
+++ monkey/phy/roi_decoding_tf.m
@@ -12,64 +12,85 @@ if ~exist(pic_dir,'dir')
     mkdir(pic_dir);
 end
 % load time-frequency data
-load([data_dir 'tf_level3_' m '.mat'])
-% get number of roi
-load([data_dir 'pic/trial_count/odor_level3_trial_count_' m '.mat'])
+% load([data_dir 'tf_level5_' m '.mat'],'freq_sep_all')
+% load([data_dir 'pic/trial_count/odorresp_level5_trial_count_' m '.mat'],'cur_level_roi')
+load([data_dir 'pic/pca_power/noair/' m '/data_pca-33.mat'])
+load([data_dir 'pic/pca_power/noair/' m '/data_pca_per-33.mat'])
 %% analysis
 % get number of roi
-roi_num=size(cur_level_roi,1);
-% number of repeats
-repeat_num=100;
-% time_range=[1.4 2];
-time=[0:0.2:1];
-% time=[0 0.4 0.6 1 1.2];
+roi_con = {'All','each'};
+roi_connum=length(roi_con);
+data_time=[-3,3];
+% time before -1s (41) may contain nan
+time=[-0.5:0.1:2.5];
+% each tf data point represent 0.05s
 time_win=1;
-
-conditions = {'5odor','vaodor'};
-% conditions = [conditions {'6odor','banana','intensity','fakeva'}];
-% trial_num = [100 100 100 100 100 100 100];
-trial_num = 80*ones(1,length(conditions));
-results = cell(length(conditions),2);
-results(:,1) = conditions';
-for time_i = 1:length(time)
-    time_range=[time(time_i) time(time_i)+time_win];
-    % run decoding for each condition
-    for condition_i = 1:length(conditions)
-        condition = conditions{condition_i};
-        results_odor = cell(roi_num,repeat_num);
-        tnum = trial_num(condition_i);
+conditions = {'5odor'};
+results = cell(roi_connum,length(conditions));
+% run decoding for each condition
+for condition_i = 1:length(conditions)
+    condition = conditions{condition_i};        
+    % each roi_condition
+    for roi_coni=1:roi_connum
+        % select condition
+        switch condition
+            case 'intensity'
+                nlabel = 2;
+                data_pca(:,2) = cellfun(@(x) x(2:3,:,:),data_pca(:,2),'UniformOutput',false);
+            otherwise
+                nlabel = 5;
+                data_pca(:,2) = cellfun(@(x) x(1:5,:,:),data_pca(:,2),'UniformOutput',false);
+        end
+        % combine rois
+        roisdata = {};
+        switch roi_con{roi_coni}
+            case 'All'
+                roisdata{1,1} = 'All';
+                roisdata{1,2} = cat(1,data_pca{:,2});
+            case 'HA'
+                roisdata{1,1} = 'HF';
+                roisdata{1,2} = cat(1,data_pca{ismember(data_pca(:,1),{'Hi','S'}),2});
+                roisdata{2,1} = 'Amy';
+                roisdata{1,2} = cat(1,data_pca{~ismember(data_pca(:,1),{'Hi','S'}),2});
+            otherwise
+                roisdata = data_pca(:,1:2);
+        end
+        roi_num = size(roisdata,1);
+        results_odor = cell(roi_num,2+length(time));
+        results_odor(:,2)=roisdata(:,1);
         % each roi
         for roi_i=1:roi_num
-            % baseline correction
-%             cfg              = [];
-%             cfg.baseline     = [-1 -0.5];
-%             tf_odor = ft_freqbaseline(cfg, freq_sep_all{roi_i});
-            % select time range
-            cfg = [];
-            cfg.latency = time_range;
-            cfg.frequency = [0 80];
-            cfg.keeptrials = 'yes';
-            tf_odor = ft_freqdescriptives(cfg, freq_sep_all{roi_i});
-            % average across time
-            % tf_odor.trial = squeeze(mean(tf_odor.powspctrm,4));
-            tf_odor.trial = reshape(permute(squeeze(tf_odor.powspctrm),...
-                [2 3 1]),[],length(tf_odor.trialinfo))';
-            % remove fields
-            tf_odor = rmfield(tf_odor,{'powspctrm','cfg'});
-            % run decoding
-            parfor repeat_i=1:repeat_num
-                results_odor{roi_i,repeat_i}=sample_lfp_decoding(tf_odor, condition, tnum);
+            results_odor{roi_i,1} = condition;
+            for time_i = 1:length(time)
+                % select time
+                time_range = [time(time_i)-time_win/2 time(time_i)+time_win/2];
+                time_idx = 20*(time_range-data_time(1))+1;
+                tmpdata = roisdata{roi_i,2};
+                tmpdata = tmpdata(:,time_idx(1):time_idx(2),:);                
+                % reshape data
+                tmpdata = permute(tmpdata,[2,1,3]);
+                tmpdata = reshape(tmpdata,[],size(tmpdata,3));
+                passed_data.data = tmpdata;
+                % run decoding
+                [results_odor{roi_i,time_i+2},~]=odor_decoding_function(passed_data,nlabel);
             end
-            results_odor{roi_i,1}.analysis = [cur_level_roi{roi_i,1}, '_' results_odor{roi_i,1}.analysis];        
+            % save results for this condition
+            results{roi_coni,condition_i}=results_odor;
         end
-        % save results for this condition
-        results{condition_i,2}=results_odor;    
     end
-    % save results
-    time_bin=[num2str(time(time_i)) '-' num2str(time(time_i)+time_win) 's'];
-    matname = ['decoding_results' num2str(tnum) '_tf_linear_' time_bin '.mat'];
-    save([pic_dir matname],'results')
-    % plot acc
-%     odor_decoding_results(matname);
-%     close all
+end
+%% plot
+for condition_i = 1:length(conditions)
+    condition = conditions{condition_i};        
+    % each roi_condition
+    for roi_coni=1:roi_connum
+        roic = roi_con{roi_coni};
+        results_odor = results{roi_coni,condition_i};
+        % get acc minus chance
+        acc = cellfun(@(x) x.accuracy_minus_chance.output,results_odor(:,3:end));
+        figure
+        plot(time,acc')
+        legend(results_odor(:,2))
+        title([roic '-' condition])
+    end
 end

8f84737c812284f50fa26618717377977f6e9c82
flashsherlock <sherlockholmes221b@163.com>
2023-01-28 15:41:08 +0800

    add a function to decode data_pca_tf

--- monkey/phy/roi_decoding_tf.m
+++ monkey/phy/roi_decoding_tf.m
@@ -35,66 +35,9 @@ for condition_i = 1:length(conditions)
     condition = conditions{condition_i};        
     % each roi_condition
     for roi_coni=1:roi_connum
-        % select condition
-        switch condition
-            case 'intensity'
-                nlabel = 2;
-                data_pca(:,2) = cellfun(@(x) x(2:3,:,:),data_pca(:,2),'UniformOutput',false);
-            otherwise
-                nlabel = 5;
-                data_pca(:,2) = cellfun(@(x) x(1:5,:,:),data_pca(:,2),'UniformOutput',false);
-        end
-        % combine rois
-        roisdata = {};
-        switch roi_con{roi_coni}
-            case 'All'
-                roisdata{1,1} = 'All';
-                roisdata{1,2} = cat(1,data_pca{:,2});
-            case 'HA'
-                roisdata{1,1} = 'HF';
-                roisdata{1,2} = cat(1,data_pca{ismember(data_pca(:,1),{'Hi','S'}),2});
-                roisdata{2,1} = 'Amy';
-                roisdata{1,2} = cat(1,data_pca{~ismember(data_pca(:,1),{'Hi','S'}),2});
-            otherwise
-                % combine to 7 rois
-                roi_focus = {{'CoA'},{'APir','VCo'}; {'BA'}, {'BL','PaL'};{'CeMe'},{'Ce','Me'};...
-                    {'BM'},{'BM'};{'BL'},{'BL'};{'Hi'},{'Hi'};{'S'},{'S'}};
-                for roi_i=1:size(roisdata,1)
-                    roisdata(roi_i,1) = roi_focus{roi_i,1};
-                    roisdata{roi_i,2} = cat(1,data_pca{ismember(data_pca(:,1),roi_focus{roi_i,2}),2});
-                end
-        end
-        roi_num = size(roisdata,1);
-        results_odor = cell(roi_num,2+length(time));
-        results_odor(:,2)=roisdata(:,1);
-        % each roi
-        for roi_i=1:roi_num
-            results_odor{roi_i,1} = condition;
-            tmp = roisdata{roi_i,2};                
-            parfor time_i = 1:length(time)
-                passed_data = [];
-                % select time
-                time_range = [time(time_i)-time_win/2 time(time_i)+time_win/2];
-                time_idx = dsearchn(data_time',time_range');                
-                tmpdata = tmp(:,time_idx(1):time_idx(2),:);   
-                % data labels
-                odors = kron(ones(size(tmpdata,1)/nlabel,size(tmpdata,2)),(1:nlabel)');
-                tlabel = kron(ones(size(tmpdata,1),1),1:size(tmpdata,2));
-                roi = kron((1:size(tmpdata,1)/nlabel)',ones(nlabel,size(tmpdata,2)));
-                labels = cat(3,odors,tlabel,roi);
-                % reshape data
-                tmpdata = permute(tmpdata,[2,1,3]);
-                tmpdata = reshape(tmpdata,[],size(tmpdata,3));
-                % sort data
-                labels = reshape(permute(labels,[2,1,3]),[],size(labels,3));
-                [labels,I] = sortrows(labels,1);
-                passed_data.data = tmpdata(I,:);
-                % run decoding
-                [results_odor{roi_i,time_i+2},~]=odor_decoding_function(passed_data,nlabel,labels(:,2));
-            end
-            % save results for this condition
-            results{roi_coni,condition_i}=results_odor;
-        end
+        % save results for this condition
+        results{roi_coni,condition_i} = sample_tf_decoding(data_pca, condition, roi_con{roi_coni},time,time_win,data_time );
+        % save permutated results
     end
 end
 %% plot
---
+++ monkey/phy/sample_tf_decoding.m
@@ -0,0 +1,62 @@
+function results_odor = sample_tf_decoding(data_pca, condition, roi_con, time, time_win, data_time )
+% decode data_pca_tf
+    % select condition
+    switch condition
+        case 'intensity'
+            nlabel = 2;
+            data_pca(:,2) = cellfun(@(x) x(2:3,:,:),data_pca(:,2),'UniformOutput',false);
+        otherwise
+            nlabel = 5;
+            data_pca(:,2) = cellfun(@(x) x(1:5,:,:),data_pca(:,2),'UniformOutput',false);
+    end
+    % combine rois    
+    switch roi_con
+        case 'All'
+            roisdata{1,1} = 'All';
+            roisdata{1,2} = cat(1,data_pca{:,2});
+        case 'HA'
+            roisdata{1,1} = 'HF';
+            roisdata{1,2} = cat(1,data_pca{ismember(data_pca(:,1),{'Hi','S'}),2});
+            roisdata{2,1} = 'Amy';
+            roisdata{1,2} = cat(1,data_pca{~ismember(data_pca(:,1),{'Hi','S'}),2});
+        otherwise
+            % combine to 7 rois
+            roi_focus = {{'CoA'},{'APir','VCo'}; {'BA'}, {'BL','PaL'};{'CeMe'},{'Ce','Me'};...
+                {'BM'},{'BM'};{'BL'},{'BL'};{'Hi'},{'Hi'};{'S'},{'S'}};
+            roisdata = cell(size(roisdata,1),2);
+            for roi_i=1:size(roisdata,1)
+                roisdata(roi_i,1) = roi_focus{roi_i,1};
+                roisdata{roi_i,2} = cat(1,data_pca{ismember(data_pca(:,1),roi_focus{roi_i,2}),2});
+            end
+    end
+    roi_num = size(roisdata,1);
+    results_odor = cell(roi_num,2+length(time));
+    results_odor(:,2)=roisdata(:,1);
+    % each roi
+    for roi_i=1:roi_num
+        results_odor{roi_i,1} = condition;
+        tmp = roisdata{roi_i,2};                
+        for time_i = 1:length(time)
+            passed_data = [];
+            % select time
+            time_range = [time(time_i)-time_win/2 time(time_i)+time_win/2];
+            time_idx = dsearchn(data_time',time_range');                
+            tmpdata = tmp(:,time_idx(1):time_idx(2),:);   
+            % data labels
+            odors = kron(ones(size(tmpdata,1)/nlabel,size(tmpdata,2)),(1:nlabel)');
+            tlabel = kron(ones(size(tmpdata,1),1),1:size(tmpdata,2));
+            roi = kron((1:size(tmpdata,1)/nlabel)',ones(nlabel,size(tmpdata,2)));
+            labels = cat(3,odors,tlabel,roi);
+            % reshape data
+            tmpdata = permute(tmpdata,[2,1,3]);
+            tmpdata = reshape(tmpdata,[],size(tmpdata,3));
+            % sort data
+            labels = reshape(permute(labels,[2,1,3]),[],size(labels,3));
+            [labels,I] = sortrows(labels,1);
+            passed_data.data = tmpdata(I,:);
+            % run decoding
+            [results_odor{roi_i,time_i+2},~]=odor_decoding_function(passed_data,nlabel,labels(:,2));
+        end
+    end
+end
+

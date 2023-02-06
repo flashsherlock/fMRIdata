e0bc440e5eaa1eba25479910dd7cda2c59b79aa3
flashsherlock <sherlockholmes221b@163.com>
2023-02-06 19:00:02 +0800

    try to plot betas obtained from glmfit

--- 7T/odor/RSA/5odor/Cleandata_trial/test_corr.m
+++ 7T/odor/RSA/5odor/Cleandata_trial/test_corr.m
@@ -14,7 +14,8 @@ subs = cell(1,length(subn));
 for sub_i = 1:length(subn)
     subs{sub_i} = sprintf('S%02d', subn(sub_i));
 end
-
+useavg = 0;
+plotrdm = 0;
 %% plot the response pattern
 % for field_i = 1:length(fields)    
 %     % for each sub
@@ -27,20 +28,23 @@ end
 %     end 
 % end
 %% use averaged sim RDM
-simavg = mean(cat(3,Models(6, :).RDM),3);
-for sub_i = 1:length(subn)
-    Models(6, sub_i).RDM = simavg;
+if useavg == 1
+    simavg = mean(cat(3,Models(6, :).RDM),3);
+    for sub_i = 1:length(subn)
+        Models(6, sub_i).RDM = simavg;
+    end
 end
 %% pariwise correlation for all subjects
 colms = [];
 cormat = [];
+betas = [];
 % reshape to ncon, if not 180
 ncon = 180;
 for sub_i = 1:length(subn)
     for field_i = 1:length(fields)
         cur_res = responsePatterns.(fields{field_i}).(subs{sub_i});
         % select voxels
-        cur_res = select_voxel(cur_res,100);
+        cur_res = select_voxel(cur_res,100,0,2);
         % only reshape        
 %         cur_res = reshape(cur_res,[],ncon);
         % average
@@ -48,8 +52,8 @@ for sub_i = 1:length(subn)
         cur_res = squeeze(mean(cur_res,2));        
         % RDM
         cur_res = 1-corr(cur_res);
-        % plot
-        if field_i==1
+        % plot        
+        if field_i==1 && plotrdm==1
             figure
             imagesc(cur_res)
             colormap jet
@@ -69,6 +73,12 @@ for sub_i = 1:length(subn)
         colms(:, length(fields)+m_i) = cur_res(triu(true(size(cur_res)), 1));
     end
     cormat(:, :, sub_i) = corr(colms, 'type', 'Spearman');
+    % glmfit
+    for field_i = 1:length(fields)
+        % the first bet is constant term if not set 'constant','off'
+        models = [6,1];
+        betas(field_i,:,sub_i) = glmfit(colms(:,length(fields)+models),colms(:,field_i),'normal')';        
+    end
 end
 
 %% extract the correlation between rois and strut & sim
@@ -83,7 +93,7 @@ strsimr = cormat(length(fields) + 1,length(fields) + 6,:);
 s={[1:size(represent,3)],[1:size(represent,3)/2],[size(represent,3)/2+1:size(represent,3)]};
 for sub_i=1:3
     % average across subs
-    repm = squeeze(mean(represent(:,:,s{sub_i}), 3));
+    repm = squeeze(mean(represent(:,:,s{sub_i}), 3));    
     % plot mean and stand error
     figure('position', [20, 0, 1000, 300], 'Renderer', 'Painters');
     h = bar(repm');
@@ -99,10 +109,27 @@ for sub_i=1:3
     legend({'Struture', 'Similarity'});
     title(['Conditions: ' num2str(ncon),' Sub: ' num2str(sub_i)])
     % save svg figure to data folder
-    saveas(gcf, [datafolder 'Figures/' mask '_strut&val_' num2str(ncon) '_' num2str(sub_i) '.svg']);
+    %saveas(gcf, [datafolder 'Figures/' mask '_strut&val_' num2str(ncon) '_' num2str(sub_i) '.svg']);
 end
-close all
-% export for ANOVA
+%close all
+%% betas
+% remove constant term
+betas(:, 1, :) = [];
+for sub_i=1:3    
+    b = squeeze(mean(betas(:,:,s{sub_i}), 3));
+    figure('position', [20, 0, 1000, 300], 'Renderer', 'Painters');
+    h = bar(b);
+    % set face colors to red and blue
+    h(1).FaceColor = hex2rgb('#f0803b');
+    h(2).FaceColor = hex2rgb('#56a2d4');
+    set(gca,'TicklabelInterpreter','none')
+    set(gca, 'XTickLabel', xl);
+    set(gca, 'FontSize',18)
+    ylabel('Beta');
+    legend({'Struture', 'Similarity'});
+    title(['Conditions: ' num2str(ncon),' Sub: ' num2str(sub_i)])    
+end
+%% export for ANOVA
 repwide = permute(represent,[3 1 2]);
 repwide = reshape(repwide,length(subn),[]);
 names = reshape([strcat(fields,'_str') strcat(fields,'_sim')]',[],1);

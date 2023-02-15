e05ed538d7d892b168d932f1daabd0c2a8ff0aa2
flashsherlock <sherlockholmes221b@163.com>
2023-02-15 09:49:38 +0800

    return ratings if rate==1

--- 7T/odor/MVPA_cleandata/findtrs.m
+++ 7T/odor/MVPA_cleandata/findtrs.m
@@ -1,14 +1,16 @@
-function timing = findtrs( shift,sub,run)
+function timing = findtrs( shift,sub,rating,run)
 % find TRs when odor evoked activity reaches peak
 % shift is the time shift (seconds) after inhalation
 if nargin<3
+    rating=0;
+end
+if nargin<4
     run=1:6;
 end
 
 % datadir=['/Volumes/WD_E/gufei/7T_odor/' sub '/behavior/'];
 datadir=['/Volumes/WD_F/gufei/7T_odor/' sub '/behavior/'];
 sub=[lower(sub) '_run'];
-
 
 % the first run (get parameters)
 i=1;
@@ -42,6 +44,9 @@ timing(1,:,i)=odor;
 timing(2,:,i)=time+(i-1)*trs;
 % run number
 timing(3,:,i)=i;
+% ratings
+timing(4,:,i)=result(:,2);
+timing(5,:,i)=result(:,6);
 
 % start from the 2nd run
 for i=2:length(run)
@@ -63,10 +68,13 @@ for i=2:length(run)
     timing(1,:,i)=odor;
     timing(2,:,i)=time+(i-1)*trs;
     timing(3,:,i)=i;
+    timing(4,:,i)=result(:,2);
+    timing(5,:,i)=result(:,6);
+    
 end
 
 % reshape to only 2 rows
-timing=reshape(timing,[3 length(run)*odor_num*times]);
+timing=reshape(timing,[5 length(run)*odor_num*times]);
 % code odors to 1:4 if above 6
 if timing(1,1)>=7
     timing(1,:)=timing(1,:)-6;
@@ -76,6 +84,8 @@ timing(2,:)=ceil((timing(2,:)+shift)/3);
 % sort by odors
 timing=sortrows(timing');
 % add a column for repeats
-timing=[timing  kron(ones(size(timing,1)/times,1),(1:times)')];
+if rating ~= 1
+    timing=[timing(:,1:3)  kron(ones(size(timing,1)/times,1),(1:times)')];
+end
 end
 

101d37b5f018658c9b65654fc0ce890806736127
flashsherlock <sherlockholmes221b@163.com>
2023-04-12 23:44:32 +0800

    save converted resp data

--- monkey/behavior/resp_response.m
+++ monkey/behavior/resp_response.m
@@ -1,10 +1,9 @@
+%% store results (dates by test)
 data_dir = '/Volumes/WD_D/gufei/monkey_data/respiratory/adinstrument/';
 dates = {'0301','0316'};
-
-% store results (dates by test)
 results=cell(length(dates),4);
 for d = 1:length(dates)
-    %data filename
+    % data filename
     for t = 1:4
         cur_date=dates{d};
         pattern=[data_dir cur_date 'test0' num2str(t) '*' '.mat'];
@@ -13,8 +12,11 @@ for d = 1:length(dates)
         for i=1:length(adname)
             disp(['Processing... ' adname(i).name]);
             resp = load([data_dir adname(i).name]);
-            
-        end
-        
+            % convert data
+            results{d,t}{i}=resp_convert(resp,t);
+        end       
     end
 end
+save([data_dir 'results.mat'],'results')
+%% load and analyze
+load([data_dir 'results.mat'])

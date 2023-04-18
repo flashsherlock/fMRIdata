1ec6bca24dfdb5919cac43a96305f4da084c04f6
flashsherlock <sherlockholmes221b@163.com>
2023-04-18 14:21:16 +0800

    add marker to the last column

--- monkey/behavior/resp_separate.m
+++ monkey/behavior/resp_separate.m
@@ -1,7 +1,10 @@
-% function separated = resp_separate(out)
+function separated = resp_separate(out, plot)
+if nargin < 2
+    plot = 0;
+end
 trials = size(out.marker,1);
-% baselineCorrectedRespiration inhaleOnsets inhaleOffsets
-separated = cell(trials,3,length(out.bmObj));
+% baselineCorrectedRespiration inhaleOnsets inhaleOffsets marker
+separated = cell(trials,4,length(out.bmObj));
 odor_time = 7;
 for trial_i=1:trials
     % separate resp and inhalation
@@ -11,27 +14,30 @@ for trial_i=1:trials
         separated{trial_i,1,chan_i}=out.bmObj{chan_i}.baselineCorrectedRespiration(timel:timer);
         separated{trial_i,2,chan_i}=out.bmObj{chan_i}.inhaleOnsets(ismember(out.bmObj{chan_i}.inhaleOnsets,(timel:timer)))-out.marker(trial_i,2)+7*out.bmObj{chan_i}.srate;
         separated{trial_i,3,chan_i}=out.bmObj{chan_i}.inhaleOffsets(ismember(out.bmObj{chan_i}.inhaleOffsets,(timel:timer)))-out.marker(trial_i,2)+7*out.bmObj{chan_i}.srate;
+        separated{trial_i,4,chan_i}=out.marker(trial_i,1);
     end
-    % plot
-    p_time = 1:size(p_data,2);
-    p_data = [separated{trial_i,1,1};separated{trial_i,1,2}];
-    figure;    
-    hold on;
-    % plot inhalation
-    color=[0.5 0.5 0.5;0 0.74902 1];
-    for channel_resp = 1:2
-        plot(p_time,p_data(channel_resp,:),'Color',color(channel_resp,:),'LineWidth',1.5);
-        onpoint = nan(size(p_data,2),1);
-        onpoint(separated{trial_i,2,channel_resp})=p_data(channel_resp,separated{trial_i,2,channel_resp});    
-        offpoint = nan(size(p_data,2),1);
-        offpoint(separated{trial_i,3,channel_resp})=p_data(channel_resp,separated{trial_i,3,channel_resp});
-        plot(onpoint,'>','MarkerFaceColor','g','MarkerSize',5*channel_resp);
-        plot(offpoint,'^','MarkerFaceColor','k','MarkerSize',5*channel_resp);
+    % plot    
+    if plot == 1
+        p_data = [separated{trial_i,1,1};separated{trial_i,1,2}];
+        p_time = 1:size(p_data,2);
+        figure;    
+        hold on;
+        % plot inhalation
+        color=[0.5 0.5 0.5;0 0.74902 1];
+        for channel_resp = 1:2
+            plot(p_time,p_data(channel_resp,:),'Color',color(channel_resp,:),'LineWidth',1.5);
+            onpoint = nan(size(p_data,2),1);
+            onpoint(separated{trial_i,2,channel_resp})=p_data(channel_resp,separated{trial_i,2,channel_resp});    
+            offpoint = nan(size(p_data,2),1);
+            offpoint(separated{trial_i,3,channel_resp})=p_data(channel_resp,separated{trial_i,3,channel_resp});
+            plot(onpoint,'>','MarkerFaceColor','g','MarkerSize',5*channel_resp);
+            plot(offpoint,'^','MarkerFaceColor','k','MarkerSize',5*channel_resp);
+        end
+        xlim([p_time(1) p_time(end)]);
+        % set major tick
+        set(gca,'XTick',p_time(1):1000:p_time(end));
+        set(gca,'XGrid','on');
+        set(gca,'XMinorGrid','on');
     end
-    xlim([p_time(1) p_time(end)]);
-    % set major tick
-    set(gca,'XTick',p_time(1):1000:p_time(end));
-    set(gca,'XGrid','on');
-    set(gca,'XMinorGrid','on');
 end
-% end
+end

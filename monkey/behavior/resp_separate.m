062896e63204b3f542323de8f4820419cc77507b
flashsherlock <sherlockholmes221b@163.com>
2023-04-19 15:40:39 +0800

    add trialplot

--- monkey/behavior/resp_separate.m
+++ monkey/behavior/resp_separate.m
@@ -18,26 +18,7 @@ for trial_i=1:trials
     end
     % plot    
     if plot == 1
-        p_data = [separated{trial_i,1,1};separated{trial_i,1,2}];
-        p_time = 1:size(p_data,2);
-        figure;    
-        hold on;
-        % plot inhalation
-        color=[0.5 0.5 0.5;0 0.74902 1];
-        for channel_resp = 1:2
-            plot(p_time,p_data(channel_resp,:),'Color',color(channel_resp,:),'LineWidth',1.5);
-            onpoint = nan(size(p_data,2),1);
-            onpoint(separated{trial_i,2,channel_resp})=p_data(channel_resp,separated{trial_i,2,channel_resp});    
-            offpoint = nan(size(p_data,2),1);
-            offpoint(separated{trial_i,3,channel_resp})=p_data(channel_resp,separated{trial_i,3,channel_resp});
-            plot(onpoint,'>','MarkerFaceColor','g','MarkerSize',5*channel_resp);
-            plot(offpoint,'^','MarkerFaceColor','k','MarkerSize',5*channel_resp);
-        end
-        xlim([p_time(1) p_time(end)]);
-        % set major tick
-        set(gca,'XTick',p_time(1):1000:p_time(end));
-        set(gca,'XGrid','on');
-        set(gca,'XMinorGrid','on');
+        trialplot(separated,trial_i);
     end
 end
 end

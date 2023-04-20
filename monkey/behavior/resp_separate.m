1f3d8c4826146b6ed7fc11d008d2721378bb007e
flashsherlock <sherlockholmes221b@163.com>
2023-04-20 11:42:35 +0800

    add a parameter to remove trials

--- monkey/behavior/resp_separate.m
+++ monkey/behavior/resp_separate.m
@@ -1,20 +1,27 @@
-function separated = resp_separate(out, plot)
-if nargin < 2
+function separated = resp_separate(out,remove, plot)
+if nargin < 3
     plot = 0;
+end
+if nargin < 2
+    remove = [];
 end
 trials = size(out.marker,1);
 % baselineCorrectedRespiration inhaleOnsets inhaleOffsets marker
 separated = cell(trials,4,length(out.bmObj));
 odor_time = 7;
 for trial_i=1:trials
-    % separate resp and inhalation
-    for chan_i=1:length(out.bmObj)
-        timel = out.marker(trial_i,2)-odor_time*out.bmObj{chan_i}.srate;
-        timer = out.marker(trial_i,2)+odor_time*out.bmObj{chan_i}.srate;
-        separated{trial_i,1,chan_i}=out.bmObj{chan_i}.baselineCorrectedRespiration(timel:timer);
-        separated{trial_i,2,chan_i}=out.bmObj{chan_i}.inhaleOnsets(ismember(out.bmObj{chan_i}.inhaleOnsets,(timel:timer)))-out.marker(trial_i,2)+7*out.bmObj{chan_i}.srate;
-        separated{trial_i,3,chan_i}=out.bmObj{chan_i}.inhaleOffsets(ismember(out.bmObj{chan_i}.inhaleOffsets,(timel:timer)))-out.marker(trial_i,2)+7*out.bmObj{chan_i}.srate;
-        separated{trial_i,4,chan_i}=out.marker(trial_i,1);
+    if ~ismember(out.marker(trial_i,1),remove)
+        % separate resp and inhalation
+        for chan_i=1:length(out.bmObj)
+            srate = out.bmObj{chan_i}.srate;
+            timel = out.marker(trial_i,2)-odor_time*srate;
+            timer = out.marker(trial_i,2)+odor_time*srate;
+            % right padding 5s
+            separated{trial_i,1,chan_i}=out.bmObj{chan_i}.baselineCorrectedRespiration(timel:timer+5*srate);
+            separated{trial_i,2,chan_i}=out.bmObj{chan_i}.inhaleOnsets(ismember(out.bmObj{chan_i}.inhaleOnsets,(timel:timer)))-out.marker(trial_i,2)+odor_time*srate;
+            separated{trial_i,3,chan_i}=out.bmObj{chan_i}.inhaleOffsets(ismember(out.bmObj{chan_i}.inhaleOffsets,(timel:timer)))-out.marker(trial_i,2)+odor_time*srate;
+            separated{trial_i,4,chan_i}=out.marker(trial_i,1);
+        end
     end
     % plot    
     if plot == 1

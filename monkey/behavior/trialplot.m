5521ca9a2c17d46d4b39e8604342638edeced06c
flashsherlock <sherlockholmes221b@163.com>
2023-04-24 22:26:37 +0800

    fix index of points
    min position can be 0

--- monkey/behavior/trialplot.m
+++ monkey/behavior/trialplot.m
@@ -8,9 +8,9 @@ function trialplot(separated,trial_i)
     for channel_resp = 1:2
         plot(p_time,p_data(channel_resp,:),'Color',color(channel_resp,:),'LineWidth',1.5);
         onpoint = nan(size(p_data,2),1);
-        onpoint(separated{trial_i,2,channel_resp})=p_data(channel_resp,separated{trial_i,2,channel_resp});    
+        onpoint(1+separated{trial_i,2,channel_resp})=p_data(channel_resp,1+separated{trial_i,2,channel_resp});    
         offpoint = nan(size(p_data,2),1);
-        offpoint(separated{trial_i,3,channel_resp})=p_data(channel_resp,separated{trial_i,3,channel_resp});
+        offpoint(1+separated{trial_i,3,channel_resp})=p_data(channel_resp,1+separated{trial_i,3,channel_resp});
         plot(onpoint,'>','MarkerFaceColor','g','MarkerSize',5*channel_resp);
         plot(offpoint,'^','MarkerFaceColor','k','MarkerSize',5*channel_resp);
     end

708a1c6f0f534038a9d108faaf49691945162e9f
flashsherlock <sherlockholmes221b@163.com>
2023-05-22 23:19:18 +0800

    results after adding visual areas
    remove s01, s05

--- blind/results_blind_render.R
+++ blind/results_blind_render.R
@@ -2,10 +2,12 @@
 path <- "/Volumes/WD_F/gufei/blind/"
 analysis <- c("pade")
 # subs
-subs <- c(sprintf('S%02d',c(1:10)))
+subs <- c(sprintf('S%02d',c(2:4,6:10)))
 # rois
-roi <- c('Amy8_at165','corticalAmy_at165','CeMeAmy_at165','BaLaAmy_at165','Pir_new_at165','Pir_old_at165','APC_new_at165','APC_old_at165','PPC_at165')
-roiname <- c("Amy","Cortical","CeMe","BaLa","Pir_new","Pir_old","APC_new","APC_old","PPC")
+# roi <- c('Amy8_at165','corticalAmy_at165','CeMeAmy_at165','BaLaAmy_at165','Pir_new_at165','Pir_old_at165','APC_new_at165','APC_old_at165','PPC_at165')
+# roiname <- c("Amy","Cortical","CeMe","BaLa","Pir_new","Pir_old","APC_new","APC_old","PPC")
+roi <- c('Amy8_at165','Pir_new_at165','Pir_old_at165','APC_new_at165','APC_old_at165','PPC_at165','EarlyV_at165','V1_at165','V2_at165','V3_at165')
+roiname <- c("Amy","Pir_new","Pir_old","APC_new","APC_old","PPC","EarlyV","V1","V2","V3")
 
 # render roistats
 suffix <- "_tent_12.txt"

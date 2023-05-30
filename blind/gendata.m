195a405a246fb96a74afb89d50556e5739e5af50
flashsherlock <sherlockholmes221b@163.com>
2023-05-30 22:51:25 +0800

    generate data for s14-s16

--- blind/gendata.m
+++ blind/gendata.m
@@ -1,5 +1,5 @@
 % generate images, timing files for each subject
-subs=11:13;
+subs=14:16;
 rootfolder='/Volumes/WD_F/gufei/blind/';
 % generate files for each subject
 for subi=1:length(subs)
@@ -17,7 +17,7 @@ for subi=1:length(subs)
             runs = [17 18 23 24 25];
             pa = 26;
             str = 5;
-        case {3 4}
+        case {3 4 9 11 14}
             runs = 15:1:20;
             pa = 21;
             str = 22;
@@ -35,10 +35,6 @@ for subi=1:length(subs)
             runs = [14:1:17 19:1:20];
             pa = 21;
             str = 22;
-        case {9 11}
-            runs = 15:1:20;
-            pa = 21;
-            str = 22;
         case 12
             runs = [15 17:1:19 21:1:22];
             pa = 23;
@@ -47,6 +43,14 @@ for subi=1:length(subs)
             runs = [15:1:16 18:1:21];
             pa = 22;
             str = 17;
+        case 15
+            runs = [15:1:16 19:1:22];
+            pa = 23;
+            str = 24;
+        case 16
+            runs = [15 17 19:1:22];
+            pa = 23;
+            str = 24;
     end
     % generate images if they are not exist
     if ~exist([datafolder filesep sub '.str.nii'],'file') 

6ed3674dbc548694965390518931745814fc0cef
flashsherlock <sherlockholmes221b@163.com>
2023-06-02 11:05:43 +0800

    group level anova for 16subs
    use a mask for all ROI

--- blind/blind_group_analysis.bash
+++ blind/blind_group_analysis.bash
@@ -4,11 +4,34 @@ datafolder=/Volumes/WD_F/gufei/blind
 cd "${datafolder}" || exit
 stats=stats
 suffix=pade
-mask=group/mask/bmask.nii
 
+# if allroi.nii not exists
+if [ ! -f group/mask/allroi.nii ]; then
+      # resample maxprob_vol.nii
+      3dresample \
+      -input ProbAtlas_v4/subj_vol_all/maxprob_vol.nii \
+      -master group/mask/bmask.nii \
+      -rmode NN \
+      -prefix group/mask/maxprob_vol.nii
+
+      # mask for allROI
+      3dcalc \
+      -a group/mask/maxprob_vol.nii \
+      -b group/mask/Amy8_align.freesurfer+tlrc \
+      -c group/mask/Pir_new.draw+tlrc \
+      -expr 'amongst(a,1,2,3,4,5,6)+b+c' \
+      -prefix group/mask/allroi.nii
+fi
+
+mask=group/mask/allroi.nii
+
+# if anova results exists, delete it
+if [ -f group/ANOVA_results+tlrc.HEAD ]; then
+      rm group/ANOVA_results+tlrc*
+fi
 # from 1 to 22 are gas ind ros pin app min fru flo
 3dANOVA2                                 \
-      -type 3 -alevels 8 -blevels 11  -mask ${mask} \
+      -type 3 -alevels 8 -blevels 13  -mask ${mask} \
       -dset 1 1 "S02/S02.pade.results/${stats}.S02.${suffix}+tlrc[1]"        \
       -dset 2 1 "S02/S02.pade.results/${stats}.S02.${suffix}+tlrc[4]"        \
       -dset 3 1 "S02/S02.pade.results/${stats}.S02.${suffix}+tlrc[7]"        \
@@ -97,6 +120,22 @@ mask=group/mask/bmask.nii
       -dset 6 11 "S13/S13.pade.results/${stats}.S13.${suffix}+tlrc[16]"      \
       -dset 7 11 "S13/S13.pade.results/${stats}.S13.${suffix}+tlrc[19]"      \
       -dset 8 11 "S13/S13.pade.results/${stats}.S13.${suffix}+tlrc[22]"      \
+      -dset 1 12 "S14/S14.pade.results/${stats}.S14.${suffix}+tlrc[1]"       \
+      -dset 2 12 "S14/S14.pade.results/${stats}.S14.${suffix}+tlrc[4]"       \
+      -dset 3 12 "S14/S14.pade.results/${stats}.S14.${suffix}+tlrc[7]"       \
+      -dset 4 12 "S14/S14.pade.results/${stats}.S14.${suffix}+tlrc[10]"      \
+      -dset 5 12 "S14/S14.pade.results/${stats}.S14.${suffix}+tlrc[13]"      \
+      -dset 6 12 "S14/S14.pade.results/${stats}.S14.${suffix}+tlrc[16]"      \
+      -dset 7 12 "S14/S14.pade.results/${stats}.S14.${suffix}+tlrc[19]"      \
+      -dset 8 12 "S14/S14.pade.results/${stats}.S14.${suffix}+tlrc[22]"      \
+      -dset 1 13 "S16/S16.pade.results/${stats}.S16.${suffix}+tlrc[1]"       \
+      -dset 2 13 "S16/S16.pade.results/${stats}.S16.${suffix}+tlrc[4]"       \
+      -dset 3 13 "S16/S16.pade.results/${stats}.S16.${suffix}+tlrc[7]"       \
+      -dset 4 13 "S16/S16.pade.results/${stats}.S16.${suffix}+tlrc[10]"      \
+      -dset 5 13 "S16/S16.pade.results/${stats}.S16.${suffix}+tlrc[13]"      \
+      -dset 6 13 "S16/S16.pade.results/${stats}.S16.${suffix}+tlrc[16]"      \
+      -dset 7 13 "S16/S16.pade.results/${stats}.S16.${suffix}+tlrc[19]"      \
+      -dset 8 13 "S16/S16.pade.results/${stats}.S16.${suffix}+tlrc[22]"      \
       -amean 1 gas -amean 2 ind -amean 3 ros -amean 4 pin -amean 5 app       \
       -amean 6 min -amean 7 fru -amean 8 flo                                 \
       -adiff 7 8 df_fruflo                                                   \

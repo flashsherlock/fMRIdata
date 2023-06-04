ed8a74ee920d4837485854b99d2d26762b66f7bd
flashsherlock <sherlockholmes221b@163.com>
2023-06-04 17:07:27 +0800

    check grids

---
+++ blind/blind_check_grid.bash
@@ -0,0 +1,24 @@
+#! /bin/bash
+
+datafolder=/Volumes/WD_F/gufei/blind
+cd "${datafolder}" || exit
+
+# for each sub
+for sub in S{01..16}; do
+    # if sub folder not exsist then continue
+      if [[ ! -e "${sub}" ]]; then
+            echo "${sub} not exsist"
+            continue
+            # else cd to sub folder
+      else
+            echo ${sub}
+            cd "${sub}" || exit
+      # check gird
+      3dinfo -prefix -header_line -same_all_grid  ${sub}.run*
+      # find volreg number
+      pbvol=$(ls ${sub}.pade.results/pb0?.*.r01.volreg+orig.HEAD | cut -d / -f2 | cut -c1-4)
+      # echo ${pbvol}
+      3dinfo -prefix -header_line -same_all_grid  ${sub}.pade.results/${pbvol}.${sub}.pade.r*.volreg+orig.HEAD
+      cd ..
+      fi
+done

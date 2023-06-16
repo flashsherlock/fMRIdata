763a36cf4091822be69b8a8d22ad9e55a645f376
flashsherlock <sherlockholmes221b@163.com>
2023-06-16 21:18:22 +0800

    change color & add hline at zero

--- monkey/behavior/respplot.m
+++ monkey/behavior/respplot.m
@@ -16,6 +16,9 @@ alltime = size(resp4s,2);
 x=1:alltime;
 x=x/1000;
 limx=limx/1000;
+% plot a horizontal line at 0
+f0 = plot([limx(1) limx(2)],[0 0],'k--','linewidth', 0.5);
+f0.Annotation.LegendInformation.IconDisplayStyle = 'off';
 % plot respiration
 for con_i = 1:num
     stdshade(resp4s(con_i,:),1.96*semresp4s(con_i,:),0.2,hex2rgb(colors(1,con_i)),x); 
@@ -30,7 +33,11 @@ ystart = 0.5;
 for i = 1:size(p,1)
     x(p(i,:) >= cri) = nan;
     y = ystart(i)*ones(size(x));
-    f2 = plot(x,y,'Color',hex2rgb(colors(1,i)),'linewidth', 1);
+    if size(p,1)==1
+        f2 = plot(x,y,'Color',hex2rgb('#000000'),'linewidth', 1);
+    else
+        f2 = plot(x,y,'Color',hex2rgb(colors(1,i)),'linewidth', 1);
+    end
     % the following line skip the name of the previous plot from the legend
     f2.Annotation.LegendInformation.IconDisplayStyle = 'off';
 end

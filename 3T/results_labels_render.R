c74083dcd5a068b9a146a28ec3234b0dfea6e57c
flashsherlock <sherlockholmes221b@163.com>
2023-07-17 09:38:28 +0800

    render more results

--- 3T/results_labels_render.R
+++ 3T/results_labels_render.R
@@ -35,3 +35,33 @@ for (s in suf) {
                     output_file = paste0("ROIstatas_11s",s),
                     params = list(path = path, sub = subs, roi = roi, roiname = roiname, suffix = suffix))
 }
+
+roi <- c('Amy8_atfixdu165','corticalAmy_atfixdu165','CeMeAmy_atfixdu165','BaLaAmy_atfixdu165')
+# render roistats
+for (s in c("142","142p")) {
+  suffix <- paste0("_tent_", s, ".txt")
+  rmarkdown::render("/Users/mac/Documents/GitHub/fMRIdata/3T/roistatas3t.Rmd",
+                    output_dir = paste0(path,"results_labels_r"),
+                    output_file = paste0("ROIstatas_fixdu2",s),
+                    params = list(path = path, sub = subs, roi = roi, roiname = roiname, suffix = suffix))
+}
+
+roi <- c('Amy8_atfixl165','corticalAmy_atfixl165','CeMeAmy_atfixl165','BaLaAmy_atfixl165')
+# render roistats
+for (s in suf) {
+  suffix <- paste0("_tent_", s, ".txt")
+  rmarkdown::render("/Users/mac/Documents/GitHub/fMRIdata/3T/roistatas3t.Rmd",
+                    output_dir = paste0(path,"results_labels_r"),
+                    output_file = paste0("ROIstatas_fixl",s),
+                    params = list(path = path, sub = subs, roi = roi, roiname = roiname, suffix = suffix))
+}
+
+roi <- c('Amy8_atfix2165','corticalAmy_atfix2165','CeMeAmy_atfix2165','BaLaAmy_atfix2165')
+# render roistats
+for (s in suf) {
+  suffix <- paste0("_tent_", s, ".txt")
+  rmarkdown::render("/Users/mac/Documents/GitHub/fMRIdata/3T/roistatas3t.Rmd",
+                    output_dir = paste0(path,"results_labels_r"),
+                    output_file = paste0("ROIstatas_fix2",s),
+                    params = list(path = path, sub = subs, roi = roi, roiname = roiname, suffix = suffix))
+}

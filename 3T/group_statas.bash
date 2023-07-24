31587e20aa387c736d9ea8c1aec3b2916c54e4c5
flashsherlock <sherlockholmes221b@163.com>
2023-07-24 16:51:53 +0800

    use inputs for mask and suffix

--- 3T/group_statas.bash
+++ 3T/group_statas.bash
@@ -3,79 +3,11 @@
 datafolder=/Volumes/WD_F/gufei/3T_cw
 # datafolder=/Volumes/WD_D/allsub/
 cd "${datafolder}" || exit
-suffix=de.frame
-mask=group/mask/Amy8_align.freesurfer+tlrc
-outsuffix=fsframe
 
-# S03 S04 S05 S06 S07 S08 S11 S14 S15 S16 S19 S21 S22 S23 S24 S25 S26 S27 S28 S29
-# 3dROIstats -nzmean -mask ${mask} \
-# "S03/analysis/S03.analysis.${suffix}+tlrc[25]" \
-# "S04/analysis/S04.analysis.${suffix}+tlrc[25]" \
-# "S05/analysis/S05.analysis.${suffix}+tlrc[25]" \
-# "S06/analysis/S06.analysis.${suffix}+tlrc[25]" \
-# "S07/analysis/S07.analysis.${suffix}+tlrc[25]" \
-# "S08/analysis/S08.analysis.${suffix}+tlrc[25]" \
-# "S11/analysis/S11.analysis.${suffix}+tlrc[25]" \
-# "S14/analysis/S14.analysis.${suffix}+tlrc[25]" \
-# "S15/analysis/S15.analysis.${suffix}+tlrc[25]" \
-# "S16/analysis/S16.analysis.${suffix}+tlrc[25]" \
-# "S19/analysis/S19.analysis.${suffix}+tlrc[25]" \
-# "S21/analysis/S21.analysis.${suffix}+tlrc[25]" \
-# "S22/analysis/S22.analysis.${suffix}+tlrc[25]" \
-# "S23/analysis/S23.analysis.${suffix}+tlrc[25]" \
-# "S24/analysis/S24.analysis.${suffix}+tlrc[25]" \
-# "S25/analysis/S25.analysis.${suffix}+tlrc[25]" \
-# "S26/analysis/S26.analysis.${suffix}+tlrc[25]" \
-# "S27/analysis/S27.analysis.${suffix}+tlrc[25]" \
-# "S28/analysis/S28.analysis.${suffix}+tlrc[25]" \
-# "S29/analysis/S29.analysis.${suffix}+tlrc[25]" \
-# > group/congruency_${outsuffix}.txt
-# # visible
-# 3dROIstats -nzmean -mask ${mask} \
-# "S03/analysis/S03.analysis.${suffix}+tlrc[28]" \
-# "S04/analysis/S04.analysis.${suffix}+tlrc[28]" \
-# "S05/analysis/S05.analysis.${suffix}+tlrc[28]" \
-# "S06/analysis/S06.analysis.${suffix}+tlrc[28]" \
-# "S07/analysis/S07.analysis.${suffix}+tlrc[28]" \
-# "S08/analysis/S08.analysis.${suffix}+tlrc[28]" \
-# "S11/analysis/S11.analysis.${suffix}+tlrc[28]" \
-# "S14/analysis/S14.analysis.${suffix}+tlrc[28]" \
-# "S15/analysis/S15.analysis.${suffix}+tlrc[28]" \
-# "S16/analysis/S16.analysis.${suffix}+tlrc[28]" \
-# "S19/analysis/S19.analysis.${suffix}+tlrc[28]" \
-# "S21/analysis/S21.analysis.${suffix}+tlrc[28]" \
-# "S22/analysis/S22.analysis.${suffix}+tlrc[28]" \
-# "S23/analysis/S23.analysis.${suffix}+tlrc[28]" \
-# "S24/analysis/S24.analysis.${suffix}+tlrc[28]" \
-# "S25/analysis/S25.analysis.${suffix}+tlrc[28]" \
-# "S26/analysis/S26.analysis.${suffix}+tlrc[28]" \
-# "S27/analysis/S27.analysis.${suffix}+tlrc[28]" \
-# "S28/analysis/S28.analysis.${suffix}+tlrc[28]" \
-# "S29/analysis/S29.analysis.${suffix}+tlrc[28]" \
-# > group/congruency_vis_${outsuffix}.txt
-# # invisible
-# 3dROIstats -nzmean -mask ${mask} \
-# "S03/analysis/S03.analysis.${suffix}+tlrc[31]" \
-# "S04/analysis/S04.analysis.${suffix}+tlrc[31]" \
-# "S05/analysis/S05.analysis.${suffix}+tlrc[31]" \
-# "S06/analysis/S06.analysis.${suffix}+tlrc[31]" \
-# "S07/analysis/S07.analysis.${suffix}+tlrc[31]" \
-# "S08/analysis/S08.analysis.${suffix}+tlrc[31]" \
-# "S11/analysis/S11.analysis.${suffix}+tlrc[31]" \
-# "S14/analysis/S14.analysis.${suffix}+tlrc[31]" \
-# "S15/analysis/S15.analysis.${suffix}+tlrc[31]" \
-# "S16/analysis/S16.analysis.${suffix}+tlrc[31]" \
-# "S19/analysis/S19.analysis.${suffix}+tlrc[31]" \
-# "S21/analysis/S21.analysis.${suffix}+tlrc[31]" \
-# "S22/analysis/S22.analysis.${suffix}+tlrc[31]" \
-# "S23/analysis/S23.analysis.${suffix}+tlrc[31]" \
-# "S24/analysis/S24.analysis.${suffix}+tlrc[31]" \
-# "S25/analysis/S25.analysis.${suffix}+tlrc[31]" \
-# "S26/analysis/S26.analysis.${suffix}+tlrc[31]" \
-# "S27/analysis/S27.analysis.${suffix}+tlrc[31]" \
-# "S28/analysis/S28.analysis.${suffix}+tlrc[31]" \
-# "S29/analysis/S29.analysis.${suffix}+tlrc[31]" \
-# > group/congruency_inv_${outsuffix}.txt
+suffix=${1}
+mask=group/mask/${2}+tlrc
+outsuffix=${2}
+
 # 8 conditions
 3dROIstats -nzmean -mask ${mask} \
 "S03/S03.de.results/stats.S03.${suffix}+tlrc[1,4,7,10,13,16,19,22]" \
@@ -105,4 +37,4 @@ outsuffix=fsframe
 "S27/S27.de.results/stats.S27.${suffix}+tlrc[1,4,7,10,13,16,19,22]" \
 "S28/S28.de.results/stats.S28.${suffix}+tlrc[1,4,7,10,13,16,19,22]" \
 "S29/S29.de.results/stats.S29.${suffix}+tlrc[1,4,7,10,13,16,19,22]" \
-> group/conditoins_${outsuffix}.txt
+> group/conditions_${outsuffix}.txt

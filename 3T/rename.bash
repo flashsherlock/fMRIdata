910384e6699143e1774ab9b60f8af1b730015a06
flashsherlock <sherlockholmes221b@163.com>
2023-08-07 23:47:26 +0800

    add comments

--- 3T/rename.bash
+++ 3T/rename.bash
@@ -1,9 +1,10 @@
 #! /bin/bash
+# rename behavior results files
 datafolder=/Volumes/WD_D/allsub/behavior/
 cd "${datafolder}" || exit
 # for each file in the folder rename it
 for file in *.mat; do
-    # rename file start with "s0" of "s10" to name start with s
+    # rename file start with "s0" or "s10" to name start with s
     if [[ ${file:0:2} == "s0" ]]; then
         mv "$file" "${file/s0/s}"
         # echo "${file/s0/s}"

1ddb29dab23953aff6d6b9ca5b56b38f9b096e62
flashsherlock <sherlockholmes221b@163.com>
2023-08-22 14:40:55 +0800

    decoding in the indiv_40 interaction ROI

--- 3T/run_decoding.m
+++ 3T/run_decoding.m
@@ -1,8 +1,8 @@
 analysis_all={'de'};
 shift=[6];
 % rois={'Amy8_at165'};
-rois={'Amy8_align'};
-for i = 4:29
+rois={'Indiv40'};
+for i = 3:29
     sub=sprintf('S%02d',i);
     decoding_roi(sub,analysis_all,rois,shift);
     % close figures

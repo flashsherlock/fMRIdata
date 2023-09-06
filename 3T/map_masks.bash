86c0548c4daaee72a0152b9f50e7f54585c98c6c
flashsherlock <sherlockholmes221b@163.com>
2023-09-06 10:23:14 +0800

    add masks for odor & face_vis

--- 3T/map_masks.bash
+++ 3T/map_masks.bash
@@ -9,7 +9,7 @@ nvox=40
 # for each pvalue
 for p in 0.001 #0.05  
 do
-    for brick in fointer_inv #face_inv odor_inv
+    for brick in odor # face_vis #fointer_inv #face_inv odor_inv
     do
     mask=${datafolder}/group/mask/Cluster${nvox}_${p}_${brick}_${roi}+tlrc
     # indivisual masks
@@ -26,14 +26,14 @@ do
         # cd to results folder
         cd "${subdir}" || exit 
         # if mask exsist then remove it
-        # if [ -e "../mask/Indiv${nvox}_${p}_${brick}_${roi}+orig.HEAD" ]; then
-        #     rm ../mask/Indiv${nvox}_${p}_${brick}_${roi}+orig*
-        # fi   
-        # # map group level masks to individual space
-        # 3dNwarpApply -nwarp "anatSS.${sub}_al_keep_mat.aff12.1D  INV(anatQQ.${sub}.aff12.1D) INV(anatQQ.${sub}_WARP.nii)"   \
-        #      -source ${mask}  -interp NN -ainterp NN                   \
-        #      -master vr_base_min_outlier+orig    \
-        #      -prefix ../mask/Indiv${nvox}_${p}_${brick}_${roi}
+        if [ -e "../mask/Indiv${nvox}_${p}_${brick}_${roi}+orig.HEAD" ]; then
+            rm ../mask/Indiv${nvox}_${p}_${brick}_${roi}+orig*
+        fi   
+        # map group level masks to individual space
+        3dNwarpApply -nwarp "anatSS.${sub}_al_keep_mat.aff12.1D  INV(anatQQ.${sub}.aff12.1D) INV(anatQQ.${sub}_WARP.nii)"   \
+             -source ${mask}  -interp NN -ainterp NN                   \
+             -master vr_base_min_outlier+orig    \
+             -prefix ../mask/Indiv${nvox}_${p}_${brick}_${roi}
         # get tent values
         data_tent=tent.${subj}.odor_14+orig
         # rm ../../stats/${sub}/Indiv${nvox}_${p}_${brick}_${roi}_tent_12.txt

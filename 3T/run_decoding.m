39c19c6e2230c31fe0658c16e72f7e935232b311
flashsherlock <sherlockholmes221b@163.com>
2023-09-15 10:16:09 +0800

    run searchlight

--- 3T/run_decoding.m
+++ 3T/run_decoding.m
@@ -1,13 +1,16 @@
 analysis_all={'de'};
 shift=[6];
-decode = 'face';
-con = {'vis','inv'};
+% decode = 'face';
+% con = {'all','vis','inv'};
+decode = 'odor';
+con = {'all'};
 rois={'Amy8_align','Amy8_at165'};
 % rois={'Indiv40'};
-for i = 3:29
+parfor i = 3:29
     sub=sprintf('S%02d',i);
     for con_i = 1:length(con)
-        decoding_roi(sub,analysis_all,rois,shift,decode,con{con_i});
+%         decoding_roi(sub,analysis_all,rois,shift,decode,con{con_i});
+        decoding_search(sub,analysis_all,rois,shift,decode,con{con_i});
     end
     % close figures
     close all

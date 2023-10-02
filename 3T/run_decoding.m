3c1892f2fb1e0cc44cc22f79d81a8cef80df8a94
flashsherlock <sherlockholmes221b@163.com>
2023-10-02 11:49:22 +0800

    run decoding in OFC & aSTS

--- 3T/run_decoding.m
+++ 3T/run_decoding.m
@@ -5,14 +5,15 @@ shift=[6];
 decode = 'odor';
 con = {'all'};
 % rois={'Amy8_align','Amy8_at165','Pir_new','Pir_new_at165','fusiformCA','fusiformCA_at165'};
-rois={'FFV_CA','FFV_CA_at165','OFC6mm','OFC6mm_at165'};
+% rois={'FFV_CA','FFV_CA_at165','OFC6mm','OFC6mm_at165'};
+rois={'aSTS','OFC_AAL'};
 % rois={'Indiv40'};
 parfor i = 3:29
     sub=sprintf('S%02d',i);
     for con_i = 1:length(con)
 %         try
-%             decoding_roi(sub,analysis_all,rois,shift,decode,con{con_i});
-        decoding_search(sub,analysis_all,rois,shift,decode,con{con_i});
+            decoding_roi(sub,analysis_all,rois,shift,decode,con{con_i});
+%         decoding_search(sub,analysis_all,rois,shift,decode,con{con_i});
 %         catch
 %             disp(sub)
 %         end

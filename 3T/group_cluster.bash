da453b0ab24fc3d0bd23a42cdcb564435f705eda
flashsherlock <sherlockholmes221b@163.com>
2023-10-22 17:27:03 +0800

    generate cluster thresholds for more roi
    without anysig mask

--- 3T/group_cluster.bash
+++ 3T/group_cluster.bash
@@ -43,9 +43,12 @@ elif [ "$1" = "aSTS" ]; then
 elif [ "$1" = "OFC_AAL" ]; then
       mask=group/mask/OFC_AAL+tlrc
       out=OFC_AAL
-else
+elif [ "$1" = "Amy" ]; then
       mask=group/mask/Amy8_align.freesurfer+tlrc
       out=Amy
+else
+      mask=group/mask/$1+tlrc
+      out=$1
 fi
 # if $2 esists, use it as suffix
 if [ -n "$2" ]; then
@@ -56,29 +59,30 @@ else
       outsuffix=${out}
 fi
 # acf parameters
-# zork=($(3dFWHMx -acf group/3dFWHMx_${outsuffix} -mask ${mask} group/errs.wholenew+tlrc))
+zork=($(3dFWHMx -acf group/3dFWHMx_${outsuffix} -mask ${mask} group/errs.wholenew+tlrc))
 # echo the 5-7 elements of $zork
 # acf=${echo ${zork} | awk '{print $5,$6,$7}'}
 # echo ${zork[4]} ${zork[5]} ${zork[6]}
 # cluster simulation
-# 3dClustSim -mask ${mask} -both \
-# -acf ${zork[4]} ${zork[5]} ${zork[6]} \
-# -athr 0.10 0.05 0.02 0.01 \
-# -pthr 0.05 0.01 0.005 0.001 \
-# -iter 10000 -nodec \
-# -cmd group/3dClustSim_${outsuffix}.cmd \
-# -prefix group/${outsuffix}
-# add anysig mask
-for sig in anyvis any anyinv
-do
-zork=($(3dFWHMx -acf group/3dFWHMx_${outsuffix} -mask "3dcalc( -a ${mask} -b group/mask/whole_${sig}_at165+tlrc -expr a*b )" group/errs.wholenew+tlrc))
-3dClustSim -mask "3dcalc( -a ${mask} -b group/mask/whole_${sig}_at165+tlrc -expr a*b )" -both \
+3dClustSim -mask ${mask} -both \
 -acf ${zork[4]} ${zork[5]} ${zork[6]} \
--athr 0.10 0.05 0.01 0.001 \
+-athr 0.10 0.05 0.02 0.01 \
 -pthr 0.05 0.01 0.005 0.001 \
 -iter 10000 -nodec \
--cmd group/findmask/3dClustSim_${sig}_${outsuffix}.cmd \
--prefix group/findmask/${outsuffix}_${sig}
-done
+-cmd group/findmask/3dClustSim_${outsuffix}.cmd \
+-prefix group/findmask/${outsuffix}
+
+# add anysig mask
+# for sig in anyvis any anyinv
+# do
+# zork=($(3dFWHMx -acf group/3dFWHMx_${outsuffix} -mask "3dcalc( -a ${mask} -b group/mask/whole_${sig}_at165+tlrc -expr a*b )" group/errs.wholenew+tlrc))
+# 3dClustSim -mask "3dcalc( -a ${mask} -b group/mask/whole_${sig}_at165+tlrc -expr a*b )" -both \
+# -acf ${zork[4]} ${zork[5]} ${zork[6]} \
+# -athr 0.10 0.05 0.01 0.001 \
+# -pthr 0.05 0.01 0.005 0.001 \
+# -iter 10000 -nodec \
+# -cmd group/findmask/3dClustSim_${sig}_${outsuffix}.cmd \
+# -prefix group/findmask/${outsuffix}_${sig}
+# done
 # add to header
 # $(cat group/3dClustSim_${outsuffix}.cmd) group/ANOVA_results_${outsuffix}+tlrc

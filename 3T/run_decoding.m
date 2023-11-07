08286afe763ee69a0a8d4e3d0fea7cd04e00070f
flashsherlock <sherlockholmes221b@163.com>
2023-11-07 11:06:42 +0800

    run lesion decoding

--- 3T/run_decoding.m
+++ 3T/run_decoding.m
@@ -1,5 +1,7 @@
+%% settings
 analysis_all={'de'};
 shift=[6];
+%% normal decoding
 % decode = 'face';
 % con = {'all','vis','inv'};
 decode = 'odor';
@@ -25,10 +27,10 @@ parfor i = 3:29
     % close figures
     close all
 end
-% decoding trans
+%% decoding trans
 % con = {'con','inc','vis','inv'};
 rois={'Amy8_align','OFC_AAL'};
-rois={'FFV_CA005'};
+% rois={'FFV_CA005'};
 parfor i = 3:29
     sub=sprintf('S%02d',i);
 %     for con_i = 1:length(con)
@@ -40,6 +42,23 @@ parfor i = 3:29
     decoding_roitrans(sub,analysis_all,rois,shift,'of','test_inv')
     decoding_roitrans(sub,analysis_all,rois,shift,'fo','train_vis')
     decoding_roitrans(sub,analysis_all,rois,shift,'fo','train_inv')
+    % close figures
+    close all
+end
+%% lesion
+rois={'Amy8_align','OFC_AAL'};
+prefix={'face_vis','face_inv','odor_all'};
+suffix={'p1','p2','l0','l1','l2'};
+% combine prefix and suffix
+condition=cell(1,length(prefix)*length(suffix));
+for i = 1:length(prefix)
+    for j = 1:length(suffix)
+        condition{(i-1)*length(suffix)+j}=[prefix{i} '_' suffix{j}];
+    end
+end
+parfor i = 3:29
+    sub=sprintf('S%02d',i);
+    decoding_roilesion(sub,analysis_all,rois,shift,condition);
     % close figures
     close all
 end

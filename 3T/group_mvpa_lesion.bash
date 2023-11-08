ef14c20e33036238737bc6799703a3f21d6ed27d
flashsherlock <sherlockholmes221b@163.com>
2023-11-08 12:21:48 +0800

    lesion from the whole ROI
    fix input orders

--- 3T/group_mvpa_lesion.bash
+++ 3T/group_mvpa_lesion.bash
@@ -21,7 +21,7 @@ else
 fi
 
 # if the first input exist and is sm
-if [ -n "$1" ] && [ "$1" = "sm" ]; then
+if [ -n "$3" ] && [ "$3" = "sm" ]; then
       pre="sm_"
 else
       pre=""
@@ -33,46 +33,54 @@ for p in 0.001 #0.05
 do
     for brick in face_vis face_inv odor_all
     do              
-            # if $3 exist
-            if [ -n "$3" ]; then
+            # if $4 exist
+            if [ -n "$4" ]; then
                   # group level masks
                   cd "${datafolder}" || exit                  
-                  old=group/mvpa/lesion/${orad}
-                  mkdir ${old}
-                  # convert to short data first if not exist
-                  if [ ! -f group/mvpa/lesion/${pre}${roi}_${brick}_t+tlrc.HEAD ]; then
-                        3dcalc \
-                        -a group/plotmask/${pre}${roi}_${brick}_t+tlrc \
-                        -prefix group/mvpa/lesion/${pre}${roi}_${brick}_t \
-                        -expr 'a' \
-                        -datum short
-                  fi
-                  # find maxima in RAI coordinate
-                  # use -dset_coords to convert to LPI      
-                  mv group/mvpa/lesion/${pre}${roi}_${brick}_max* ${old}/
-                  # rm group/mvpa/lesion/${pre}${roi}_${brick}_max*
-                  3dmaxima \
-                  -input group/mvpa/lesion/${pre}${roi}_${brick}_t+tlrc \
-                  -spheres_1toN -out_rad 10 -prefix group/mvpa/lesion/${pre}${roi}_${brick}_max \
-                  -min_dist 20 -thresh 1.65 -coords_only > group/mvpa/lesion/${pre}${roi}_${brick}.txt
+                  # old=group/mvpa/lesion/${orad}
+                  # # if dir not exsist then make it
+                  # if [ ! -d "${old}" ]; then
+                  #       mkdir ${old}
+                  # fi
+                  # # convert to short data first if not exist
+                  # if [ ! -f group/mvpa/lesion/${pre}${roi}_${brick}_t+tlrc.HEAD ]; then
+                  #       3dcalc \
+                  #       -a group/plotmask/${pre}${roi}_${brick}_t+tlrc \
+                  #       -prefix group/mvpa/lesion/${pre}${roi}_${brick}_t \
+                  #       -expr 'a' \
+                  #       -datum short
+                  # fi
+                  # # find maxima in RAI coordinate
+                  # # use -dset_coords to convert to LPI      
+                  # mv group/mvpa/lesion/${pre}${roi}_${brick}_max* ${old}/
+                  # # rm group/mvpa/lesion/${pre}${roi}_${brick}_max*
+                  # 3dmaxima \
+                  # -input group/mvpa/lesion/${pre}${roi}_${brick}_t+tlrc \
+                  # -spheres_1toN -out_rad 10 -prefix group/mvpa/lesion/${pre}${roi}_${brick}_max \
+                  # -min_dist 20 -thresh 1.65 -coords_only > group/mvpa/lesion/${pre}${roi}_${brick}.txt
                   # find the first two sphere
                   for i in 1 2
                   do
                         # sphere masks
-                        mv group/mvpa/lesion/${pre}${roi}_${brick}_p${i}* ${old}/
-                        # rm group/mvpa/lesion/${pre}${roi}_${brick}_p${i}*
-                        3dcalc \
-                        -a group/mvpa/lesion/${pre}${roi}_${brick}_max+tlrc \
-                        -expr "equals(a,$i)" \
-                        -prefix group/mvpa/lesion/${pre}${roi}_${brick}_p${i}
+                        # mv group/mvpa/lesion/${pre}${roi}_${brick}_p${i}* ${old}/
+                        # # rm group/mvpa/lesion/${pre}${roi}_${brick}_p${i}*
+                        # 3dcalc \
+                        # -a group/mvpa/lesion/${pre}${roi}_${brick}_max+tlrc \
+                        # -expr "equals(a,$i)" \
+                        # -prefix group/mvpa/lesion/${pre}${roi}_${brick}_p${i}
                         # generate mask with out the cluster
-                        mv group/mvpa/lesion/${pre}${roi}_${brick}_l${i}* ${old}/
-                        # rm group/mvpa/lesion/${pre}${roi}_${brick}_l${i}*
+                        # mv group/mvpa/lesion/${pre}${roi}_${brick}_l${i}* ${old}/
+                        rm group/mvpa/lesion/${pre}${roi}_${brick}_l${i}*
                         3dcalc \
                         -a group/mvpa/lesion/${pre}${roi}_${brick}_p${i}+tlrc \
-                        -b group/mvpa/${pre}${roi}_${brick}_${p}+tlrc \
+                        -b ${mask} \
                         -expr "step(bool(b)-a)" \
                         -prefix group/mvpa/lesion/${pre}${roi}_${brick}_l${i}
+                        # 3dcalc \
+                        # -a group/mvpa/lesion/${pre}${roi}_${brick}_p${i}+tlrc \
+                        # -b group/mvpa/${pre}${roi}_${brick}_${p}+tlrc \
+                        # -expr "step(bool(b)-a)" \
+                        # -prefix group/mvpa/lesion/${pre}${roi}_${brick}_l${i}
                   done
 
                   # # mask without lesion
@@ -98,7 +106,7 @@ do
                   fi
                   # cd to results folder
                   cd "${datafolder}/${subdir}" || exit 
-                  for m in p1 p2 l1 l2 #l0
+                  for m in l1 l2 #p1 p2 #l0
                   do
                         # define group mask
                         gmask=group/mvpa/lesion/${pre}${roi}_${brick}_${m}+tlrc
@@ -109,8 +117,8 @@ do
                               if [ ! -d "../mask/lesion${orad}" ]; then                              
                                     mkdir ../mask/lesion${orad}
                               fi
-                              mv ../mask/${indmask}+orig* ../mask/lesion${orad}/
-                              # rm ../mask/${indmask}+orig*
+                              # mv ../mask/${indmask}+orig* ../mask/lesion${orad}/
+                              rm ../mask/${indmask}+orig*
                         fi   
                         # map group level masks to individual space
                         if [ ! -e "../mask/${indmask}+orig.HEAD" ]; then

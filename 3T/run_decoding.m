e455c2642f2fece1c6869f97a6131182808ca687
flashsherlock <sherlockholmes221b@163.com>
2023-11-22 14:22:50 +0800

    searchlight in BoxROIsext
    use 3voxels radius

--- 3T/run_decoding.m
+++ 3T/run_decoding.m
@@ -19,7 +19,20 @@ parfor i = 3:29
     for con_i = 1:length(con)
 %         try
             decoding_roi(sub,analysis_all,rois,shift,decode,con{con_i});
-%         decoding_search(sub,analysis_all,rois,shift,decode,con{con_i});
+%         catch
+%             disp(sub)
+%         end
+    end
+    % close figures
+    close all
+end
+%% searchlight
+rois = {'BoxROIsext'};
+parfor i = 3:29
+    sub=sprintf('S%02d',i);
+    for con_i = 1:length(con)
+%         try
+        decoding_search(sub,analysis_all,rois,shift,decode,con{con_i});
 %         catch
 %             disp(sub)
 %         end

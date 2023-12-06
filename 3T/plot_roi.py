c53ff599f02b7d1a2b447eab93ff20353e192417
flashsherlock <sherlockholmes221b@163.com>
2023-12-06 12:00:03 +0800

    plot rois in rendering view

--- 3T/plot_roi.py
+++ 3T/plot_roi.py
@@ -40,7 +40,7 @@ gl.colorbarsize(0.05)
 # Set mosaic slices 
 gl.mosaic("A H 0 V 0 0.33")
 # Save the image 
-gl.savebmp(data_dir + 'ROIs.png')
+gl.savebmp(data_dir + '../plotmask/ROIs.png')
 
 # # individual
 # import gl
@@ -83,3 +83,51 @@ gl.savebmp(data_dir + 'ROIs.png')
 # gl.colorbarsize(0.05)
 # # Set mosaic slices 
 # gl.mosaic("A H 0 V 0 0.3 0.4; 0.55 0.60")
+
+# Basic set-up 
+import gl
+gl.resetdefaults()
+# set data directory
+data_dir = '/Volumes/WD_F/gufei/3T_cw/group/mask/'
+# Open background image
+bgimage = 'MNI152_T1_2009c+tlrc'
+# combine data_dir and background image name
+gl.loadimage(data_dir + bgimage)
+# Jagged (0) or smooth (1) interpolation of overlay  
+gl.overlayloadsmooth(1)
+gl.smooth(1)
+# Open overlay
+namelist = ['OFC6mm', 'Pir_new.draw', 'Amy8_align.freesurfer']
+# get length of namelist
+namelen=len(namelist)
+# for n from 1 to length of namelist
+gl.overlaycloseall()
+for n in range(namelen):
+    print(n)
+    name=namelist[n]
+    ovimage = name+'+tlrc'
+    gl.overlayload(data_dir + ovimage)
+    # set cname according to n
+    if n==0:
+        cname = '3blue'
+    elif n==1:
+        cname='2red'
+    elif n==2:
+        cname='1green'
+    # Set overlay display parameters; 1 indicates 1st overlay
+    gl.colorname(n+1,cname)
+    # gl.minmax(n+1, 0, 1)
+    # gl.opacity(n+1, 80)
+    gl.zerointensityinvisible(n+1, 1)
+# Set the color bar options 
+gl.colorbarposition(0)
+gl.colorbarsize(0.05)
+gl.cameradistance(1)
+# gl.cameradistance(2)
+gl.cutout(0.5,0.42,0.33,0,1,1)
+gl.shaderquality1to10(10)
+gl.shadermatcap('01_fuzzy')
+gl.azimuthelevation(95, 20)
+# gl.azimuthelevation(55, 25)
+# Save the image 
+gl.savebmp(data_dir + '../plotmask/ROIsrender.png')

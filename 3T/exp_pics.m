65650b55e9e6f7513ca00f263cb92b5a18fa344d
flashsherlock <sherlockholmes221b@163.com>
2023-12-12 21:56:15 +0800

    save red face and blue noise

--- 3T/exp_pics.m
+++ 3T/exp_pics.m
@@ -1,12 +1,16 @@
 datafolder='/Volumes/WD_D/allsub/fMRI/fMRI/';
 GreenInt=0.6;
 BlueInt=0.6;
+RedInt=1;
 for i=[2 6 13 18]
     GreyFearfulFace=imread([datafolder int2str(i) '.jpg']);
     GreenFearfulFace=zeros(size(GreyFearfulFace));
     GreenFearfulFace(:,:,2)=GreyFearfulFace(:,:,2)*GreenInt;
     GreenFearfulFace(:,:,3)=GreyFearfulFace(:,:,3)*BlueInt;
     imwrite(GreenFearfulFace/255,[datafolder int2str(i) '_gb.jpg'])
+    GreenFearfulFace=zeros(size(GreyFearfulFace));
+    GreenFearfulFace(:,:,1)=GreyFearfulFace(:,:,1)*RedInt;
+    imwrite(GreenFearfulFace/255,[datafolder int2str(i) '_rd.jpg'])
 end
 StimSizeNoise=[0 0 260 260];
 NoiseSize=4; % The number is bigger, the size is smaller.
@@ -15,3 +19,6 @@ CycleFrames=100; % How many noise frames
 NoiseColor=1;
 w=GenerateDynamicCFS_UMNVAL(StimSizeNoise(3), StimSizeNoise(4), CycleFrames, NoiseNum, NoiseSize, NoiseColor);
 imwrite(w(:,:,:,1)/255,[datafolder 'noise.bmp'])
+NoiseColor=2;
+w=GenerateDynamicCFS_UMNVAL(StimSizeNoise(3), StimSizeNoise(4), CycleFrames, NoiseNum, NoiseSize, NoiseColor);
+imwrite(w(:,:,:,1)*BlueInt/255,[datafolder 'noiseb.bmp'])

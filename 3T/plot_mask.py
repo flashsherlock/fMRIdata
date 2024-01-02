edd14e30ad49d86f44173207953d050995fff7f4
flashsherlock <sherlockholmes221b@163.com>
2024-01-02 13:19:21 +0800

    plot percentage

--- 3T/plot_mask.py
+++ 3T/plot_mask.py
@@ -43,7 +43,52 @@ for roi in rois:
 
         # Save the image 
         gl.savebmp(data_dir + 'cplot_' + roi + '_' + name + '.png')
+        
+# plot percentage of
+import gl
+gl.resetdefaults()
+# set data directory
+data_dir = '/Volumes/WD_F/gufei/3T_cw/group/plotmask/'
+# Open background image
+# bgimage = 'MNI152_T1_2009c+tlrc'
+bgimage = '../colin27.nii'
+# combine data_dir and background image name
+gl.loadimage(data_dir + bgimage)
+# Jagged (0) or smooth (1) interpolation of overlay  
+gl.overlayloadsmooth(0)
+gl.smooth(0)  
+# Open overlay
+namelist = ['Amy', 'OFC_AAL']
+# array for cutoff
+cutoff = [0.25, 0.18]
+# get length of namelist
+namelen=len(namelist)
+# for n from 1 to length of namelist
+for n in range(namelen):
+    gl.overlaycloseall()
+    print(n)
+    name=namelist[n]
+    ovimage = '../mvpa/'+'percent_'+name+'+tlrc'
+    gl.overlayload(data_dir + ovimage)      
+    gl.generateclusters(1, cutoff[n], 1, 2, 0)      
+    cname='4hot'
+    # Set overlay display parameters; 1 indicates 1st overlay
+    gl.colorname(1,cname)
+    gl.minmax(1, cutoff[n], cutoff[n]+0.1)
+    gl.opacity(1, 80)
+    gl.zerointensityinvisible(1, 1)
+    # Set the color bar options 
+    gl.colorbarposition(2)
+    gl.colorbarsize(0.05)
+    # Set mosaic slices 
+    if name == 'Amy':
+        gl.mosaic("C H 0 V 0 -5")
+    elif name == 'OFC_AAL':
+        gl.mosaic("A H 0 V 0 -15")
 
+    # Save the image 
+    gl.savebmp(data_dir + 'cplot_' + name + '_' + 'percent' + '.png')
+    
 # # Basic set-up 
 # import gl
 # gl.resetdefaults()

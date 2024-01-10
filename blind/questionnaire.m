598a78088b8dc5e435fd4622228fc2e918f06e70
flashsherlock <sherlockholmes221b@163.com>
2024-01-10 16:22:46 +0800

    update blind questionnaire

--- blind/questionnaire.m
+++ blind/questionnaire.m
@@ -1,5 +1,6 @@
 data_dir = '/Volumes/WD_F/gufei/blind/';
 [num,txt,raw]=xlsread([data_dir 'question.xlsx'],'Sheet1');
+[congen,~,~]=xlsread([data_dir 'questioninfo.xlsx'],'Sheet1','E:F');
 % sort by id
 subseq=txt(2:end,7);
 % remove first character of subseq
@@ -10,7 +11,9 @@ txt = txt(1+idx,:);
 raw = raw(1+idx,:);
 % select sub
 % select_sub = 1:length(subs);
-select_sub = [2:4,6:14 16:17 19:25 27 28:59];
+% no 68 in question subs
+select_sub = [2:4,6:14 16:17 19:25 27 28:62 64:68];
+mrisub = [2:4,6:14 16:17 19:25 27 29:63 65:69];
 subs = subs(select_sub);
 % calculate age from date of birth
 t1 = cellfun(@(x) datetime(x,'InputFormat','yyyy/MM/dd HH:mm:ss'),txt(select_sub,2),'UniformOutput',false);
@@ -18,14 +21,16 @@ t2 = cellfun(@(x) datetime(x,'InputFormat','yyyy-MM-dd'),txt(select_sub,10),'Uni
 age = cellfun(@(x,y) calyears(between(x,y,'years')),t2,t1);
 % 1-female 2-male
 sex = num(select_sub,9);
-nsub = length(select_sub);
-subinfo = [select_sub' sex age];
+subinfo = [mrisub' sex age congen];
+% remove 10 low sight
+% mrisub = mrisub(congen(:,2)==1);
+% select_sub = select_sub(congen(:,2)==1);
 % ratings for 6 odors 48-65
-ratings = reshape(num(select_sub,48:65),nsub,3,[]);
+ratings = reshape(num(select_sub,48:65),length(select_sub),3,[]);
 % intensity
 intensity = squeeze(ratings(:,2,:));
 % repeated measures anova for odors 1-6
-t = table(select_sub',intensity(:,1),intensity(:,2),intensity(:,3),intensity(:,4),intensity(:,5),intensity(:,6),...
+t = table(mrisub',intensity(:,1),intensity(:,2),intensity(:,3),intensity(:,4),intensity(:,5),intensity(:,6),...
     'VariableNames',{'sub','pin','app','ros','min','ind','gas'});
 rm = fitrm(t,'pin-gas~1','WithinDesign',table([1:6]','VariableNames',{'odorIntensity'}));
 ranovatbl = ranova(rm);
@@ -41,12 +46,10 @@ num(:,44:47) = 6-num(:,44:47);
 aware = sum(num(select_sub,[14:43 46]),2);
 results = [subinfo aware num(select_sub,48:65)];
 % add vividness in mri (another selection for subs)
-mrisub = [2:4,6:14,16];
 for sub_i = mrisub
     rate = blind_rate(sub_i);
     results(results(:,1)==sub_i,23:30)=rate.vivid;
     results(results(:,1)==sub_i,31:78)=reshape(rate.vividrun,1,[]);
 end
-% mriresults=results(mrisub,:);
-mriresults = results(1:16,:);
+mriresults=results(:,:);
 save([data_dir,'rating_mri.mat'],'mriresults');

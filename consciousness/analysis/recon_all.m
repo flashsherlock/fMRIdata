8e9039dc31f0256ad37072dc949c42b29d40e246
flashsherlock <sherlockholmes221b@163.com>
2024-02-19 23:28:00 +0800

    change recon_all to a function

--- consciousness/analysis/recon_all.m
+++ consciousness/analysis/recon_all.m
@@ -1,12 +1,15 @@
-% the filename should not contain '-',so change to '_'
-for subi = 17
-sub		   = sprintf('s%02d',subi);
-fshome     = '/Applications/freesurfer/7.1.1/';
-subdir     = ['/Volumes/WD_D/gufei/consciousness/electrode/use/' sub];
-mrfile     = [subdir '/' sub '_MRI_acpc.nii'];
-% recon-all can be run parallely
-system(['export FREESURFER_HOME=' fshome '; ' ...
-'source $FREESURFER_HOME/SetUpFreeSurfer.sh; ' ...
-'mri_convert -c -oc 0 0 0 ' mrfile ' ' [subdir '/tmp.nii'] '; ' ...
-'recon-all -i ' [subdir '/tmp.nii'] ' -s ' sub ' -sd ' subdir ' -all -parallel -threads 8'])
+function recon_all(subs)
+% function to run recon-all command in freesurfer
+    % the filename should not contain '-',so change to '_'
+    for subi = subs
+    sub		   = sprintf('s%02d',subi);
+    fshome     = '/Applications/freesurfer/7.1.1/';
+    subdir     = ['/Volumes/WD_D/gufei/consciousness/electrode/use/' sub];
+    mrfile     = [subdir '/' sub '_MRI_acpc.nii'];
+    % recon-all can be run parallely
+    system(['export FREESURFER_HOME=' fshome '; ' ...
+    'source $FREESURFER_HOME/SetUpFreeSurfer.sh; ' ...
+    'mri_convert -c -oc 0 0 0 ' mrfile ' ' [subdir '/tmp.nii'] '; ' ...
+    'recon-all -i ' [subdir '/tmp.nii'] ' -s ' sub ' -sd ' subdir ' -all -parallel -threads 8'])
+    end
 end

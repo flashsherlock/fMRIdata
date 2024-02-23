e25fd0033e55742499aee27baaa498f642200368
flashsherlock <sherlockholmes221b@163.com>
2024-02-23 11:13:12 +0800

    change bold string

--- learning/CV_gf/lua/strong.lua
+++ learning/CV_gf/lua/strong.lua
@@ -2,13 +2,13 @@ function Block(el)
     if el.t == "Para" or el.t == "Plain" then
         for k, _ in ipairs(el.content) do
 
-            if el.content[k].t == "Str" and el.content[k].text == "Zhang," and
+            if el.content[k].t == "Str" and el.content[k].text == "Gu," and
                 el.content[k + 1].t == "Space" and el.content[k + 2].t == "Str" and
-                el.content[k + 2].text:find("^H.") then
+                el.content[k + 2].text:find("^F.") then
 
-                local _, e = el.content[k + 2].text:find("^H.")
+                local _, e = el.content[k + 2].text:find("^F.")
                 local rest = el.content[k + 2].text:sub(e + 1) -- empty if e+1>length
-                el.content[k] = pandoc.Strong {pandoc.Str("Zhang, H.")}
+                el.content[k] = pandoc.Strong {pandoc.Str("Gu, F.")}
                 el.content[k + 1] = pandoc.Str(rest)
                 table.remove(el.content, k + 2) -- safe? another way would be to set element k+2 to Str("")
                 -- no real need to skip ipairs items here

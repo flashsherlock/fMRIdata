2c730b2d10cfe122609e94754e25a5dc49ae726a
flashsherlock <sherlockholmes221b@163.com>
2024-03-14 23:00:55 +0800

    fix maskNames
    fix saving directory

--- 7T/odor/RSA/5odor/Cleandata_trial/maskfullvol.m
+++ 7T/odor/RSA/5odor/Cleandata_trial/maskfullvol.m
@@ -1,13 +1,14 @@
 function responsePatterns = maskfullvol( maskname )
 % mask full volumes
+% maskname='at196'
 rsa_base;
 if strcmp(maskname,'at165') || strcmp(maskname,'at196')
-    userOptions.maskNames(1:5) = strcat(userOptions.maskNames(1:5),'_',maskname);
-    userOptions.maskNames(6:end) = strrep(userOptions.maskNames(6:end),'align',maskname);
+    userOptions.maskNames(6:end) = strcat(userOptions.maskNames(6:end),'_',maskname);
+    userOptions.maskNames(1:5) = strrep(userOptions.maskNames(1:5),'align',maskname);
 end
 %% Data preparation
 fullBrainVols = fMRIDataPreparation_afni(betaCorrespondence, userOptions);
 binaryMasks_nS = fMRIMaskPreparation_afni(userOptions);
 responsePatterns = rsa.fmri.fMRIDataMasking(fullBrainVols, binaryMasks_nS, betaCorrespondence, userOptions);
-save([datafolder 'Cleandata_responsePatterns_' maskname],'responsePatterns')
+save([datafolder 'ImageData/Cleandata_responsePatterns_' maskname],'responsePatterns')
 end

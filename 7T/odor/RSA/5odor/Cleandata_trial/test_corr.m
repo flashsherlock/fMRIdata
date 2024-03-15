e38da463af63649fb3d382655b4a176721c971ce
flashsherlock <sherlockholmes221b@163.com>
2024-03-15 22:14:00 +0800

    convert to a function

--- 7T/odor/RSA/5odor/Cleandata_trial/test_corr.m
+++ 7T/odor/RSA/5odor/Cleandata_trial/test_corr.m
@@ -1,5 +1,8 @@
+function [repwide, p]= test_corr(mask, voxelnum)
 %% load response patterns
-mask = 'at165';
+% mask = 'at165';
+% voxelnum = 0.05;
+chosen = [1 6];
 modelfolder = '/Volumes/WD_F/gufei/7T_odor/results_RSA/5odor_rmpolort_trial/';
 datafolder = '/Volumes/WD_F/gufei/7T_odor/results_RSA/5odor_rmbase_trial/';
 if ~exist([datafolder 'Figures'],'dir')
@@ -55,7 +58,7 @@ for sub_i = 1:length(subn)
     for field_i = 1:length(fields)
         cur_res = responsePatterns.(fields{field_i}).(subs{sub_i});
         % select voxels
-        [cur_res,perw{sub_i,field_i}] = select_voxel(cur_res,0.05,0,0);
+        [cur_res,perw{sub_i,field_i}] = select_voxel(cur_res,voxelnum);
         % only reshape        
 %         cur_res = reshape(cur_res,[],ncon);
         % average
@@ -105,24 +108,23 @@ for sub_i = 1:length(subn)
 %         neur(sr_i,:,sub_i)=mean(atanh(1-colms(idx, 1:length(fields))));
     end
     % glmfit
-    for field_i = 1:length(fields)
-        % the first beta is constant term if not set 'constant','off'
-        colms = zscore(colms,0,1);
-%         models = [5,4,1,6];        
-        labels = {'APairs' 'Haddad' 'Odorspace' 'valence' 'intensity' 'similarity' 'random'};
-        models = [4,5];
-        betas1(field_i,:,sub_i) = glmfit(colms(:,length(fields)+models),colms(:,field_i),'normal')'; 
-        models = [4,5,1];
-        betas2(field_i,:,sub_i) = glmfit(colms(:,length(fields)+models),colms(:,field_i),'normal')';  
-        models = [4,5,6];
-        betas3(field_i,:,sub_i) = glmfit(colms(:,length(fields)+models),colms(:,field_i),'normal')';
-        models = [4,5,7];
-        betas4(field_i,:,sub_i) = glmfit(colms(:,length(fields)+models),colms(:,field_i),'normal')';
-    end
+%     for field_i = 1:length(fields)
+%         % the first beta is constant term if not set 'constant','off'
+%         colms = zscore(colms,0,1);
+% %         models = [5,4,1,6];        
+%         labels = {'APairs' 'Haddad' 'Odorspace' 'valence' 'intensity' 'similarity' 'random'};
+%         models = [4,5];
+%         betas1(field_i,:,sub_i) = glmfit(colms(:,length(fields)+models),colms(:,field_i),'normal')'; 
+%         models = [4,5,1];
+%         betas2(field_i,:,sub_i) = glmfit(colms(:,length(fields)+models),colms(:,field_i),'normal')';  
+%         models = [4,5,6];
+%         betas3(field_i,:,sub_i) = glmfit(colms(:,length(fields)+models),colms(:,field_i),'normal')';
+%         models = [4,5,7];
+%         betas4(field_i,:,sub_i) = glmfit(colms(:,length(fields)+models),colms(:,field_i),'normal')';
+%     end
 end
 %% extract the correlation between rois and strut & sim
 represent = zeros(2,length(fields),length(subn));
-chosen = [1 6];
 for sub_i = 1:length(subn)
     % 1-7 APairs Haddad Odorspace mrvalence mrintensity mrsimilarity random
     represent(:, :, sub_i) = cormat([length(fields) + chosen(1) length(fields) + chosen(2)], 1:length(fields), sub_i);
@@ -180,45 +182,48 @@ end
 % end
 %close all
 %% betas
-% remove constant term
-betas(:, 1:2, :) = betas1(:,2:3,:);
-% structure
-betas(:, 3, :) = betas2(:,4,:);
-% simlarity
-betas(:, 4, :) = betas3(:,4,:);
-% random
-betas(:, 5, :) = betas4(:,4,:);
-for sub_i=1%:3    
-    b = squeeze(mean(betas(:,:,s{sub_i}), 3));
-    figure('position', [20, 0, 1000, 300], 'Renderer', 'Painters');
-    h = bar(b);
-    % set face colors to red and blue
-%     h(1).FaceColor = hex2rgb('#f0803b');
-%     h(2).FaceColor = hex2rgb('#56a2d4');
-    set(gca,'TicklabelInterpreter','none')
-    set(gca, 'XTickLabel', xl);
-    set(gca, 'FontSize',18)
-    ylabel('Beta');
-    models = [4,5,1,6,7];
-    legend(labels(models));
-    title(['Conditions: ' num2str(ncon),' Sub: ' num2str(sub_i)])    
-end
+% % remove constant term
+% betas(:, 1:2, :) = betas1(:,2:3,:);
+% % structure
+% betas(:, 3, :) = betas2(:,4,:);
+% % simlarity
+% betas(:, 4, :) = betas3(:,4,:);
+% % random
+% betas(:, 5, :) = betas4(:,4,:);
+% for sub_i=1%:3    
+%     b = squeeze(mean(betas(:,:,s{sub_i}), 3));
+%     figure('position', [20, 0, 1000, 300], 'Renderer', 'Painters');
+%     h = bar(b);
+%     % set face colors to red and blue
+% %     h(1).FaceColor = hex2rgb('#f0803b');
+% %     h(2).FaceColor = hex2rgb('#56a2d4');
+%     set(gca,'TicklabelInterpreter','none')
+%     set(gca, 'XTickLabel', xl);
+%     set(gca, 'FontSize',18)
+%     ylabel('Beta');
+%     models = [4,5,1,6,7];
+%     legend(labels(models));
+%     title(['Conditions: ' num2str(ncon),' Sub: ' num2str(sub_i)])    
+% end
 %% plot perw
-perw = cellfun(@sort,perw,'UniformOutput',false);
-perw_mean = cellfun(@(x) mean(x(1:min(50,length(x)))),perw);
-[~,idx]=sort(mean(perw_mean,2));
-disp(subn(idx))
+% perw = cellfun(@sort,perw,'UniformOutput',false);
+% perw_mean = cellfun(@(x) mean(x(1:min(50,length(x)))),perw);
+% [~,idx]=sort(mean(perw_mean,2));
+% disp(subn(idx))
 %% export for ANOVA
 repwide = permute(represent,[3 1 2]);
 repwide = reshape(repwide,length(subn),[]);
 names = reshape([strcat(fields,'_str') strcat(fields,'_sim')]',[],1);
 % repwide = repwide(idx,:);
 % test interaction
-[h,p,ci,t] = ttest((repwide(:,13)-repwide(:,14))-(repwide(:,17)-repwide(:,18)));
+p=zeros(1,3);
+disp([mask num2str(voxelnum)])
+[h,p(1),ci,t] = ttest((repwide(:,13)-repwide(:,14))-(repwide(:,17)-repwide(:,18)));
 disp(names(13))
-disp(p)
+disp(p(1))
 for roi_i =[15 19]    
-    [h,p,ci,t] = ttest((repwide(:,roi_i)-repwide(:,roi_i+1))-(repwide(:,roi_i+2)-repwide(:,roi_i+3)));
+    [h,p((roi_i-7)/4),ci,t] = ttest((repwide(:,roi_i)-repwide(:,roi_i+1))-(repwide(:,roi_i+2)-repwide(:,roi_i+3)));
     disp(names(roi_i))
-    disp(p)
+    disp(p((roi_i-7)/4))
+end
 end

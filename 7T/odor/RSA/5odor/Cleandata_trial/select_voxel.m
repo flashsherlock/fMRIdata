5a335a59c51ee74ec0ef7f6bcbb9a19854c3f588
flashsherlock <sherlockholmes221b@163.com>
2024-03-16 09:56:04 +0800

    try selecting by activation

--- 7T/odor/RSA/5odor/Cleandata_trial/select_voxel.m
+++ 7T/odor/RSA/5odor/Cleandata_trial/select_voxel.m
@@ -1,42 +1,33 @@
-function [res_select,perw] = select_voxel( cur_res, voxel_num, run, zrun )
+function [res_select,perw] = select_voxel( cur_res, voxel_num, actper)
 % select voxels that has minimum within condition variance
-    if nargin<4
-        zrun = 0;
-    end
     if nargin<3
-        run = 0;
+        actper = 0.90;
     end
     if nargin<2
-        voxel_num = 100;
+        voxel_num = 0.05;
     end
-    % if voxel_num<=1, treat it as percentage
-    voxn = size(cur_res,1);
+    % calculate mean activity
+    voxn = size(cur_res,1);    
+    minnum = 10;
+%     maximum = 100;
+    % if voxel_num<=1, treat it as percentage    
     if voxel_num<=1
         % add minimum number
-        voxel_num = min(30,ceil(voxel_num*voxn));
+        voxel_num = max(minnum,ceil(voxel_num*voxn));
+%         voxel_num = min(maximum,ceil(voxel_num*voxn));
     end
+%     if actper<=1
+%         % add minimum number
+%         actper = max(minnum,ceil(actper*voxn));
+%     end
+%     act = mean(cur_res,2);
+%     [~,actindex]=sort(abs(act),'descend');
+%     cur_res = cur_res(actindex(1:min(actper,voxn)),:);
+    voxn = size(cur_res,1);
     conn = size(cur_res,2);
     runn = 6;
     repeat = 6;
-    if run == 1
-        odorn = conn/runn;
-    else
-        odorn = conn/runn/repeat;
-    end
-    % zscore witin runs
-    if zrun == 1
-        cur_res = reshape(cur_res',[],runn,5,voxn);
-        cur_res = permute(cur_res,[1 3 2 4]);
-        cur_res = reshape(cur_res,[],runn,voxn);
-        cur_res = zscore(cur_res,0,1);
-        cur_res = reshape(cur_res,[],5,runn,voxn);
-        cur_res = permute(cur_res,[1 3 2 4]);
-        cur_res = reshape(cur_res,[],voxn)';
-    elseif zrun == 2
-        cur_res = reshape(cur_res',[],5,voxn);
-        cur_res = zscore(cur_res,0,2);
-        cur_res = reshape(cur_res,[],voxn)';
-    end
+    odorn = conn/runn/repeat;
     % all variance
     ssa=var(cur_res,0,2)*(conn-1);
     % within condition variance
@@ -47,8 +38,31 @@ function [res_select,perw] = select_voxel( cur_res, voxel_num, run, zrun )
     ssb=squeeze(ssb*(conn/odorn));
     % percent of within variance
     perw=ssw./ssa;
-    % sort by perw
+%     % sort by perw
+%     [~,indexper]=sort(perw,'ascend');
+%     [~,indexw]=sort(ssw,'ascend');
+%     [~,indexb]=sort(ssw,'descend');
+%     % calculate scores for each voxel
+%     numbers = 1:voxn;
+%     scores = zeros(voxn,3);
+%     scores(indexper, 1) = numbers;
+%     scores(indexw, 2) = numbers;
+%     scores(indexb, 3) = numbers;
+%     scores = mean(scores,2);
+%     [~,index]=sort(scores,'ascend');
     [~,index]=sort(perw,'ascend');
     res_select = cur_res(index(1:min(voxel_num,voxn)),:);
+    % select by cutoff
+%     res_select = cur_res(perw<=max(0.85,perw(index(min(voxel_num,voxn)))),:);
+    
+    % select mean activation
+%     voxn = size(res_select,1);
+%     if actper<=1
+%         % add minimum number
+%         actper = max(minnum,ceil(actper*voxn));
+%     end
+%     act = mean(res_select,2);
+% %     hist(act)
+%     [~,actindex]=sort(abs(zscore(act)),'descend');
+%     res_select = res_select(actindex(1:min(actper,voxn)),:);
 end
-

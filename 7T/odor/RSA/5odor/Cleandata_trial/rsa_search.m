2cbb448446f8163b57d6ed4689a798daf3395427
flashsherlock <sherlockholmes221b@163.com>
2024-03-18 23:49:44 +0800

    fix length(masks)

--- 7T/odor/RSA/5odor/Cleandata_trial/rsa_search.m
+++ 7T/odor/RSA/5odor/Cleandata_trial/rsa_search.m
@@ -1,8 +1,9 @@
-masks = {'align','at165','at196'};
+% masks = {'align','at165','at196'};
+masks = {'at165'};
 voxn = [0.05];
 results = cell(length(voxn),length(masks));
 ps = results;
-for mask_i = 1:3
+for mask_i = 1:length(masks)
     for vi = 1:length(voxn)
         [results{vi,mask_i}, ps{vi,mask_i}]=test_corr(masks{mask_i},voxn(vi));
     end

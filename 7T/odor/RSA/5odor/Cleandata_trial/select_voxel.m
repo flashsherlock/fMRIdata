1bac1aea8e92ac8af0914fdc3da4a76a6226a3bc
flashsherlock <sherlockholmes221b@163.com>
2024-03-19 11:45:05 +0800

    select by zscore for each trial

--- 7T/odor/RSA/5odor/Cleandata_trial/select_voxel.m
+++ 7T/odor/RSA/5odor/Cleandata_trial/select_voxel.m
@@ -1,7 +1,7 @@
 function [res_select,perw] = select_voxel( cur_res, voxel_num, actper)
 % select voxels that has minimum within condition variance
     if nargin<3
-        actper = 0.85;
+        actper = 0.95;
     end
     if nargin<2
         voxel_num = 0.05;
@@ -18,15 +18,23 @@ function [res_select,perw] = select_voxel( cur_res, voxel_num, actper)
         % add minimum number
         actper = max(minnum,ceil(actper*voxn));
     end
-%     act = mean(cur_res,2);    
+    % calculate z activity
+    act = zscore(reshape(cur_res',[],voxn),0,1);
+    outindex = all(abs(act)<=3);
+    cur_res = cur_res(outindex,:);
+    voxn = size(cur_res,1);
     % calculate mean z activity
-    act = zscore(squeeze(mean(reshape(cur_res',[],odorn,voxn),1)),0,2);
-    act = mean(abs(act));
+%     act = zscore(squeeze(mean(reshape(cur_res',[],odorn,voxn),1)),0,2);
+%     outindex = all(abs(act)<=4);
+%     cur_res = cur_res(outindex,:);
+%     voxn = size(cur_res,1);
+    % averaged act
+%     act = mean(abs(zscore(squeeze(mean(reshape(cur_res',[],odorn,voxn),1)),0,2)));
 %     [~,actindex]=sort(abs(act),'descend');
-%     cur_res = cur_res(actindex(1:min(actper,voxn)),:);
-    cur_res = cur_res(act<=2&act>=0.45,:);
-    % calculate new voxel number
-    voxn = size(cur_res,1);        
+%     cur_res = cur_res(actindex(1:min(actper,size(cur_res,1))),:);
+%     cur_res = cur_res(act<=2&act>=0.45,:);
+%     voxn = size(cur_res,1);
+
 %     maximum = 100;
     % if voxel_num<=1, treat it as percentage    
     if voxel_num<=1
@@ -59,16 +67,20 @@ function [res_select,perw] = select_voxel( cur_res, voxel_num, actper)
     [~,index]=sort(perw,'ascend');
     res_select = cur_res(index(1:min(voxel_num,voxn)),:);
     % select by cutoff
-%     res_select = cur_res(perw<=max(0.85,perw(index(min(voxel_num,voxn)))),:);
+%     res_select = cur_res(perw<=max(0,perw(index(min(voxel_num,voxn)))),:);
     
     % select mean activation
 %     voxn = size(res_select,1);
-%     if actper<=1
-%         % add minimum number
-%         actper = max(minnum,ceil(actper*voxn));
+% %     act = zscore(squeeze(mean(reshape(res_select',[],odorn,voxn),1)),0,2);
+%     act = act(:,index(1:min(voxel_num,voxn)));
+%     % remove outliers
+%     outindex = all(abs(act)<=5);
+% %     if sum(outindex)>=minnum
+%         res_select = res_select(outindex,:);
 %     end
-%     act = mean(res_select,2);
-% %     hist(act)
-%     [~,actindex]=sort(abs(zscore(act)),'descend');
-%     res_select = res_select(actindex(1:min(actper,voxn)),:);
+%     act = mean(abs(act(:,outindex)));
+%     [~,actindex]=sort(abs(act),'descend');
+%     res_select = res_select(actindex(1:min(actper,size(res_select,1))),:);
+%     res_select = res_select(act>=0.45,:);
+%     res_select = res_select(act<=2&act>=0.45,:);
 end

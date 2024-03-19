6fcee8e1dc39b034c5240a2cf1cdc1e99709b1eb
flashsherlock <sherlockholmes221b@163.com>
2024-03-19 23:10:52 +0800

    output resutls to txt file

--- 7T/odor/RSA/5odor/Cleandata_trial/rsa_search.m
+++ 7T/odor/RSA/5odor/Cleandata_trial/rsa_search.m
@@ -1,5 +1,6 @@
-masks = {'align','at165','at196'};
-% masks = {'at165'};
+outdatadir = '/Volumes/WD_F/gufei/7T_odor/figures/';
+% masks = {'align','at165','at196'};
+masks = {'at165'};
 voxn = [0.05];
 ncon = 180;
 results = cell(length(voxn),length(masks));
@@ -7,5 +8,6 @@ ps = results;
 for mask_i = 1:length(masks)
     for vi = 1:length(voxn)
         [results{vi,mask_i}, ps{vi,mask_i}]=test_corr(masks{mask_i},voxn(vi),ncon);
+        dlmwrite([outdatadir 'rsa_' num2str(ncon) '_' masks{mask_i} '.txt'],results{vi,mask_i},'delimiter',' ');
     end
 end

b8a78660fc6f73537fa17f30ea28b49713978356
flashsherlock <sherlockholmes221b@163.com>
2024-03-21 17:30:12 +0800

    average stats for each roi & sub

--- 7T/odor/5odor/plots_7T.R
+++ 7T/odor/5odor/plots_7T.R
@@ -113,6 +113,42 @@ save(results,file = paste0(figure_dir, str_remove(txtname,".txt"),".RData"))
 # 2.2 calculate new values ----------------------------------------------
 # load data
 load(paste0(figure_dir, str_remove(txtname,".txt"),".RData"))
+# compute strnorm
+results[,strnbet:=(abs(`m_cit-lim`)-abs(`m_car-lim`))/(abs(`m_cit-lim`)+abs(`m_car-lim`))]
+# acc below 0 is 0
+results[,`a_lim-tra`:=ifelse(`a_lim-tra`<=0,50,`a_lim-tra`+50)]
+results[,`a_lim-car`:=ifelse(`a_lim-car`<=0,50,`a_lim-car`+50)]
+results[,`a_lim-cit`:=ifelse(`a_lim-cit`<=0,50,`a_lim-cit`+50)]
+results[,`a_lim-ind`:=ifelse(`a_lim-ind`<=0,50,`a_lim-ind`+50)]
+results[,`a_tra-car`:=ifelse(`a_tra-car`<=0,50,`a_tra-car`+50)]
+results[,`a_tra-cit`:=ifelse(`a_tra-cit`<=0,50,`a_tra-cit`+50)]
+results[,`a_tra-ind`:=ifelse(`a_tra-ind`<=0,50,`a_tra-ind`+50)]
+results[,`a_cit-ind`:=ifelse(`a_cit-ind`<=0,50,`a_cit-ind`+50)]
+results[,strnacc:=(`a_lim-cit`-`a_lim-car`)/(`a_lim-cit`+`a_lim-car`)]
+# valence index
+results[,valacc:=(`a_lim-tra`+`a_lim-ind`+`a_tra-cit`+`a_cit-ind`)/2]
+results[,valbet:=(abs(`m_lim-tra`)+abs(`m_ind-lim`-`m_cit-lim`)+abs(`m_ind-lim`)+abs(`m_lim-tra`+`m_cit-lim`))/2]
+# recode rois
+newlables <- c("Deep","Deep","Super","Super","Super","Deep","Super","Deep","APC","PPC","APn")
+results[.(roi = roilabels, to = newlables),on = "roi", roinew := i.to]
+# calculate if abs(t_cit-lim) and abs(t_car-lim) > tvalue
+tvalue <- 1.65
+results[,str:=ifelse(abs(`t_cit-lim`)>tvalue,1,0)]
+results[,qua:=ifelse(abs(`t_car-lim`)>tvalue,1,0)]
+# average stnbet when sign165==1 for each sub
+# avg_results <- results[roinew != "APn" & sig165==1, .(sig = sum(sig165),valacc = mean(valacc),valbet = mean(valbet)), by = list(sub,roinew)]
+avg_results <- results[roinew != "APn" & (str==1 | qua==1), 
+                       .(strnbet=mean(strnbet),
+                         strnacc=mean(strnacc),
+                         valacc = mean(valacc),
+                         valbet = mean(valbet)), 
+                       by = list(sub,roinew)]
+avg_results165 <- results[roinew != "APn" & (str==1 | qua==1) & sig165==1, 
+                       .(strnbet=mean(strnbet),
+                         strnacc=mean(strnacc),
+                         valacc = mean(valacc),
+                         valbet = mean(valbet)), 
+                       by = list(sub,roinew)]
 
 # 3 group level results ----------------------------------------------
 prefix <- "results"

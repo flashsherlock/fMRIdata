f74f249f1b275010c55ac20347900f428abb12a3
flashsherlock <sherlockholmes221b@163.com>
2024-03-27 17:34:44 +0800

    output valence and intensity RSA results

--- 7T/odor/RSA/5odor/Cleandata_trial/rsa_search.m
+++ 7T/odor/RSA/5odor/Cleandata_trial/rsa_search.m
@@ -7,6 +7,15 @@ results = cell(length(voxn),length(masks));
 ps = results;
 for mask_i = 1:length(masks)
     for vi = 1:length(voxn)
+        % valence
+        chosen = [1 4];
+        [results{vi,mask_i}, ps{vi,mask_i}]=test_corr(masks{mask_i},voxn(vi),ncon,chosen);
+        dlmwrite([outdatadir 'rsaval_' num2str(ncon) '_' masks{mask_i} '.txt'],results{vi,mask_i},'delimiter',' ');      
+        % intensity
+        chosen = [1 5];
+        [results{vi,mask_i}, ps{vi,mask_i}]=test_corr(masks{mask_i},voxn(vi),ncon,chosen);
+        dlmwrite([outdatadir 'rsaint_' num2str(ncon) '_' masks{mask_i} '.txt'],results{vi,mask_i},'delimiter',' ');
+        % quality
         [results{vi,mask_i}, ps{vi,mask_i}]=test_corr(masks{mask_i},voxn(vi),ncon);
         dlmwrite([outdatadir 'rsa_' num2str(ncon) '_' masks{mask_i} '.txt'],results{vi,mask_i},'delimiter',' ');
     end

6897b7d332857be5925c1625bff78f91b726dd77
flashsherlock <sherlockholmes221b@163.com>
2024-05-15 12:03:49 +0800

    generate absolute betas and masks for each sub

---
+++ 3T/indiv_absolute.bash
@@ -0,0 +1,42 @@
+#! /bin/bash
+
+datafolder=/Volumes/WD_F/gufei/3T_cw/
+cd "${datafolder}" || exit
+# p=0.001 t=3.3011
+# p=0.01 t=2.5812
+# p=0.05 t=1.9625
+# p=0.1 t=1.6465
+maskdec_t=3.3011
+# for each sub
+for ub in $(count -dig 2 ${1} ${2})
+do
+      sub=S${ub}
+      analysis=de
+      subj=${sub}.${analysis}
+      # absolute value of betas
+      3dcalc  -a "${sub}/${subj}.results/stats.${subj}.new+tlrc[34]" \
+            -expr 'abs(a)' \
+            -prefix ${sub}/${subj}.results/absolute_faceall
+      3dcalc  -a "${sub}/${subj}.results/stats.${subj}.new+tlrc[37]" \
+            -expr 'abs(a)' \
+            -prefix ${sub}/${subj}.results/absolute_facevis
+      3dcalc  -a "${sub}/${subj}.results/stats.${subj}.new+tlrc[40]" \
+            -expr 'abs(a)' \
+            -prefix ${sub}/${subj}.results/absolute_faceinv
+      3dcalc  -a "${sub}/${subj}.results/stats.${subj}.new+tlrc[43]" \
+            -expr 'abs(a)' \
+            -prefix ${sub}/${subj}.results/absolute_odorall
+      # sig map for absolute value
+      3dcalc  -a "${sub}/${subj}.results/stats.${subj}.new+tlrc[35]" \
+            -expr "astep(a,${maskdec_t})" \
+            -prefix ${sub}/mask/absmap_faceall_${maskdec_t}
+      3dcalc  -a "${sub}/${subj}.results/stats.${subj}.new+tlrc[38]" \
+            -expr "astep(a,${maskdec_t})" \
+            -prefix ${sub}/mask/absmap_facevis_${maskdec_t}
+      3dcalc  -a "${sub}/${subj}.results/stats.${subj}.new+tlrc[41]" \
+            -expr "astep(a,${maskdec_t})" \
+            -prefix ${sub}/mask/absmap_faceinv_${maskdec_t}
+      3dcalc  -a "${sub}/${subj}.results/stats.${subj}.new+tlrc[44]" \
+            -expr "astep(a,${maskdec_t})" \
+            -prefix ${sub}/mask/absmap_odorall_${maskdec_t}
+done

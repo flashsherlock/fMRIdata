ed637e0bb931c8717840e01feb190ab5d56ccd01
flashsherlock <sherlockholmes221b@163.com>
2024-05-22 23:28:12 +0800

    try t=1.96 in individual level
    no suffix for previous t=3.30

--- 3T/group_absolute.bash
+++ 3T/group_absolute.bash
@@ -7,43 +7,49 @@ cd "${datafolder}" || exit
 # for each condition odorall faceall facevis faceinv
 for brick in odorall faceall facevis faceinv
 do
+      maskdec_t=1.9625
       for pre in absweight #absolute
       do
+      if [ ${pre} = "absweight" ]; then
+            brickn=${brick}_${maskdec_t}
+      else
+            brickn=${brick}
+      fi
       # if ttest results exists, delete it
-      if [ -f  group/ttest_${pre}_${brick}+tlrc.HEAD ]; then
-            rm  group/ttest_${pre}_${brick}+tlrc*
+      if [ -f  group/ttest_${pre}_${brickn}+tlrc.HEAD ]; then
+            rm  group/ttest_${pre}_${brickn}+tlrc*
       fi
       # 8 conditions
-      3dttest++ -prefix group/ttest_${pre}_${brick}    -setA all        \
-                  01 "S03/S03.de.results/${pre}_${brick}+tlrc" \
-                  02 "S04/S04.de.results/${pre}_${brick}+tlrc" \
-                  03 "S05/S05.de.results/${pre}_${brick}+tlrc" \
-                  04 "S06/S06.de.results/${pre}_${brick}+tlrc" \
-                  05 "S07/S07.de.results/${pre}_${brick}+tlrc" \
-                  06 "S08/S08.de.results/${pre}_${brick}+tlrc" \
-                  07 "S09/S09.de.results/${pre}_${brick}+tlrc" \
-                  08 "S10/S10.de.results/${pre}_${brick}+tlrc" \
-                  09 "S11/S11.de.results/${pre}_${brick}+tlrc" \
-                  10 "S12/S12.de.results/${pre}_${brick}+tlrc" \
-                  11 "S13/S13.de.results/${pre}_${brick}+tlrc" \
-                  12 "S14/S14.de.results/${pre}_${brick}+tlrc" \
-                  13 "S15/S15.de.results/${pre}_${brick}+tlrc" \
-                  14 "S16/S16.de.results/${pre}_${brick}+tlrc" \
-                  15 "S17/S17.de.results/${pre}_${brick}+tlrc" \
-                  16 "S18/S18.de.results/${pre}_${brick}+tlrc" \
-                  17 "S19/S19.de.results/${pre}_${brick}+tlrc" \
-                  18 "S20/S20.de.results/${pre}_${brick}+tlrc" \
-                  19 "S21/S21.de.results/${pre}_${brick}+tlrc" \
-                  20 "S22/S22.de.results/${pre}_${brick}+tlrc" \
-                  21 "S23/S23.de.results/${pre}_${brick}+tlrc" \
-                  22 "S24/S24.de.results/${pre}_${brick}+tlrc" \
-                  23 "S25/S25.de.results/${pre}_${brick}+tlrc" \
-                  24 "S26/S26.de.results/${pre}_${brick}+tlrc" \
-                  25 "S27/S27.de.results/${pre}_${brick}+tlrc" \
-                  26 "S28/S28.de.results/${pre}_${brick}+tlrc" \
-                  27 "S29/S29.de.results/${pre}_${brick}+tlrc"
+      3dttest++ -prefix group/ttest_${pre}_${brickn}    -setA all        \
+                  01 "S03/S03.de.results/${pre}_${brickn}+tlrc" \
+                  02 "S04/S04.de.results/${pre}_${brickn}+tlrc" \
+                  03 "S05/S05.de.results/${pre}_${brickn}+tlrc" \
+                  04 "S06/S06.de.results/${pre}_${brickn}+tlrc" \
+                  05 "S07/S07.de.results/${pre}_${brickn}+tlrc" \
+                  06 "S08/S08.de.results/${pre}_${brickn}+tlrc" \
+                  07 "S09/S09.de.results/${pre}_${brickn}+tlrc" \
+                  08 "S10/S10.de.results/${pre}_${brickn}+tlrc" \
+                  09 "S11/S11.de.results/${pre}_${brickn}+tlrc" \
+                  10 "S12/S12.de.results/${pre}_${brickn}+tlrc" \
+                  11 "S13/S13.de.results/${pre}_${brickn}+tlrc" \
+                  12 "S14/S14.de.results/${pre}_${brickn}+tlrc" \
+                  13 "S15/S15.de.results/${pre}_${brickn}+tlrc" \
+                  14 "S16/S16.de.results/${pre}_${brickn}+tlrc" \
+                  15 "S17/S17.de.results/${pre}_${brickn}+tlrc" \
+                  16 "S18/S18.de.results/${pre}_${brickn}+tlrc" \
+                  17 "S19/S19.de.results/${pre}_${brickn}+tlrc" \
+                  18 "S20/S20.de.results/${pre}_${brickn}+tlrc" \
+                  19 "S21/S21.de.results/${pre}_${brickn}+tlrc" \
+                  20 "S22/S22.de.results/${pre}_${brickn}+tlrc" \
+                  21 "S23/S23.de.results/${pre}_${brickn}+tlrc" \
+                  22 "S24/S24.de.results/${pre}_${brickn}+tlrc" \
+                  23 "S25/S25.de.results/${pre}_${brickn}+tlrc" \
+                  24 "S26/S26.de.results/${pre}_${brickn}+tlrc" \
+                  25 "S27/S27.de.results/${pre}_${brickn}+tlrc" \
+                  26 "S28/S28.de.results/${pre}_${brickn}+tlrc" \
+                  27 "S29/S29.de.results/${pre}_${brickn}+tlrc"
       done
-      # maskdec_t=3.3011
+      
       # # if ttest results exists, delete it
       # if [ -f  group/absmap_${brick}_${maskdec_t}+tlrc.HEAD ]; then
       #       rm  group/absmap_${brick}_${maskdec_t}+tlrc*

633e094bda3b0fca417c6e43c88094212828053b
flashsherlock <sherlockholmes221b@163.com>
2024-06-05 22:14:41 +0800

    masks for absweight0

--- 3T/group_masks_abs.bash
+++ 3T/group_masks_abs.bash
@@ -3,15 +3,16 @@
 datafolder=/Volumes/WD_F/gufei/3T_cw
 # datafolder=/Volumes/WD_D/allsub/
 cd "${datafolder}" || exit
+prefix=absweight0
 # open new file
-printf "odorinv odorvis odorall faceinv facevis faceall\n" > group/absweight_results.txt
+printf "odorinv odorvis odorall faceinv facevis faceall\n" > group/${prefix}_results.txt
 # for each maskdec_t
 for maskdec_t in 1.6465 #1.9625 #2.5812 3.3011
 do
 # for each pvalue
 for p in 0.005 #0.001 0.005 0.01 0.05
 do
-    for roi in OFC_AAL Amy fusiformCA
+    for roi in OFC_AAL Amy fusiformCA #FFV01
     do
       if [ "$roi" = "fusiformCA" ]; then
             mask=group/mask/fusiformCA+tlrc
@@ -45,15 +46,21 @@ do
       #     nvox=10
       results=${roi}_${p}_${maskdec_t}
       # for brick in face face_vis face_inv odor
-      for brick in faceall facevis faceinv odorall odorvis odorinv
+      for brick in faceall facevis faceinv odorall #odorvis odorinv
       do
             # string=$(3dClusterize -nosum -1Dformat \
             #   -inset group/ANOVA_results_wholenew+tlrc \
             #   -mask ${mask} \
             #   -idat "${brick}" -ithr "${brick} t" \
             #   -NN 2 -clust_nvox ${nvox} -bisided p=${p})
+            # string=$(3dClusterize -nosum -1Dformat \
+            # -inset group/ttest_${prefix}_${brick}_${maskdec_t}+tlrc \
+            # -mask ${mask} \
+            # -idat "all_mean" -ithr "all_Tstat" \
+            # -NN 2 -clust_nvox ${nvox} -bisided p=${p})
+            # masks and save the results
             string=$(3dClusterize -nosum -1Dformat \
-            -inset group/ttest_absweight_${brick}_${maskdec_t}+tlrc \
+            -inset group/ttest_${prefix}_${brick}_${maskdec_t}+tlrc \
             -mask ${mask} \
             -idat "all_mean" -ithr "all_Tstat" \
             -NN 2 -clust_nvox ${nvox} -bisided p=${p}\
@@ -64,7 +71,7 @@ do
                   results=1_${results}
             fi
       done
-      printf "%s\n" ${results} >> group/absweight_results.txt
+      printf "%s\n" ${results} >> group/${prefix}_results.txt
     done
 done
 done

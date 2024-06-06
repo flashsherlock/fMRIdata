5e29953215001b50193f960f047c845d31542cfd
flashsherlock <sherlockholmes221b@163.com>
2024-06-06 11:45:06 +0800

    add correction for p value

--- monkey/phy/plotsigx.m
+++ monkey/phy/plotsigx.m
@@ -1,9 +1,16 @@
-function plotsigx( x, p, colors, ystart, line_wid)
+function plotsigx( x, p, colors, ystart, line_wid, correct)
 % plot hline indicating significant x
+if nargin < 6
+    correct = 0;
+end
 if nargin < 5
     line_wid = 1;
 end
 cri = 0.05;
+% correction
+if correct==1
+    cri = cri/length(x);
+end
 hold on
 for i = 1:size(p,1)
     x(p(i,:) >= cri) = nan;

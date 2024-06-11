d9f831666abf5281721552030966c0ea839d66a6
flashsherlock <sherlockholmes221b@163.com>
2024-06-11 11:02:05 +0800

    plot mask for abs

---
+++ 3T/plot_mask_abs.py
@@ -0,0 +1,51 @@
+# Basic set-up 
+import gl
+gl.resetdefaults()
+# set data directory
+data_dir = '/Volumes/WD_F/gufei/3T_cw/group/absplot/'
+# Open background image
+bgimage = '/Volumes/WD_F/gufei/3T_cw/group/colin27.nii'
+# combine data_dir and background image name
+gl.loadimage(bgimage)
+# Jagged (0) or smooth (1) interpolation of overlay  
+gl.overlayloadsmooth(0)
+gl.smooth(0)
+rois = ['Amy', 'OFC_AAL', 'fusiformCA', 'FFV01','whole']
+for roi in rois:
+    # Open overlay
+    namelist = ['_odorall', '_faceinv',  '_facevis']
+    cname = ['3blue', '2green', '1red']
+    # get length of namelist
+    namelen=len(namelist)
+    gl.overlaycloseall()
+    nc=0
+    # for n from 1 to length of namelist
+    for n in range(namelen):
+        print(n)        
+        name=namelist[n]
+        ovimage = roi+name+'_mask+tlrc'
+        status = gl.overlayload(data_dir + ovimage)
+        # if file exists
+        if status:
+            # Set overlay display parameters; 1 indicates 1st overlay
+            gl.colorname(nc+1,cname[n])
+            gl.minmax(nc+1, 0, 1)
+            gl.opacity(nc+1, 80)
+            gl.zerointensityinvisible(n+1, 1)
+            # Set the color bar options 
+            gl.colorbarposition(0)
+            gl.colorbarsize(0.05)
+            nc=nc+1
+    # Set mosaic slices 
+    if roi == 'Amy':
+        gl.mosaic("C H 0 V 0 -3")
+    elif roi == 'OFC_AAL':
+        gl.mosaic("A H 0 V 0 -15")
+    elif roi == 'fusiformCA':
+        gl.mosaic("A H 0 V 0 -20")
+    elif roi == 'FFV01':
+        gl.mosaic("A H 0 V 0 -20")
+    elif roi == 'whole':
+        gl.mosaic("A H 0 V 0 -15")
+    # Save the image 
+    gl.savebmp(data_dir + roi + '_mask.png')
